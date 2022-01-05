c25db1dfb5c0c1daf05b0a0f0e9a4c11dcb3c5ed
hamid_jambaree <goudarzi.hamid@gmail.com>
2022-01-05 12:36:57 -0800

    adding style for mobile screen

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -8,27 +8,6 @@ import Hero from "../components/Hero/Hero";
 import Features from "../components/Features/Features";
 import WhyTaqueria from "../components/WhyTaqueria/WhyTaqueria";
 import LogoGrid from "../components/LogoGrid/LogoGrid";
-import Header from "../components/Header/Header";
-
-// function HomepageHeader() {
-// 	const { siteConfig } = useDocusaurusContext();
-// 	return (
-// 		<header className={clsx("hero hero--primary", styles.heroBanner)}>
-// 			<div className="container">
-// 				<h1 className="hero__title">{siteConfig.title}</h1>
-// 				<p className="hero__subtitle">{siteConfig.tagline}</p>
-// 				<div className={styles.buttons}>
-// 					<Link
-// 						className="button button--secondary button--lg"
-// 						to="/docs/intro"
-// 					>
-// 						Docusaurus Tutorial - 5min ⏱️
-// 					</Link>
-// 				</div>
-// 			</div>
-// 		</header>
-// 	);
-// }
 
 export default function Home() {
 	const { siteConfig } = useDocusaurusContext();
@@ -37,7 +16,6 @@ export default function Home() {
 			title={`Hello from ${siteConfig.title}`}
 			description="Description will go into a meta tag in <head />"
 		>
-			<Header />
 			<main>
 				<Hero />
 				<Features />
