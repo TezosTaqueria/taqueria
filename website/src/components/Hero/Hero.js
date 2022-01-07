6441468bccec72d4565761a12791664a8791967d
hamid_jambaree <goudarzi.hamid@gmail.com>
2022-01-07 11:57:08 -0800

    adding triangles

--- website/src/components/Hero/Hero.js
+++ website/src/components/Hero/Hero.js
@@ -6,6 +6,11 @@ const FeatureList = [
 	{
 		title: "Insert awesome intro title here.",
 		Svg: require("../../../static/img/heroSVG.svg").default,
+		SvgTraiangle1: require("../../../static/img/triangle1.svg").default,
+		SvgTraiangle2: require("../../../static/img/triangle2.svg").default,
+		SvgTraiangle3: require("../../../static/img/triangle3.svg").default,
+		SvgTraiangle4: require("../../../static/img/triangle4.svg").default,
+		SvgTraiangle5: require("../../../static/img/triangle5.svg").default,
 		description: (
 			<>
 				Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no
@@ -16,23 +21,47 @@ const FeatureList = [
 			title: "Quick Start",
 			url: "/blog",
 		},
+		gif: require("../../../static/gif/mp4gifcode.mp4").default,
 	},
 ];
 
-function Feature({ Svg, title, description, link }) {
+function Feature({
+	Svg,
+	title,
+	description,
+	link,
+	gif,
+	SvgTraiangle1,
+	SvgTraiangle2,
+	SvgTraiangle3,
+	SvgTraiangle4,
+	SvgTraiangle5,
+}) {
 	return (
-		<div className={styles.heroCard}>
-			<h1 className={styles.heroTitle}>{title}</h1>
-			<div className={styles.heroCardContent}>
-				<p className={styles.heroCardDescription}>{description}</p>
-				<div className={styles.heroButtonContainer}>
-					<a className={styles.heroButton} href={link.url}>
-						{link.title}
-					</a>
+		<div className={styles.content}>
+			<div className={styles.heroCardContainer}>
+				<div className={styles.heroCard}>
+					<h1 className={styles.heroTitle}>{title}</h1>
+					<div className={styles.heroCardContent}>
+						<p className={styles.heroCardDescription}>{description}</p>
+						<div className={styles.heroButtonContainer}>
+							<a className={styles.heroButton} href={link.url}>
+								{link.title}
+							</a>
+						</div>
+						{link.tilte}
+					</div>
+					<Svg className={styles.featureSvg} alt={title} />
+					<SvgTraiangle1 className={styles.featureSvgTraiangle1} alt={title} />
+					<SvgTraiangle2 className={styles.featureSvgTraiangle2} alt={title} />
+					<SvgTraiangle3 className={styles.featureSvgTraiangle3} alt={title} />
+					<SvgTraiangle4 className={styles.featureSvgTraiangle4} alt={title} />
+					<SvgTraiangle5 className={styles.featureSvgTraiangle5} alt={title} />
 				</div>
-				{link.tilte}
 			</div>
-			<Svg className={styles.featureSvg} alt={title} />
+			<div className={styles.carousel}>
+				<video autoplay="true" muted src={gif} />
+			</div>
 		</div>
 	);
 }
@@ -41,12 +70,7 @@ export default function Hero() {
 	return (
 		<section className={styles.features}>
 			<div className={styles.container}>
-				<div className={styles.content}>
-					<div className={styles.heroCardContainer}>
-						<Feature {...FeatureList[0]} />
-					</div>
-					<div className={styles.carousel}></div>
-				</div>
+				<Feature {...FeatureList[0]} />
 			</div>
 			<div className={styles.leftPurpleLine}></div>
 			<div className={styles.rightPurpleLine}></div>
