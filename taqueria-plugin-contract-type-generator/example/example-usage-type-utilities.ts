8a9214c3bd59738228c74ca1bfb2e749e44b7b08
Rick Love <rick@toldpro.com>
2022-01-21 15:19:00 -0600

    Cleanup

--- taqueria-plugin-contract-type-generator/example/example-usage-type-utilities.ts
+++ taqueria-plugin-contract-type-generator/example/example-usage-type-utilities.ts
@@ -1,5 +1,4 @@
-import { Context, ContractAbstraction, ContractMethod, ContractMethodObject, ContractProvider, StorageProvider, Wallet } from '@taquito/taquito';
-import { EntrypointsResponse, RpcClientInterface, ScriptResponse } from '@taquito/rpc';
+import { ContractAbstraction, ContractMethod, ContractMethodObject, ContractProvider, Wallet } from '@taquito/taquito';
 
 type BaseContractType = { methods: unknown, methodsObject: unknown, storage: unknown };
 
@@ -25,9 +24,6 @@ export type ContractAbstractionFromContractType<TContract extends BaseContractTy
         {},
         ContractStorageOf<TContract>
     >;
-export const contractAbstractionComposer = <TContract extends BaseContractType>() => (abs: ContractAbstraction<ContractProvider>, context: Context): ContractAbstractionFromContractType<TContract> => {
-    return abs as unknown as ContractAbstractionFromContractType<TContract>;
-}
 
 export type WalletContractAbstractionFromContractType<TContract extends BaseContractType> = 
     ContractAbstraction<Wallet, 
@@ -37,41 +33,3 @@ export type WalletContractAbstractionFromContractType<TContract extends BaseCont
         {},
         ContractStorageOf<TContract>
     >;
-export const walletAbstractionComposer = <TContract extends BaseContractType>() => (abs: ContractAbstraction<Wallet>, context: Context): WalletContractAbstractionFromContractType<TContract> => {
-    return abs as unknown as WalletContractAbstractionFromContractType<TContract>;
-}
-
-
-// Taquito Modifications: 
-// - Types need to be added to ContractAbstraction to allow typed methods, methodsObject, and storage
-
-// Possible modification: ContractAbstractionWithExtendedTypes
-// export type ContractAbstractionWithExtendedTypes<T extends ContractProvider | Wallet, 
-// TMethods extends {
-//     [key: string]: (...args: any[]) => ContractMethod<T>;
-// } = {},
-// TMethodsObject extends {
-//     [key: string]: (args?: any) => ContractMethodObject<T>;
-// } = {}, 
-// TStorageDefault = {}> = Omit<ContractAbstraction<T>, 'methods' |'methodsObject'|'storage'> & {
-//     methods: TMethods;
-//     methodsObject: TMethodsObject;
-//     storage<T = TStorageDefault>(): Promise<T>;
-// }
-
-// export class ContractAbstractionWithExtendedTypes<T extends ContractProvider | Wallet, 
-// TMethods extends {
-//     [key: string]: (...args: any[]) => ContractMethod<T>;
-// } = {},
-// TMethodsObject extends {
-//     [key: string]: (args?: any) => ContractMethodObject<T>;
-// } = {}, 
-// TStorageDefault = {}> extends ContractAbstraction<T> {
-//     public methods: TMethods = null as unknown as TMethods;
-//     public methodsObject: TMethodsObject = null as unknown as TMethodsObject;
-//     // public storage: <T = TStorageDefault>() => Promise<TStorageDefault> = null as unknown as () => Promise<TStorageDefault>;
-
-//     constructor(address: string, script: ScriptResponse, provider: T, storageProvider: StorageProvider, entrypoints: EntrypointsResponse, chainId: string, rpc: RpcClientInterface){
-//         super(address, script, provider, storageProvider, entrypoints, chainId, rpc);
-//     }
-// }
