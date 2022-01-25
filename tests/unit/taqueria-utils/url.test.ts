81b2488cf3dc32a28da683f1627f7d9e3c82e7b9
Ivan Romanov <ivan.romanov.box@gmail.com>
2022-01-25 06:23:54 -0800

    Adding unit tests to cover taqueria-utils package

--- tests/unit/taqueria-utils/url.test.ts
+++ tests/unit/taqueria-utils/url.test.ts
@@ -1,24 +1,36 @@
 import { assertEquals, unreachable } from "https://deno.land/std@0.121.0/testing/asserts.ts";
-import {make, Url} from "../../../taqueria-utils/url.ts";
+import {make, Url, view} from "../../../taqueria-utils/url.ts";
 import {fork} from 'https://cdn.skypack.dev/fluture';
 import chai from "https://cdn.skypack.dev/chai@4.3.4?dts";
 
-Deno.test("Positive scenario test", () => {
+Deno.test("Positive scenario test for {make} function", () => {
     const result = make("http://path")
-    // @ts-ignore
     const assertSuccess = (input: Url) => assertEquals(input, "http://path")
-    // @ts-ignore
     const assertUnreachable = () => unreachable();
     fork (assertUnreachable) (assertSuccess) (result);
-    // Where console.error = (input) => console.log(input)
 });
 
 
-Deno.test("Negative scenario test", () => {
+Deno.test("Negative scenario test for {make} function", () => {
     const result = make("path")
     // @ts-ignore
     const assertFailed = (err: TaqError) => assertEquals(err.kind, "E_INVALID_URL")
-    // @ts-ignore
     const assertUnreachable = () => unreachable();
     fork (assertFailed) (assertUnreachable) (result);
+});
+
+Deno.test("Positive scenario test for {view} function", () => {
+    const testUrl = "http://path" as Url;
+    const assert = chai.assert;
+    const result = view(testUrl);
+    assertEquals(result, "http://path")
+    assert.typeOf(result, "string");
+});
+
+
+Deno.test("Negative scenario test for {view} function", () => {
+    const expect = chai.expect;
+    const testUrl = "path" as Url;
+    const result = view(testUrl);
+    expect(result).not.to.equal("http://path");
 });
