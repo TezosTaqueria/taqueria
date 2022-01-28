38c69f66c1e3662f20a6c4ce94f38d35f3f43313
Rick Love <rick@toldpro.com>
2022-01-28 14:23:38 -0600

    Add wallet originate example

--- taqueria-plugin-contract-type-generator/example/example-usage.ts
+++ taqueria-plugin-contract-type-generator/example/example-usage.ts
@@ -3,7 +3,7 @@ import { ExampleContract1ContractType as TestContract, ExampleContract1WalletTyp
 import { ExampleContract2ContractType as TestContract2, ExampleContract2WalletType as TestWallet2  } from './types-file/example-contract-2.types';
 import { nat, tas } from './types-file/type-aliases';
 
-export const exampleContractMethods1 = async () => {
+export const exampleTypedMethods_existingContract = async () => {
 
     const Tezos = new TezosToolkit(`https://YOUR_PREFERRED_RPC_URL`)
 
@@ -48,7 +48,7 @@ export const exampleContractMethods1 = async () => {
 
 };
 
-export const exampleWalletContractMethods1 = async () => {
+export const exampleTypedMethods_existingWallet = async () => {
 
     const Tezos = new TezosToolkit(`https://YOUR_PREFERRED_RPC_URL`)
 
@@ -99,7 +99,7 @@ export const exampleWalletContractMethods1 = async () => {
 
 };
 
-export const exampleContractMethods2 = async () => {
+export const exampleTypedOriginateStorageAndResultContract_contract = async () => {
 
     const Tezos = new TezosToolkit(`https://YOUR_PREFERRED_RPC_URL`)
 
@@ -165,12 +165,120 @@ export const exampleContractMethods2 = async () => {
 
 };
 
+export const exampleTypedOriginateStorageAndResultContract_wallet = async () => {
 
-export const exampleContractStorage1 = async () => {
+    const Tezos = new TezosToolkit(`https://YOUR_PREFERRED_RPC_URL`)
+
+    // TODO: Remove this comment (leaving for debugging purposes for now)
+    // const contractObj = null as unknown as TestContract2;
+    // const storageResult = await contractObj.storage();
+    // const storageMethod = contractObj.storage;
+    // const storageReturnType = null as unknown as ReturnType<typeof contractObj['storage']>;
+    // type PromiseReturnType<T extends (...args: any) => any> = T extends (...args: any) => PromiseLike<infer R> ? R : any;
+    // const s0 = null as unknown as PromiseReturnType<TestContract2['storage']>;
+    // const s = null as unknown as ContractStorageType<TestContract2>;
+    
+    const originationResult = await Tezos.wallet.originate<TestWallet2>({
+        code: ``,
+        // Require typed initial storage
+        storage: {
+            assets: {
+                ledger: tas.bigMap([
+                    { key: { 0: tas.address('tz123'), 1: tas.nat(42) }, value:tas.nat(42) }
+                ]),
+                operators: tas.bigMap([]),
+                token_metadata: tas.bigMap([]),
+                token_total_supply: tas.bigMap([]),
+            },
+            metadata: tas.bigMap([]),
+        },
+    }).send();
+    // Originated contract is also typed
+    const contract = await originationResult.contract();
+    contract.methods.set_admin(tas.address(`tz123`));
+
+    contract.methods.create_token(
+        /*token_id:*/ tas.nat(`100000000000000`),
+        /*token_info:*/ tas.map([
+            { key: `0`, value: tas.bytes(`abc`) },
+            { key: `1`, value: tas.bytes(`def`) },
+        ]),
+    );
+    contract.methods.create_token(
+        /*token_id:*/ tas.nat(`100000000000000`),
+        /*token_info:*/ tas.map({
+            0: tas.bytes(`abc`),
+            1: tas.bytes(`def`),
+        }),
+    );
+
+    // methodsObject
+    contract.methodsObject.set_admin(tas.address(`tz123`));
+    contract.methodsObject.create_token({
+        token_id: tas.nat(`100000000000000`),
+        token_info: tas.map([
+            { key: `0`, value: tas.bytes(`abc`) },
+            { key: `1`, value: tas.bytes(`def`) },
+        ]),
+    });
+    contract.methodsObject.create_token({
+        token_id: tas.nat(`100000000000000`),
+        token_info: tas.map({
+            0: tas.bytes(`abc`),
+            1: tas.bytes(`def`),
+        }),
+    });
+
+};
+
+
+export const exampleTypedStorage_contract = async () => {
 
     const Tezos = new TezosToolkit(`https://YOUR_PREFERRED_RPC_URL`)
 
     const contract = await Tezos.contract.at<TestContract>(`tz123`);
+
+    const getAuctionInfo = async (id: nat) => {
+        const storage = await contract.storage();
+
+        const auctions = storage.auctions;
+        const auction = await auctions.get(id);
+        if (!auction) {
+            throw new Error(`Auction is missing`);
+        }
+        return auction;
+    };
+
+    const auctionId = tas.nat(42);
+
+    // Get current bid
+    const { current_bid } = await getAuctionInfo(auctionId);
+
+    // Make next bid
+    await (await contract.methods.bid(auctionId).send({
+        mutez: true,
+
+        // Not strictly typed yet
+        // amount: tas.add(current_bid, tas.mutez(1000)),
+        amount: tas.number(tas.add(current_bid, tas.mutez(1000))),
+    })).confirmation(100);
+
+    // Get current owner
+    const { highest_bidder } = await getAuctionInfo(auctionId);
+    const userAddress = await Tezos.wallet.pkh();
+
+    if (highest_bidder === userAddress) {
+        console.log(`You are the highest bidder!`);
+    }
+
+};
+
+
+export const exampleTypedStorage_wallet = async () => {
+
+    const Tezos = new TezosToolkit(`https://YOUR_PREFERRED_RPC_URL`)
+
+    const contract = await Tezos.wallet.at<TestWallet>(`tz123`);
 
     const getAuctionInfo = async (id: nat) => {
         const storage = await contract.storage();
