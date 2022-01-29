752cdacef2bd088fbb0c854658d568035f892555
Russell <morton.russell@gmail.com>
2022-01-29 13:17:48 -0800

    Why Taqueria video

--- website/src/components/WhyTaqueria/WhyTaqueria.js
+++ website/src/components/WhyTaqueria/WhyTaqueria.js
@@ -5,7 +5,7 @@ import styles from "./WhyTaqueria.module.css";
 const FeatureList = [
   {
     title: "Why Taqueria?",
-    gif: require("../../../static/gif/mp4gifcode.mp4").default,
+    gif: require("../../../static/gif/taqueria_init.mp4").default,
     features: [
       {
         icon: require("../../../static/img/briefcase.svg").default,
