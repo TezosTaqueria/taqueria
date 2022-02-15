e664175c9957a4a301bbff2662ffcf90c0594ae3
Hamid Goudarzi <51421954+HamidGoudarzi1988@users.noreply.github.com>
2022-02-15 15:37:17 -0800

    add style directory  to config file

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,43 +1,49 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: "Taqueria - Developer Tooling for Tezos",
-	tagline: "Taqueria - Developer Tooling for Tezos",
-	url: "https://taqueria.io",
-	baseUrl: "/",
+	title: 'Taqueria - Developer Tooling for Tezos',
+	tagline: 'Taqueria - Developer Tooling for Tezos',
+	url: 'https://taqueria.io',
+	baseUrl: '/',
 
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
-	favicon: "/img/favicon.ico",
-	organizationName: "ecadlabs", // Usually your GitHub org/user name.
-	projectName: "taqueria", // Usually your repo name.
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: '/img/favicon.ico',
+	organizationName: 'ecadlabs', // Usually your GitHub org/user name.
+	projectName: 'taqueria', // Usually your repo name.
 
 	presets: [
 		[
-			"@docusaurus/preset-classic",
+			'@docusaurus/preset-classic',
 			/** @type {import('@docusaurus/preset-classic').Options} */
 
 			({
 				docs: {
-					path: "docs",
-					sidebarPath: require.resolve("./sidebars.js"),
+					path: 'docs',
+					sidebarPath: require.resolve('./sidebars.js'),
 					// Please change this to your repo.
 
-					editUrl: "https://github.com/ecadlabs/taqueria/edit/main/website/",
+					editUrl: 'https://github.com/ecadlabs/taqueria/edit/main/website/',
 				},
 				blog: {
 					showReadingTime: true,
 					// Please change this to your repo.
 					editUrl:
-						"https://github.com/ecadlabs/taqueria/edit/main/website/blog/",
+						'https://github.com/ecadlabs/taqueria/edit/main/website/blog/',
 				},
 				theme: {
-					customCss: require.resolve("./src/css/custom.css"),
+					customCss: [
+						require.resolve('./src/css/custom.css'),
+						require.resolve('./src/css/tables.scss'),
+						require.resolve('./src/css/admonitions.scss'),
+						require.resolve('./src/css/codBlock.scss'),
+						require.resolve('./src/css/tabs.scss'),
+					],
 				},
 			}),
 		],
@@ -46,124 +52,124 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			gtag: {
-				trackingID: "GTM-N6G3QD5",
+				trackingID: 'GTM-N6G3QD5',
 				anonymizeIP: true,
 			},
 			colorMode: {
-				defaultMode: "light",
+				defaultMode: 'light',
 				disableSwitch: true,
 				respectPrefersColorScheme: false,
 				switchConfig: {
-					darkIcon: "🌙",
+					darkIcon: '🌙',
 					darkIconStyle: {
-						marginLeft: "2px",
+						marginLeft: '2px',
 					},
 					// Unicode icons such as '\u2600' will work
 					// Unicode with 5 chars require brackets: '\u{1F602}'
-					lightIcon: "\u{1F602}",
+					lightIcon: '\u{1F602}',
 					lightIconStyle: {
-						marginLeft: "1px",
+						marginLeft: '1px',
 					},
 				},
 			},
 			navbar: {
 				logo: {
-					alt: "Tezos Taqueria Logo",
-					src: "/img/Taqueria_magenta_beta.svg",
+					alt: 'Tezos Taqueria Logo',
+					src: '/img/Taqueria_magenta_beta.svg',
 				},
 				items: [
 					{
-						type: "doc",
-						docId: "getting-started/installation",
-						label: " Get Started",
-						position: "right",
-						className: "header-link",
+						type: 'doc',
+						docId: 'getting-started/installation',
+						label: ' Get Started',
+						position: 'right',
+						className: 'header-link',
 					},
 					{
-						type: "doc",
-						docId: "intro",
-						position: "right",
-						label: "Docs",
-						className: "header-link",
+						type: 'doc',
+						docId: 'intro',
+						position: 'right',
+						label: 'Docs',
+						className: 'header-link',
 					},
 					{
-						type: "doc",
-						docId: "plugins",
-						label: "Plugins",
-						position: "right",
-						className: "header-link",
+						type: 'doc',
+						docId: 'plugins',
+						label: 'Plugins',
+						position: 'right',
+						className: 'header-link',
 					},
 					{
-						href: "https://discord.gg/bujt7syVVT",
-						position: "right",
-						className: "header-discord-link",
-						"aria-label": "Discord",
+						href: 'https://discord.gg/bujt7syVVT',
+						position: 'right',
+						className: 'header-discord-link',
+						'aria-label': 'Discord',
 					},
 					{
-						href: "https://twitter.com/tezostaqueria",
-						position: "right",
-						className: "header-twitter-link",
-						"aria-label": "Twitter",
+						href: 'https://twitter.com/tezostaqueria',
+						position: 'right',
+						className: 'header-twitter-link',
+						'aria-label': 'Twitter',
 					},
 					{
-						href: "https://github.com/ecadlabs/taqueria",
-						position: "right",
-						className: "header-github-link",
-						"aria-label": "GitHub repository",
+						href: 'https://github.com/ecadlabs/taqueria',
+						position: 'right',
+						className: 'header-github-link',
+						'aria-label': 'GitHub repository',
 					},
 				],
 			},
 			footer: {
 				links: [
 					{
-						title: "Contact",
+						title: 'Contact',
 						items: [
 							{
-								label: "Report Issues",
-								to: "https://github.com/ecadlabs/taqueria/issues/new/choose",
+								label: 'Report Issues',
+								to: 'https://github.com/ecadlabs/taqueria/issues/new/choose',
 							},
 							{
-								label: "Contribute",
-								to: "https://github.com/ecadlabs/taquito/blob/master/CONTRIBUTING.md",
+								label: 'Contribute',
+								to: 'https://github.com/ecadlabs/taquito/blob/master/CONTRIBUTING.md',
 							},
 						],
 					},
 
 					{
-						title: "Community",
+						title: 'Community',
 						items: [
 							{
-								label: "Tezos Stack Exchange",
-								to: "https://tezos.stackexchange.com/questions/tagged/taqueria",
+								label: 'Tezos Stack Exchange',
+								to: 'https://tezos.stackexchange.com/questions/tagged/taqueria',
 							},
 							{
-								label: "Discord",
-								to: "https://discord.gg/bujt7syVVT",
+								label: 'Discord',
+								to: 'https://discord.gg/bujt7syVVT',
 							},
 							{
-								label: "Twitter",
-								to: "https://twitter.com/tezostaqueria",
+								label: 'Twitter',
+								to: 'https://twitter.com/tezostaqueria',
 							},
 							{
-								label: "Code of Conduct",
-								to: "https://github.com/ecadlabs/taquito/blob/master/code-of-conduct.md",
+								label: 'Code of Conduct',
+								to: 'https://github.com/ecadlabs/taquito/blob/master/code-of-conduct.md',
 							},
 							{
-								label: "GitHub",
-								to: "https://github.com/ecadlabs/taqueria",
+								label: 'GitHub',
+								to: 'https://github.com/ecadlabs/taqueria',
 							},
 						],
 					},
 					{
-						title: "Docs",
+						title: 'Docs',
 						items: [
 							{
-								label: "Quick Start",
-								to: "/docs/intro",
+								label: 'Quick Start',
+								to: '/docs/intro',
 							},
 							{
-								label: "Roadmap",
-								to: "https://github.com/ecadlabs/taqueria/milestones?direction=asc&sort=due_date&state=open",
+								label: 'Roadmap',
+								to: 'https://github.com/ecadlabs/taqueria/milestones?direction=asc&sort=due_date&state=open',
 							},
 						],
 					},
@@ -196,8 +202,8 @@ const config = {
 							},
 							{
 								label: `Copyright © 2021 ECAD Labs - This project is licensed under Apache License, Version 2.0`,
-								to: "/",
-								className: "copyright",
+								to: '/',
+								className: 'copyright',
 							},
 						],
 					},
