90743591d9845c8570b80703eb43a17f6a18bfa9
Michael Weichert <mweichert@gmail.com>
2022-02-16 10:32:35 -0500

    Wrapped parcel in loop to ensure its completion when segfaulting (#325)

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -6,4 +6,26 @@ if [ -n $INSTALL ]; then
     npm install
 fi
 
-npm run build --workspace=$WORKSPACE
+retry=10
+success=0
+while [ $retry -ne 0 ]; do
+    output=$(npm run build --workspace=$WORKSPACE 2>&1)
+    success=$?
+    echo $output | grep -i "segmentation" >/dev/null
+    noSegFault=$?
+    if [ $noSegFault -ne 1 ]; then
+        echo "** Could not build ${WORKSPACE} due to Parcel segfault. Retrying..."
+        retry=$((retry-1))
+    else
+        retry=0
+    fi
+    
+done
+
+if [ $success -ne 0 ]; then
+    echo "** Could not build ${WORKSPACE}."
+    echo $output
+    exit 1
+else
+    echo "** Built ${WORKSPACE}"
+fi
