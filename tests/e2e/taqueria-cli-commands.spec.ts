519580a975271e6079a21b46cbb4d7601381d824
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-02-16 22:53:05 +0000

    less strict equality for CI test version

--- tests/e2e/taqueria-cli-commands.spec.ts
+++ tests/e2e/taqueria-cli-commands.spec.ts
@@ -74,7 +74,7 @@ describe("E2E Testing for taqueria CLI,", () => {
     test('Verify that taq reports the correct version', () => {
         const version = execSync('taq --version').toString().trim();
         console.log(process.env)
-        if (process.env.CI === 'true') {
+        if (process.env.CI == 'true') {
             try {
                 const packageJsonContents = JSON.parse(fs.readFileSync('package.json').toString())
                 expect(version).toEqual('v' + packageJsonContents.version)
