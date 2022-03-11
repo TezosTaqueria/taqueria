20b9b35ad0031d9d7c911aba283aaa7d4c2665d0
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-03-11 18:20:51 +0000

    removing test stuff

--- alex-1/types/increment.code.ts
+++
@@ -1,6 +0,0 @@
-
-export const IncrementCode: { __type: 'IncrementCode', protocol: string, code: object[] } = {
-    __type: 'IncrementCode',
-    protocol: 'PsDELPH1Kxsxt8f9eWbxQeRxkjfbxoqM52jvs5Y5fBxWWh4ifpo',
-    code: JSON.parse(`[{"prim":"parameter","args":[{"prim":"or","args":[{"prim":"or","args":[{"prim":"int","annots":["%decrement"]},{"prim":"int","annots":["%increment"]}]},{"prim":"unit","annots":["%reset"]}]}]},{"prim":"storage","args":[{"prim":"int"}]},{"prim":"code","args":[[[[{"prim":"DUP"},{"prim":"CAR"},{"prim":"DIP","args":[[{"prim":"CDR"}]]}]],{"prim":"IF_LEFT","args":[[{"prim":"IF_LEFT","args":[[{"prim":"SWAP"},{"prim":"SUB"}],[{"prim":"ADD"}]]}],[{"prim":"DROP","args":[{"int":"2"}]},{"prim":"PUSH","args":[{"prim":"int"},{"int":"0"}]}]]},{"prim":"NIL","args":[{"prim":"operation"}]},{"prim":"PAIR"}]]}]`)
-};
