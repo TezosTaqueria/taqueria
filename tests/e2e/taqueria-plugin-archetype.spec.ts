d39ba48bee7a93bd076b5cf1c78a65cf464e6d91
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-03-15 20:49:41 +0000

    added in tests for archetype plugin

---
+++ tests/e2e/taqueria-plugin-archetype.spec.ts
@@ -0,0 +1,130 @@
+import {checkFolderExistsWithTimeout, generateTestProject} from "./utils/utils";
+import fs from "fs";
+import {execSync} from "child_process";
+import path from "path";
+
+const taqueriaProjectPath = 'e2e/auto-test-archetype-plugin';
+
+describe("E2E Testing for taqueria ligo plugin",  () => {
+
+    beforeAll(async () => {
+        await generateTestProject(taqueriaProjectPath, ["archetype"], true);
+    })
+
+    test('Verify that taqueria archetype plugin can compile one contract under contracts folder', async () => {
+        try {
+            // 1. Copy contract from data folder to taqueria project folder
+            execSync(`cp e2e/data/fa12.arl ${taqueriaProjectPath}/contracts`);
+
+            // 2. Run taq compile ${contractName}
+            execSync(`taq compile`, {cwd: `./${taqueriaProjectPath}`});
+
+            // 3. Verify that compiled michelson version has been generated
+            await checkFolderExistsWithTimeout(`./${taqueriaProjectPath}/artifacts/fa12.tz`, 25000);
+
+        } catch(error) {
+            throw new Error (`error: ${error}`);
+        }
+
+    });
+
+    test('Verify that taqueria archetype plugin can compile one contract using compile [sourceFile] command', async () => {
+        try {
+            // 1. Copy contract from data folder to taqueria project folder
+            execSync(`cp e2e/data/fa12.arl ${taqueriaProjectPath}/contracts`);
+
+            // 2. Run taq compile ${contractName}
+            execSync(`taq compile fa12.arl`, {cwd: `./${taqueriaProjectPath}`});
+
+            // 3. Verify that compiled michelson version has been generated
+            await checkFolderExistsWithTimeout(`./${taqueriaProjectPath}/artifacts/fa12.tz`, 25000);
+
+        } catch(error) {
+            throw new Error (`error: ${error}`);
+        }
+
+    });
+
+    test('Verify that taqueria archetype plugin can compile multiple contracts under contracts folder', async () => {
+        try {
+            // 1. Copy two contracts from data folder to /contracts folder under taqueria project
+            execSync(`cp e2e/data/fa12.arl ${taqueriaProjectPath}/contracts`);
+            execSync(`cp e2e/data/animal_tracking.arl ${taqueriaProjectPath}/contracts`);
+
+            // 2. Run taq compile ${contractName}
+            execSync(`taq compile`, {cwd: `./${taqueriaProjectPath}`});
+
+            // 3. Verify that compiled michelson version for both contracts has been generated
+            await checkFolderExistsWithTimeout(`./${taqueriaProjectPath}/artifacts/fa12.tz`, 25000);
+            await checkFolderExistsWithTimeout(`./${taqueriaProjectPath}/artifacts/animal_tracking.tz`, 25000);
+
+
+        } catch(error) {
+            throw new Error (`error: ${error}`);
+        }
+
+    });
+
+    // TODO: Currently it cannot be done until the output will be places to stdout
+    // Issue to implement the test: https://github.com/ecadlabs/taqueria/issues/373
+    // Related developer issue: https://github.com/ecadlabs/taqueria/issues/372
+    test.skip('Verify that taqueria archetype plugin will display proper message if user tries to compile contract that does not exist', async () => {
+        try {
+            // 1. Run taq compile ${contractName} for contract that does not exist
+            const stdout = execSync(`taq compile test.arl`, {cwd: `./${taqueriaProjectPath}`}).toString().trim();
+
+            // 2. Verify that output includes next messages:
+            // There was a compilation error.
+            // contracts/test.mligo: No such file or directory
+            expect(stdout).toContain("There was a compilation error.");
+            expect(stdout).toContain("contracts/test.mligo: No such file or directory");
+
+
+        } catch(error) {
+            throw new Error (`error: ${error}`);
+        }
+
+    });
+
+    test('Verify that taqueria archetype plugin requires the plugin argument when other compile plugins installed', async () => {
+        try {
+            // 1. Copy contract from data folder to taqueria project folder
+            execSync(`cp e2e/data/fa12.arl ${taqueriaProjectPath}/contracts`);
+
+            execSync('taq install @taqueria/plugin-ligo', {cwd: `./${taqueriaProjectPath}`})
+
+            // 2. Run taq compile ${contractName}
+            execSync(`taq compile fa12.arl --plugin @taqueria/plugin-archetype`, {cwd: `./${taqueriaProjectPath}`});
+
+            // 3. Verify that compiled michelson version has been generated
+            await checkFolderExistsWithTimeout(`./${taqueriaProjectPath}/artifacts/fa12.tz`, 25000);
+
+        } catch(error) {
+            throw new Error (`error: ${error}`);
+        }
+
+    });
+
+    // Remove all files from artifacts folder without removing folder itself
+    afterEach(() => {
+        try {
+            const files = fs.readdirSync(`${taqueriaProjectPath}/artifacts/`);
+            for (const file of files) {
+                fs.unlinkSync(path.join(`${taqueriaProjectPath}/artifacts/`, file));
+            }
+        } catch(error){
+            throw new Error (`error: ${error}`);
+        }
+    })
+
+    // Clean up process to remove taquified project folder
+    // Comment if need to debug
+    afterAll(() => {
+        try {
+            fs.rmdirSync(taqueriaProjectPath, { recursive: true })
+        } catch(error){
+            throw new Error (`error: ${error}`);
+        }
+    })
+
+});
