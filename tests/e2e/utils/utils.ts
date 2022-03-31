72faf3516e67a30435a03abf47f674b8d51b2065
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-03-31 13:41:57 +0000

    testing which command fails for integration tests in pipeline

--- tests/e2e/utils/utils.ts
+++ tests/e2e/utils/utils.ts
@@ -34,6 +34,10 @@ export const generateTestProject = async (projectPath: string, packageNames: str
                     console.log(cwd)
                     const cwd2 = execSync(`cd ./${projectPath} && pwd`).toString()
                     console.log(cwd2)
+                    const cwd3 = execSync(`cd ./${projectPath} && ls`).toString()
+                    console.log(cwd3)
+                    const cwd4 = execSync(`cd ./${projectPath} && taq install ../../taqueria-plugin-${packageName}`).toString()
+                    console.log(cwd4)
                     execSync(`cd ./${projectPath} && taq install ../../../taqueria-plugin-${packageName}`)
                 } else {
                     execSync(`cd ./${projectPath} && taq install @taqueria/plugin-${packageName}`)
