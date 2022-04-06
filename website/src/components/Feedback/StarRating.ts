9225a0c7bf6a8dc97ec95235b8fd743d5c88f30c
houston <houston.krohman@ecadlabs.com>
2022-04-06 13:35:10 -0700

    Remove broken implementation

---
+++ website/src/components/Feedback/StarRating.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MDBContainer, MDBRating } from 'mdbreact';
+
+const PageRating = () => {
+  return (
+    <MDBContainer>
+      <MDBRating iconRegular />
+    </MDBContainer>
+  );
+};
+
+export default PageRating;
