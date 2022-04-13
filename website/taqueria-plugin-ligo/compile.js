abd22de16e0eb9bbdefe2226e69dcf54933170e5
Gino Imbrailo <ginoimbrailo@gmail.com>
2022-04-13 11:32:00 -0700

    chore: move ligo plugin dir to website for tests

---
+++ website/taqueria-plugin-ligo/compile.js
@@ -0,0 +1,74 @@
+const {execCmd, getArch, sendAsyncErr, sendJsonRes, sendErr} = require('@taqueria/node-sdk')
+const {extname, basename, join} = require('path')
+const glob = require('fast-glob')
+
+const getContractArtifactFilename = (opts) => (sourceFile) => {
+    const outFile = basename(sourceFile, extname(sourceFile))
+    return join(opts.config.artifactsDir, `${outFile}.tz`)
+}
+
+const getInputFilename = (opts) => sourceFile => {
+    return join(opts.config.contractsDir, sourceFile)
+}
+
+const getCompileCommand = (opts, _arch) => (sourceFile) => {
+    const {projectDir} = opts
+    const inputFile = getInputFilename (opts) (sourceFile)
+    const baseCommand = `DOCKER_DEFAULT_PLATFORM=linux/amd64 docker run --rm -v \"${projectDir}\":/project -w /project ligolang/ligo:next compile contract ${inputFile}`
+    const entryPoint = opts.e ? `-e ${opts.e}` : ""
+    const syntax = opts["-s"] ? `s ${opts['s']} : ""` : ""
+    const outFile = `-o ${getContractArtifactFilename(opts)(sourceFile)}`
+    const cmd = `${baseCommand} ${entryPoint} ${syntax} ${outFile}`
+    return cmd
+}
+
+const compileContract = (opts) => (sourceFile) =>
+    getArch()
+    .then(arch => getCompileCommand(opts, arch) (sourceFile))
+    .then(execCmd)
+    .then(({stderr}) => { // How should we output warnings?
+        if (stderr.length > 0) sendErr(stderr)
+        return {
+            contract: sourceFile,
+            artifact: getContractArtifactFilename(opts) (sourceFile)
+        }
+    })
+    .catch(err => {
+        sendErr(" ")
+        sendErr(err.message.split("\n").slice(1).join("\n"))
+        return Promise.resolve({
+            contract: sourceFile,
+            artifact: "Not compiled"
+        })
+    })
+
+const compileAll = parsedArgs => {
+    // TODO: Fetch list of files from SDK
+    return glob(
+        ['**/*.ligo', '**/*.religo', '**/*.mligo', '**/*.jsligo'],
+        {cwd: parsedArgs.contractsDir, absolute: false}
+    )
+    .then(entries => entries.map(sourceFile => compileContract(parsedArgs) (sourceFile)))
+    .then(processes => processes.length > 0
+        ? processes
+        : [{contract: "None found", artifact: "N/A"}]
+    )
+    .then(promises => Promise.all(promises))
+}
+
+const compile = parsedArgs => {
+    const p = parsedArgs.sourceFile
+        ? compileContract(parsedArgs) (parsedArgs.sourceFile)
+            .then(result => [result])
+        : compileAll(parsedArgs)
+            .then(results => {
+                if (results.length === 0) sendErr("No contracts found to compile.")
+                return results
+            })
+    
+    return p
+    .then(sendJsonRes)
+    .catch(err => sendAsyncErr(err, false))
+}
+
+module.exports = compile
