e1f1798210938d7c151d0655fae77240f88de767
Michael Weichert <mweichert@gmail.com>
2022-04-18 22:29:24 -0400

    Fixes #512

--- taqueria-plugin-taquito/originate.ts
+++ taqueria-plugin-taquito/originate.ts
@@ -4,10 +4,16 @@ import {join} from 'path'
 import { TezosToolkit, OriginateParams, WalletOperationBatch } from '@taquito/taquito';
 import {readFile} from 'fs/promises'
 import { InMemorySigner, importKey } from '@taquito/signer';
-import { sendAsyncJsonRes, getCurrentEnvironmentConfig, getNetworkConfig, getSandboxAccountConfig, getInitialStorage, sendErr, getSandboxConfig, getDefaultAccount } from "@taqueria/node-sdk";
+import { sendAsyncJsonRes, getCurrentEnvironmentConfig, getNetworkConfig, getSandboxAccountConfig, getInitialStorage, sendErr, getSandboxConfig, getDefaultAccount, sendJsonRes } from "@taqueria/node-sdk";
 import { BatchWalletOperation } from "@taquito/taquito/dist/types/wallet/batch-operation";
+import { OperationContentsAndResultOrigination } from "@taquito/rpc";
 
 type Opts = SanitizedArgs & Record<string, unknown>
+
+interface ContractStorageMapping {
+    filename: string,
+    storage?: unknown
+}
 
 interface OriginationResult {
     contract: string
@@ -18,52 +24,101 @@ interface OriginationResult {
 const getContractAbspath = (contractFilename: string, parsedArgs: Opts) => 
     join(parsedArgs.artifactsDir, /\.tz$/.test(contractFilename) ? contractFilename : `${contractFilename}.tz`)
 
-const addOrigination = (parsedArgs: Opts, batch: Promise<WalletOperationBatch>) => async (contractFilename: string) => {
-    const contractAbspath = getContractAbspath(contractFilename, parsedArgs)
+const addOrigination = (parsedArgs: Opts, batch: Promise<WalletOperationBatch>) => async (mapping: ContractStorageMapping) => {
+    const contractAbspath = getContractAbspath(mapping.filename, parsedArgs)
     const contractData = await readFile(contractAbspath, "utf-8")
+    return (await batch).withOrigination({
+        code: contractData,
+        storage: mapping.storage,
+        
+    })
+}
 
-    const initialStorage = getInitialStorage(parsedArgs) (contractFilename)
-    if (initialStorage) {
-        return (await batch).withOrigination({
-            code: contractData,
-            storage: initialStorage,
-            
-        })
+const getValidContracts = async (parsedArgs: Opts) => {
+    const contracts = parsedArgs.contract
+        ? [parsedArgs.contract as string]
+        : (await glob("**/*.tz", {cwd: parsedArgs.artifactsDir})) as string[]
+    
+    return contracts.reduce(
+        (retval, filename) => {
+            const storage = getInitialStorage(parsedArgs) (filename)
+            if (!storage) sendErr(`No initial storage provided for ${filename}`)
+            return [...retval, {filename, storage}]
+        },
+        [] as ContractStorageMapping[]
+    )    
+}
+
+const mapOpToContract = async (contracts: ContractStorageMapping[], op: BatchWalletOperation, destination: string) => {
+    debugger
+    const results = await op.operationResults()
+
+    return contracts.reduce(
+        (retval, contract) => {
+
+            // If initial storage was provided for the contract
+            // then we submitted an operation to originate that contract
+            if (contract.storage) {
+                const result = results.pop() as OperationContentsAndResultOrigination
+                const address = result && result.metadata.operation_result.originated_contracts
+                    ? result.metadata.operation_result.originated_contracts.join(',')
+                    : 'Error'
+
+                return [
+                    ...retval, {
+                        contract: contract.filename,
+                        address,
+                        destination
+                    }
+                ]
+            }
+
+            return [
+                ...retval, {
+                    contract: contract.filename,
+                    address: "Error",
+                    destination
+                }
+            ]
+        },
+        [] as OriginationResult[]
+    )
+}
+
+
+const createBatch = async (parsedArgs: Opts, tezos: TezosToolkit, destination: string) => {
+    const contracts = await getValidContracts (parsedArgs)                                
+    
+    const batch = await contracts.reduce(
+        (batch, contractMapping) => 
+            contractMapping.storage
+                ? addOrigination(parsedArgs, batch) (contractMapping)
+                : batch,
+        Promise.resolve(tezos.wallet.batch())
+    )
+    
+    try {
+        debugger
+        const op = await batch.send()
+        const confirmed = await op.confirmation()
+        return await mapOpToContract(contracts, op, destination)
     }
-
-    sendErr(`No initial storage value found for ${contractFilename}`)
-    return await batch
+    catch (err) {
+        return undefined
+    }
 }
 
 const originateToNetworks = (parsedArgs: Opts, currentEnv: EnvironmentConfig) =>
     currentEnv.networks
         ? currentEnv.networks.reduce(
             (retval, networkName) => {
-                debugger
                 const network = getNetworkConfig (parsedArgs) (networkName)
                 if (network.rpcUrl) {
                     if (network.faucet) {
                         const result = (async () => {
                             const tezos = new TezosToolkit(network.rpcUrl as string)
                             await importKey(tezos, network.faucet.email, network.faucet.password, network.faucet.mnemonic.join(' '), network.faucet.activation_code)
-    
-                            const contracts = parsedArgs.contract
-                                ? [parsedArgs.contract as string]
-                                : (await glob("**/*.tz", {cwd: parsedArgs.artifactsDir})) as string[]
-    
-                            const batch = await contracts.reduce(
-                                (batch, contractFilename) => addOrigination(parsedArgs, batch) (contractFilename),
-                                Promise.resolve(tezos.wallet.batch())
-                            )
-                            
-                            try {
-                                const op = await batch.send()
-                                const confirmed = await op.confirmation()
-                                return op
-                            }
-                            catch (err) {
-                                return undefined
-                            }
+                            return await createBatch(parsedArgs, tezos, networkName)
                         })()
 
                         return [...retval, result]
@@ -73,7 +128,7 @@ const originateToNetworks = (parsedArgs: Opts, currentEnv: EnvironmentConfig) =>
                 else sendErr(`Network "${networkName} is missing an RPC url"`)
                 return retval
             },
-            [] as  Promise<BatchWalletOperation|undefined>[]
+            [] as  Promise<OriginationResult[]|undefined>[]
         )
         : []
 
@@ -81,7 +136,6 @@ const originateToSandboxes = (parsedArgs: Opts, currentEnv: EnvironmentConfig) =
     currentEnv.sandboxes
         ? currentEnv.sandboxes.reduce(
             (retval, sandboxName) => {
-                debugger
                 const sandbox = getSandboxConfig (parsedArgs) (sandboxName)
                 if (sandbox.rpcUrl) {
                     const secretKey = getDefaultAccount(parsedArgs) (sandboxName) ?.keys?.secretKey
@@ -91,24 +145,7 @@ const originateToSandboxes = (parsedArgs: Opts, currentEnv: EnvironmentConfig) =
                             tezos.setProvider({
                                 signer: new InMemorySigner(secretKey.replace(/^unencrypted:/, '')),
                             });
-    
-                            const contracts = parsedArgs.contract
-                                ? [parsedArgs.contract as string]
-                                : (await glob("**/*.tz", {cwd: parsedArgs.artifactsDir})) as string[]
-    
-                            const batch = await contracts.reduce(
-                                (batch, contractFilename) => addOrigination(parsedArgs, batch) (contractFilename),
-                                Promise.resolve(tezos.wallet.batch())
-                            )
-                            try {
-                                const op = await batch.send()
-                                debugger
-                                const confirmed = await op.confirmation()
-                                return op
-                            }
-                            catch (err) {
-                                return undefined
-                            }
+                            return await createBatch(parsedArgs, tezos, sandboxName)
                         })()
                         
                         return [...retval, result]
@@ -119,7 +156,7 @@ const originateToSandboxes = (parsedArgs: Opts, currentEnv: EnvironmentConfig) =
 
                 return retval
             },
-            [] as Promise<BatchWalletOperation|undefined>[]
+            [] as Promise<OriginationResult[]|undefined>[]
         )
         : []
     
@@ -141,7 +178,16 @@ export const originate = <T>(parsedArgs: Opts): LikeAPromise<PluginResponse, Fai
         ...originateToSandboxes(parsedArgs, env)
     ]
 
-    return Promise.all(jobs).then(console.log)
+    return Promise.all(jobs)
+        .then(jobs => jobs.reduce(
+            (retval, originations) => {
+                return originations
+                    ? [...retval as OriginationResult[], ...originations]
+                    : retval
+            },
+            []
+        ))
+        .then(results => results && results.length > 0 ? sendJsonRes(results): sendErr(`No contract found`))
 }
 
 export default originate
