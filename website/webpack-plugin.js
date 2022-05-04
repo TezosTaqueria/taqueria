39b5ca2dd851cd975d6c736a39fba4f03d2034af
Claude Barde <lotolosenc.oc@gmail.com>
2022-05-04 11:10:34 +0100

    Modified webpack config to use __dirname

--- website/webpack-plugin.js
+++ website/webpack-plugin.js
@@ -6,6 +6,9 @@ const webpackPlugin = (context, opts) => {
       // will be merged into the final config using webpack-merge;
       // If the returned value is a function, it will receive the config as the 1st argument and an isServer flag as the 2nd argument.
       return {
+        node: {
+          __dirname: true
+        },
         resolve: {
           fallback: {
             stream: require.resolve("stream-browserify"),
