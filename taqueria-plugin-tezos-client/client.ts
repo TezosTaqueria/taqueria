2bfd04d5f841420a28556d787371f3b91c4201a3
Michael Weichert <mweichert@gmail.com>
2022-05-05 14:00:53 -0400

    Updated tezos-client plugin to use zod types

--- taqueria-plugin-tezos-client/client.ts
+++ taqueria-plugin-tezos-client/client.ts
@@ -1,66 +1,58 @@
-import {Sandbox as theSandbox, execCmd, getArch, sendAsyncErr, sendErr, sendRes, sendAsyncRes, sendJsonRes, sendAsyncJsonRes} from '@taqueria/node-sdk'
-import type {SanitizedArgs, Config, Attributes, SandboxConfig, Failure, LikeAPromise, Sandbox, AccountDetails, StdIO, PluginResponse} from "@taqueria/node-sdk/types"
+import {execCmd, sendAsyncErr, sendErr, sendJsonRes, sendAsyncJsonRes, getSandboxConfig, getCurrentEnvironmentConfig} from '@taqueria/node-sdk'
+import type {RequestArgs, LoadedConfig, SandboxConfig, Environment, Failure, LikeAPromise, PluginResponse} from "@taqueria/node-sdk/types"
 import type {ExecException} from 'child_process'
 import retry from 'async-retry'
 import {join} from 'path'
 import glob from 'fast-glob'
 
-type Opts = SanitizedArgs & {sandboxName?: string}
+interface Opts extends RequestArgs.ProxyRequestArgs {
+    sandboxName?: string
+    sourceFile?: string
+    sourceFiles?: string
+    storage?: string
+    input?: string
+}
 
 const isSandboxRunning = (sandboxName: string) =>
     execCmd(`docker ps --filter name=${sandboxName} | grep -w ${sandboxName}`)
     .then(_ => true)
     .catch(_ => false)
 
-const getDefaultSandboxName = (config: Config) : string => {
-    const defaultEnv = config.environment.default
-    const sandbox = config.environment[defaultEnv].sandboxes[0]
-    return sandbox
-}
-
-const getSandbox = ({sandboxName, config}: Opts) => {
-    if (!sandboxName) sandboxName = getDefaultSandboxName(config)
-    if (config.sandbox[sandboxName]) {
-        const sandboxConfig : SandboxConfig = config.sandbox[sandboxName]
-
-        // This should probably go into the SDK or protocol
-        if (sandboxName && sandboxConfig.label && sandboxConfig.protocol && sandboxConfig.rpcUrl) {
-            const {label, protocol, rpcUrl, plugin, accounts} = sandboxConfig
-            const unvalidated = {
-                name: sandboxName,
-                label,
-                protocol,
-                rpcUrl,
-                plugin,
-                accounts
-            }
-            return theSandbox.create(unvalidated)
-        }
+const getDefaultSandboxName = (config: LoadedConfig.t) => {
+    const defaultEnv = config.environment?.default as string | undefined
+    if (defaultEnv) {
+        const env = config.environment || {}
+        const envConfig = env[defaultEnv] as Environment.t
+        const sandbox = envConfig.sandboxes[0]
+        return sandbox
     }
-    // TODO: Should we throw instead?
     return undefined
 }
 
-const doesUseFlextesa = (sandbox: Sandbox) => !sandbox.plugin || sandbox.plugin === 'flextesa'
+const getContainerName = (sandboxName: string, parsedArgs: Opts) => {
+    return `taqueria-${parsedArgs.env}-${sandboxName}`
+}
+
+const doesUseFlextesa = (sandbox: SandboxConfig.t) => !sandbox.plugin || sandbox.plugin === 'flextesa'
 
 const getInputFilenameFromContractDir = (opts: Opts, sourceFile: string) => join("/project", opts.config.contractsDir, sourceFile)
 
 const getInputFilenameFromArtifactsDir = (opts: Opts, sourceFile: string) => join("/project", opts.config.artifactsDir, sourceFile)
 
-const getCheckFileExistenceCommand = (sandbox: Sandbox, sourcePath: string) => `docker exec ${sandbox.name} ls ${sourcePath}`
+const getCheckFileExistenceCommand = (sandboxName: string, sandbox: SandboxConfig.t, sourcePath: string, opts: Opts) => `docker exec ${getContainerName(sandboxName, opts)} ls ${sourcePath}`
 
 //////////// Typecheck task ////////////
 
-const getTypecheckCommand = (sandbox: Sandbox, sourcePath: string) => `docker exec ${sandbox.name} tezos-client typecheck script ${sourcePath}`
+const getTypecheckCommand = (sandboxName: string, sandbox: SandboxConfig.t, sourcePath: string, opts: Opts) => `docker exec ${getContainerName(sandboxName, opts)} tezos-client typecheck script ${sourcePath}`
 
-const typecheckContract = (opts: Opts, sandbox: Sandbox) => async (sourceFile: string) : Promise<{contract: string, result: string}> => {
+const typecheckContract = (opts: Opts, sandboxName: string, sandbox: SandboxConfig.t) => async (sourceFile: string) : Promise<{contract: string, result: string}> => {
     let sourcePath: string = getInputFilenameFromArtifactsDir(opts, sourceFile)
-    sourcePath = await execCmd(getCheckFileExistenceCommand(sandbox, sourcePath))
+    sourcePath = await execCmd(getCheckFileExistenceCommand(sandboxName, sandbox, sourcePath, opts))
                        .then(() => sourcePath)
                        .catch(() => getInputFilenameFromContractDir(opts, sourceFile))
 
     const typecheckContractHelper = () => {
-        return execCmd(getTypecheckCommand(sandbox, sourcePath))
+        return execCmd(getTypecheckCommand(sandboxName, sandbox, sourcePath, opts))
         .then(async ({stderr}) => { // How should we output warnings?
             if (stderr.length > 0) sendErr(`\n${stderr}`)
             return {
@@ -78,7 +70,7 @@ const typecheckContract = (opts: Opts, sandbox: Sandbox) => async (sourceFile: s
         })
     }
 
-    return execCmd(getCheckFileExistenceCommand(sandbox, sourcePath))
+    return execCmd(getCheckFileExistenceCommand(sandboxName, sandbox, sourcePath, opts))
     .then(typecheckContractHelper)
     .catch(err => {
         sendErr(" ")
@@ -90,32 +82,32 @@ const typecheckContract = (opts: Opts, sandbox: Sandbox) => async (sourceFile: s
     })
 }
 
-const typecheckMultiple = (opts: Opts, sandbox: Sandbox) => (sourceFiles: string[]) : Promise<{contract: string, result: string}[]> => {
-    return Promise.all(sourceFiles.map(typecheckContract(opts, sandbox)))
+const typecheckMultiple = (opts: Opts, sandboxName: string, sandbox: SandboxConfig.t) => (sourceFiles: string[]) : Promise<{contract: string, result: string}[]> => {
+    return Promise.all(sourceFiles.map(typecheckContract(opts, sandboxName, sandbox)))
 }
 
-const typecheckAll = (opts: Opts, sandbox: Sandbox): Promise<{contract: string, result: string}[]> => {
+const typecheckAll = (opts: Opts, sandboxName: string, sandbox: SandboxConfig.t): Promise<{contract: string, result: string}[]> => {
     // TODO: Fetch list of files from SDK
-    return glob(['**/*.tz'], {cwd: opts.artifactsDir, absolute: false})
+    return glob(['**/*.tz'], {cwd: opts.config.artifactsDir, absolute: false})
     .then(entriesFromArtifactsDir =>
-        glob(['**/*.tz'], {cwd: opts.contractsDir, absolute: false})
+        glob(['**/*.tz'], {cwd: opts.config.contractsDir, absolute: false})
         .then(entriesFromContractsDir =>
-            [...new Set([...entriesFromArtifactsDir, ...entriesFromContractsDir])].map(typecheckContract(opts, sandbox))
+            [...new Set([...entriesFromArtifactsDir, ...entriesFromContractsDir])].map(typecheckContract(opts, sandboxName, sandbox))
         )
     )
     .then(promises => Promise.all(promises))
 }
 
-const typecheck = <T>(parsedArgs: Opts, sandbox: Sandbox): LikeAPromise<PluginResponse, Failure<T>> => {
+const typecheck = <T>(parsedArgs: Opts, sandboxName: string, sandbox: SandboxConfig.t): LikeAPromise<PluginResponse, Failure<T>> => {
     const sourceFiles = (parsedArgs.sourceFiles as string).split(',')
     let p;
     if (parsedArgs.sourceFiles) {
         if (sourceFiles.length == 1)
-            p = typecheckContract (parsedArgs, sandbox) (parsedArgs.sourceFiles as string).then(data => [data])
+            p = typecheckContract (parsedArgs, sandboxName, sandbox) (parsedArgs.sourceFiles as string).then(data => [data])
         else
-            p = typecheckMultiple (parsedArgs, sandbox) (sourceFiles)
+            p = typecheckMultiple (parsedArgs, sandboxName, sandbox) (sourceFiles)
     } else {
-        p = typecheckAll (parsedArgs, sandbox)
+        p = typecheckAll (parsedArgs, sandboxName, sandbox)
             .then(results => {
                 if (results.length === 0) sendErr("No contracts found to compile.")
                 return results
@@ -124,27 +116,31 @@ const typecheck = <T>(parsedArgs: Opts, sandbox: Sandbox): LikeAPromise<PluginRe
     return p.then(sendAsyncJsonRes)
 }
 
-const typecheckTask = async <T>(parsedArgs: Opts) : LikeAPromise<void, Failure<T>> => {
-    const sandbox = getSandbox(parsedArgs)
-    if (sandbox) {
-        if (doesUseFlextesa(sandbox)) {
-            return await isSandboxRunning(sandbox.name.value)
-            ? typecheck(parsedArgs, sandbox).then(sendJsonRes)
-            : sendAsyncErr(`The ${sandbox.name} sandbox is not running.`)
+const typecheckTask = async <T>(parsedArgs: Opts) : Promise<void> => {
+    const sandboxName = parsedArgs.sandboxName ?? getDefaultSandboxName(parsedArgs.config)
+    if (sandboxName) {
+        const sandbox = getSandboxConfig(parsedArgs) (sandboxName)
+        if (sandbox) {
+            if (doesUseFlextesa(sandbox)) {
+                return await isSandboxRunning(sandboxName)
+                ? typecheck(parsedArgs, sandboxName, sandbox).then(sendJsonRes)
+                : sendAsyncErr(`The ${sandboxName} sandbox is not running.`)
+            }
+            return sendAsyncErr(`Cannot start ${sandboxName} as its configured to use the ${sandbox.plugin} plugin.`)
         }
-        return sendAsyncErr(`Cannot start ${sandbox.label} as its configured to use the ${sandbox.plugin} plugin.`)
+        return sendAsyncErr(`There is no sandbox configuration with the name ${sandboxName}.`)
     }
-    return sendAsyncErr(`There is no sandbox configuration with the name ${parsedArgs.sandboxName}.`)
+    else return sendAsyncErr("No sandbox specified or found in your .taq/config.json file")
 }
 
 //////////// Simulate task ////////////
 
 const getStorageFromConfig = (opts: Opts, sourceFile: string) => {
-    const config = opts.config
-    const defaultEnv = config.environment.default
-    const storages = config.environment[defaultEnv].storage
-    const storageValue = storages[sourceFile]
-    return storageValue
+    const envConfig = getCurrentEnvironmentConfig(opts)
+    if (envConfig && envConfig.storage) {
+        return envConfig.storage[sourceFile]
+    }
+    return undefined
 }
 
 // This is needed mostly due to the fact that execCmd() wraps the command in double quotes
@@ -158,7 +154,7 @@ const preprocessString = (value: string) : string => {
     return value
 }
 
-const getSimulateCommand = (opts: Opts, sandbox: Sandbox, sourceFile: string, sourcePath: string) => {
+const getSimulateCommand = (opts: Opts, sandboxName: string, sandbox: SandboxConfig.t, sourceFile: string, sourcePath: string) => {
     let storage
     if (opts.storage) {
         storage = opts.storage
@@ -172,7 +168,7 @@ const getSimulateCommand = (opts: Opts, sandbox: Sandbox, sourceFile: string, so
     storage = preprocessString(storage)
     input = preprocessString(input)
 
-    const cmd = `docker exec ${sandbox.name} tezos-client run script ${sourcePath} on storage \'${storage}\' and input \'${input}\'`
+    const cmd = `docker exec ${sandboxName} tezos-client run script ${sourcePath} on storage \'${storage}\' and input \'${input}\'`
     return cmd
 }
 
@@ -180,15 +176,15 @@ const trimTezosClientMenuIfPresent = (msg: string) : string => {
     return msg.replace(/Usage:(.|\n)+/, '')
 }
 
-const simulateContract = (opts: Opts, sandbox: Sandbox) => async (sourceFile: string) : Promise<{contract: string, result: string}> => {
+const simulateContract = (opts: Opts, sandboxName: string, sandbox: SandboxConfig.t) => async (sourceFile: string) : Promise<{contract: string, result: string}> => {
     let sourcePath: string = getInputFilenameFromArtifactsDir(opts, sourceFile)
-    sourcePath = await execCmd(getCheckFileExistenceCommand(sandbox, sourcePath))
+    sourcePath = await execCmd(getCheckFileExistenceCommand(sandboxName, sandbox, sourcePath, opts))
                        .then(() => sourcePath)
                        .catch(() => getInputFilenameFromContractDir(opts, sourceFile))
 
     const simulateContractHelper = () => {
         try {
-            const cmd = getSimulateCommand(opts, sandbox, sourceFile, sourcePath)
+            const cmd = getSimulateCommand(opts, sandboxName, sandbox, sourceFile, sourcePath)
             return execCmd(cmd)
             .then(async ({stdout, stderr}) => { // How should we output warnings?
                 if (stderr.length > 0) sendErr(`\n${stderr}`)
@@ -215,7 +211,7 @@ const simulateContract = (opts: Opts, sandbox: Sandbox) => async (sourceFile: st
         }
     }
 
-    return execCmd(getCheckFileExistenceCommand(sandbox, sourcePath))
+    return execCmd(getCheckFileExistenceCommand(sandboxName, sandbox, sourcePath, opts))
     .then(simulateContractHelper)
     .catch(err => {
         sendErr(" ")
@@ -227,26 +223,30 @@ const simulateContract = (opts: Opts, sandbox: Sandbox) => async (sourceFile: st
     })
 }
 
-const simulate = <T>(parsedArgs: Opts, sandbox: Sandbox): LikeAPromise<PluginResponse, Failure<T>> => {
+const simulate = <T>(parsedArgs: Opts, sandboxName: string, sandbox: SandboxConfig.t): LikeAPromise<PluginResponse, Failure<T>> => {
     if (parsedArgs.sourceFile) {
-        return simulateContract (parsedArgs, sandbox) (parsedArgs.sourceFile as string)
+        return simulateContract (parsedArgs, sandboxName, sandbox) (parsedArgs.sourceFile as string)
                .then(data => [data])
                .then(sendAsyncJsonRes)
     }
     return sendAsyncErr(`Please specify a contract. E.g. taq simulate local <contractName> ...`)
 }
 
-const simulateTask = async <T>(parsedArgs: Opts) : LikeAPromise<void, Failure<T>> => {
-    const sandbox = getSandbox(parsedArgs)
-    if (sandbox) {
-        if (doesUseFlextesa(sandbox)) {
-            return await isSandboxRunning(sandbox.name.value)
-            ? simulate(parsedArgs, sandbox).then(sendJsonRes)
-            : sendAsyncErr(`The ${sandbox.name} sandbox is not running.`)
+const simulateTask = async <T>(parsedArgs: Opts) : Promise<void> => {
+    const sandboxName = parsedArgs.sandboxName ?? getDefaultSandboxName(parsedArgs.config)
+    if (sandboxName) {
+        const sandbox = getSandboxConfig(parsedArgs) (sandboxName)
+        if (sandbox) {
+            if (doesUseFlextesa(sandbox)) {
+                return await isSandboxRunning(sandboxName)
+                ? simulate(parsedArgs, sandboxName, sandbox).then(sendJsonRes)
+                : sendAsyncErr(`The ${sandboxName} sandbox is not running.`)
+            }
+            return sendAsyncErr(`Cannot start ${sandboxName} as its configured to use the ${sandbox.plugin} plugin.`)
         }
-        return sendAsyncErr(`Cannot start ${sandbox.label} as its configured to use the ${sandbox.plugin} plugin.`)
+        return sendAsyncErr(`There is no sandbox configuration with the name ${sandboxName}.`)
     }
-    return sendAsyncErr(`There is no sandbox configuration with the name ${parsedArgs.sandboxName}.`)
+    else return sendAsyncErr("No sandbox specified or found in your .taq/config.json file")
 }
 
 export const client = <T>(parsedArgs: Opts) : LikeAPromise<void, Failure<T>> => {
