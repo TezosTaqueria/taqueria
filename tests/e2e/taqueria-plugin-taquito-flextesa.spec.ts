29cba5e4505361367dc37287f9d48a620aa057b1
Michael Weichert <mweichert@gmail.com>
2022-05-27 14:30:16 -0400

    Fixed e2e tests

--- tests/e2e/taqueria-plugin-taquito-flextesa.spec.ts
+++ tests/e2e/taqueria-plugin-taquito-flextesa.spec.ts
@@ -14,37 +14,32 @@ describe("E2E Testing for taqueria taquito plugin",  () => {
     beforeAll(async () => {
         await generateTestProject(taqueriaProjectPath, ["taquito", "flextesa"]);
         await exec(`cp e2e/data/config-taquito-flextesa-local-sandbox-test-environment.json ${taqueriaProjectPath}/.taq/config.json`);
+        await exec(`cp e2e/data/hello-tacos.tz ${taqueriaProjectPath}/artifacts/`)
+        await exec(`cp e2e/data/increment.tz ${taqueriaProjectPath}/artifacts/`)
+        await exec(`taq stop sandbox ${dockerName}`, {cwd: `./${taqueriaProjectPath}`})
         await exec(`taq start sandbox ${dockerName}`, {cwd: `./${taqueriaProjectPath}`})
     })
 
     // TODO: Consider in future to use keygen service to update account balance programmatically
     // https://github.com/ecadlabs/taqueria/issues/378
     test('Verify that taqueria taquito plugin can deploy one contract using deploy command', async () => {
-        try {
-            environment = "development";
+        environment = "development";
 
-            // 1. Copy config.json and michelson contract from data folder to artifacts folder under taqueria project
-            await exec(`cp e2e/data/hello-tacos.tz ${taqueriaProjectPath}/artifacts/`);
+        // 1. Run taq deploy on a selected test network described in "test" environment
+        const deployCommand = await exec(`taq deploy -e ${environment}`, {cwd: `./${taqueriaProjectPath}`})
+        const deployResponse = deployCommand.stdout.trim().split(/\r?\n/)[3]
 
-            // 2. Run taq deploy on a selected test network described in "test" environment
-            const deployCommand = await exec(`taq deploy -e ${environment}`, {cwd: `./${taqueriaProjectPath}`})
-            const deployResponse = deployCommand.stdout.trim().split(/\r?\n/)[3]
+        // 2. Verify that contract has been originated on the network
+        expect(deployResponse).toContain("hello-tacos.tz");
+        expect(deployResponse).toContain(dockerName);
+        const contractHash = deployResponse.split("│")[2].trim();
 
-            // 3. Verify that contract has been originated on the network
-            expect(deployResponse).toContain("hello-tacos.tz");
-            expect(deployResponse).toContain(dockerName);
-            const contractHash = deployResponse.split("│")[2].trim();
+        expect(contractHash).toMatch(contractRegex);
 
-            expect(contractHash).toMatch(contractRegex);
-
-            // 4. Verify that contract has been originated to the network and contains storage
-            const contractFromSandbox = await exec(`curl http://localhost:20000/chains/main/blocks/head/context/contracts/${contractHash}`)
-            expect(contractFromSandbox.stdout).toContain('"balance":"0"')
-            expect(contractFromSandbox.stdout).toContain('"storage":{"int":"1"}')
-
-        } catch(error) {
-            throw new Error (`error: ${error}`);
-        }
+        // 3. Verify that contract has been originated to the network and contains storage
+        const contractFromSandbox = await exec(`curl http://localhost:20000/chains/main/blocks/head/context/contracts/${contractHash}`)
+        expect(contractFromSandbox.stdout).toContain('"balance":"0"')
+        expect(contractFromSandbox.stdout).toContain('"storage":{"int":"1"}')
     });
 
     // TODO: Consider in future to use keygen service to update account balance programmatically
@@ -53,22 +48,18 @@ describe("E2E Testing for taqueria taquito plugin",  () => {
         try {
             environment = "development"
 
-            // 1. Copy config.json and michelson contract from data folder to artifacts folder under taqueria project
-            await exec(`cp e2e/data/hello-tacos.tz ${taqueriaProjectPath}/artifacts/`)
-            await exec(`cp e2e/data/increment.tz ${taqueriaProjectPath}/artifacts/`)
-
-            // 2. Run taq deploy ${contractName} on a selected test network described in "test" environment
+            // 1. Run taq deploy ${contractName} on a selected test network described in "test" environment
             const deployCommand = await exec(`taq deploy hello-tacos.tz -e ${environment}`, {cwd: `./${taqueriaProjectPath}`})
             const deployResponse = deployCommand.stdout.trim().split(/\r?\n/)[3]
 
-            // 3. Get the KT address from the output
+            // 2. Get the KT address from the output
             expect(deployResponse).toContain("hello-tacos.tz");
             expect(deployResponse).toContain(dockerName);
             const contractHash = deployResponse.split("│")[2].trim();
 
             expect(contractHash).toMatch(contractRegex);
 
-            // 4. Verify that contract has been originated to the network
+            // 3. Verify that contract has been originated to the network
             const contractFromSandbox = await exec(`curl http://localhost:20000/chains/main/blocks/head/context/contracts/${contractHash}`)
             expect(contractFromSandbox.stdout).toContain('"balance":"0"')
             expect(contractFromSandbox.stdout).toContain('"storage":{"int":"1"}')
@@ -85,10 +76,6 @@ describe("E2E Testing for taqueria taquito plugin",  () => {
         environment = "development";
         const contract1 = 'hello-tacos.tz'
         const contract2 = 'increment.tz'
-
-        // 1. Copy two michelson contracts from data folder to artifacts folder under taqueria project
-        await exec(`cp e2e/data/hello-tacos.tz ${taqueriaProjectPath}/artifacts/`)
-        await exec(`cp e2e/data/increment.tz ${taqueriaProjectPath}/artifacts/`)
 
         const deployCommand = await exec(`taq originate -e ${environment}`, {cwd: `./${taqueriaProjectPath}`})
         const deployResponse = deployCommand.stdout.trim()
@@ -117,46 +104,35 @@ describe("E2E Testing for taqueria taquito plugin",  () => {
 
         // 4. Verify that contracts have been originated to the network
         const contractOneFromSandbox = await exec(`curl http://localhost:20000/chains/main/blocks/head/context/contracts/${contractOneHash}`)
-        expect(contractOneFromSandbox.stdout).toContain('nat %numTacosToConsume')
+        expect(contractOneFromSandbox.stdout).toContain('%numTacosToConsume')
         expect(contractOneFromSandbox.stdout).toContain('"storage":{"int":"1"}')
 
         const contractTwoFromSandbox = await exec(`curl http://localhost:20000/chains/main/blocks/head/context/contracts/${contractTwoHash}`)
-        expect(contractTwoFromSandbox.stdout).toContain('int %increment')
+        expect(contractTwoFromSandbox.stdout).toContain('%increment')
         expect(contractTwoFromSandbox.stdout).toContain('"storage":{"int":"2"}')
 
         // 5. Verify that contracts originated on the network have different addresses
         expect(contractOneHash).not.toEqual(contractTwoHash);
     });
 
-    // Remove all files from artifacts folder without removing folder itself
-    afterEach( async () => {
-        try {
-            const files = await fsPromises.readdir(`${taqueriaProjectPath}/artifacts/`);
-            for (const file of files) {
-                await fsPromises.rm(`${taqueriaProjectPath}/artifacts/${file}`);
-            }
-        } catch(error){
-            throw new Error (`error: ${error}`);
-        }
-    })
-
     // Clean up process to remove taquified project folder
     // Comment if need to debug
     afterAll( async () => {
-        try {
-            await fsPromises.rm(taqueriaProjectPath, { recursive: true })
+        await fsPromises.rm(taqueriaProjectPath, { recursive: true })
 
-            const dockerContainer = await getContainerName(dockerName);
-            if(dockerContainer !== ""){
-                await exec(`docker stop ${dockerName}`);
+        const dockerContainer = await getContainerName(dockerName);
+        if(dockerContainer !== ""){
+            try {
+                await exec(`docker stop ${dockerContainer}`);
             }
+            catch {
+                // Ignore
+            }
+        }
 
-            const dockerListStdout = await exec("docker ps")
-            if(dockerListStdout.stdout.includes(dockerName)){
-                throw new Error("Container was not stopped properly");
-            }
-        } catch(error){
-            throw new Error (`error: ${error}`);
+        const dockerListStdout = await exec("docker ps")
+        if(dockerListStdout.stdout.includes(dockerContainer)){
+            throw new Error("Container was not stopped properly");
         }
     })
 });
