fa0129a12558919ce8c64dd67516a37691cbcb9f
houston <houston.krohman@ecadlabs.com>
2022-06-01 19:15:33 -0700

    Format repo

--- tests/e2e/taqueria-cli-scaffolding-functional.spec.ts
+++ tests/e2e/taqueria-cli-scaffolding-functional.spec.ts
@@ -11,16 +11,7 @@ describe('E2E Testing for taqueria scaffolding initialization,', () => {
 		await exec(`cd ${scaffoldDirName} && npm run setup`);
 	});
 
-	// TODO: Activate other tests after 620-operations branch has
-	// been merged and released.
-	//
-	// Why? The version of the plugins used by the scaffold are offical
-	// versions published on the NPM registry. The pluginInfo that is
-	// returned from those versions of plugins doesn't match the shape/type
-	// expected in this branch, and therefore fail.
-	//
-	// See issue: https://github.com/ecadlabs/taqueria/issues/736
-	test.only('Verify that scaffold project can be set up', async () => {
+	test('Verify that scaffold project can be set up', async () => {
 		const appContents = await exec(`ls ${scaffoldDirName}/app`);
 		const taqContents = await exec(`ls ${scaffoldDirName}/taqueria`);
 		expect(appContents.stdout).toContain('node_modules');
@@ -34,19 +25,11 @@ describe('E2E Testing for taqueria scaffolding initialization,', () => {
 		expect(taqContents.stdout).toContain('example.tz');
 	});
 
-    test('Verify that scaffold project can be set up', async () => {
-        const appContents = await exec(`ls ${scaffoldDirName}/app`)
-        const taqContents = await exec(`ls ${scaffoldDirName}/taqueria`)
-        expect(appContents.stdout).toContain('node_modules')
-        expect(taqContents.stdout).toContain('node_modules')
-        expect(taqContents.stdout).toContain('contracts')
-        expect(taqContents.stdout).toContain('artifacts')
-    })
-    test('Verify that scaffold project can build taqueria', async () => {
-        await exec(`cd ${scaffoldDirName} && npm run build:taqueria`)
-        const taqContents = await exec(`ls ${scaffoldDirName}/taqueria/artifacts`)
-        expect(taqContents.stdout).toContain('example.tz')
-    })
+	test('Verify that scaffold project can start and stop taqueria locally', async () => {
+		const startResults = await exec(`cd ${scaffoldDirName} && npm run start:taqueria:local`);
+		expect(startResults.stdout).toContain('Processing /example.tz...');
+		expect(startResults.stdout).toContain('example.tz: Types generated');
+		expect(startResults.stdout).toContain('Started local.');
 
 		const stopResults = await exec(`cd ${scaffoldDirName}/taqueria && npm run stop:local`);
 		expect(stopResults.stdout).toContain('Stopped local.');
