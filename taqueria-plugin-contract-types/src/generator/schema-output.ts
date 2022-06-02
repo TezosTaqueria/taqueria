a97edd887885218f7d49023d6dc5d99408307824
Michael Weichert <mweichert@gmail.com>
2022-06-02 14:05:19 -0400

    Format files using dprint

--- taqueria-plugin-contract-types/src/generator/schema-output.ts
+++ taqueria-plugin-contract-types/src/generator/schema-output.ts
@@ -1,58 +1,59 @@
 import { GenerateApiError } from './common';
-import { TypedMethod, TypedVar, TypedType, TypedStorage } from './contract-parser';
+import { TypedMethod, TypedStorage, TypedType, TypedVar } from './contract-parser';
 
 type SchemaObjectType = { [name: string]: SchemaType };
 type SchemaType = string | SchemaType[] | SchemaObjectType;
 type SchemaMethods = {
-    [name: string]: {
-        params: SchemaType;
-    };
+	[name: string]: {
+		params: SchemaType;
+	};
 };
 export type SchemaOutput = {
-    methods: SchemaMethods;
-    storage: SchemaType;
+	methods: SchemaMethods;
+	storage: SchemaType;
 };
 
 export const toSchema = (methods: TypedMethod[], storage: TypedStorage): SchemaOutput => {
+	const getSchemaObjectType = (vars: TypedVar[]) => {
+		// console.log('getSchemaObjectType', { vars });
 
-    const getSchemaObjectType = (vars: TypedVar[]) => {
-        // console.log('getSchemaObjectType', { vars });
+		if (vars.some(x => !x)) {
+			throw new GenerateApiError(`getSchemaObjectType has null vars`, { vars });
+		}
 
-        if (vars.some(x => !x)) {
-            throw new GenerateApiError(`getSchemaObjectType has null vars`, { vars });
-        }
+		return vars.reduce((out, x, i) => {
+			out[x.name ?? i] = getSchemaType(x.type);
+			return out;
+		}, {} as SchemaObjectType);
+	};
 
-        return vars.reduce((out, x, i) => {
-            out[x.name ?? i] = getSchemaType(x.type);
-            return out;
-        }, {} as SchemaObjectType);
-    };
+	const getSchemaType = (t: TypedType): SchemaType => {
+		// console.log('getSchemaType', { t });
 
-    const getSchemaType = (t: TypedType): SchemaType => {
-        // console.log('getSchemaType', { t });
+		return (t.kind === `value` && t.value ? t.value : null)
+			?? (t.kind === `array` && t.array ? [getSchemaType(t.array.item)] : null)
+			?? (t.kind === `map` && t.map ? [`map`, getSchemaType(t.map.key), getSchemaType(t.map.value)] : null)
+			?? (t.kind === `object` && t.fields ? getSchemaObjectType(t.fields) : null)
+			?? (t.kind === `unit` ? `unit` : null)
+			?? (t.kind === `never` ? `never` : null)
+			?? `${t.raw as unknown as string}`;
+	};
 
-        return (t.kind === `value` && t.value ? t.value : null)
-            ?? (t.kind === `array` && t.array ? [getSchemaType(t.array.item)] : null)
-            ?? (t.kind === `map` && t.map ? [`map`, getSchemaType(t.map.key), getSchemaType(t.map.value)] : null)
-            ?? (t.kind === `object` && t.fields ? getSchemaObjectType(t.fields) : null)
-            ?? (t.kind === `unit` ? `unit` : null)
-            ?? (t.kind === `never` ? `never` : null)
-            ?? `${t.raw as unknown as string}`;
-    };
+	const schemaMethods = methods.reduce((out, x) => {
+		// console.log('schemaMethods', { x });
 
-    const schemaMethods = methods.reduce((out, x) => {
-        // console.log('schemaMethods', { x });
+		out[x.name] = {
+			params: x.args.length === 1 && !x.args[0].name
+				? getSchemaType(x.args[0].type)
+				: getSchemaObjectType(x.args ?? []),
+		};
+		return out;
+	}, {} as SchemaMethods);
 
-        out[x.name] = {
-            params: x.args.length === 1 && !x.args[0].name ? getSchemaType(x.args[0].type) : getSchemaObjectType(x.args ?? []),
-        };
-        return out;
-    }, {} as SchemaMethods);
+	const schemaStorage = getSchemaType(storage.storage);
 
-    const schemaStorage = getSchemaType(storage.storage);
-
-    return {
-        methods: schemaMethods,
-        storage: schemaStorage,
-    };
+	return {
+		methods: schemaMethods,
+		storage: schemaStorage,
+	};
 };
--- taqueria-plugin-taquito/index.ts
+++ taqueria-plugin-taquito/index.ts
@@ -1,21 +1,20 @@
-import {Plugin, Task} from '@taqueria/node-sdk'
-import originate from './originate'
+import { Plugin, Task } from '@taqueria/node-sdk';
+import originate from './originate';
 
 Plugin.create(_i18n => ({
-    alias: "taquito",
-    schema: "1.0",
-    version: "0.1",
-    tasks: [
-        Task.create({
-            task: "deploy",
-            command: "deploy [contract]",
-            description: "Deploy a smart contract to a particular environment",
-            options: [
-            ],
-            aliases: ["originate"],
-            handler: "proxy",
-            encoding: "application/json"
-        }),
-    ],
-    proxy: originate
-}), process.argv)
+	alias: 'taquito',
+	schema: '1.0',
+	version: '0.1',
+	tasks: [
+		Task.create({
+			task: 'deploy',
+			command: 'deploy [contract]',
+			description: 'Deploy a smart contract to a particular environment',
+			options: [],
+			aliases: ['originate'],
+			handler: 'proxy',
+			encoding: 'application/json',
+		}),
+	],
+	proxy: originate,
+}), process.argv);
