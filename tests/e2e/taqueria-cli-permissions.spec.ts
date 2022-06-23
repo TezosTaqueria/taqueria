76b143139c2a3815b9c8f3da4f2d7fef0d2769c4
AlexZbusko <alexander.zbusko@gmail.com>
2022-06-23 12:47:05 -0400

    415 linux permissions tests (#875)
    * added initial tests for artifact permissions
    
    * added in tests for types and smartpy
    
    * adding in logging for pipeline tests
    
    * getting group info in logging
    
    * getting user primary group for all group verification
    
    * forgot to trim output
    
    * skipping known to fail tests to make sure it runs on macOS as well
    
    * addind whitespace for readability
    
    * trying to execsync method for long running smartpy compilation
    
    * adding smartpy installation step to github actions main file
    
    * installing smartpy with automatic yes
    
    * moving the order of smartpy installation before all other installations
    
    * installing smartpy first
    
    * skippin smartpy test and removing smartpy installation in pipeline
    
    * removed the unused import
    
    * skipping failing tests to determine health of pipeline
    
    * unskipping tests since rest of pipeline seems good
    
    * comment for why smartpy test has been skipped

---
+++ tests/e2e/taqueria-cli-permissions.spec.ts
@@ -0,0 +1,102 @@
+import { exec as exec1 } from 'child_process';
+import fsPromises from 'fs/promises';
+import os from 'os';
+import util from 'util';
+import { generateTestProject } from './utils/utils';
+const exec = util.promisify(exec1);
+
+const taqueriaProjectPath = './e2e/auto-test-cli-permissions';
+let username = os.userInfo().username;
+let userGroup: string;
+
+describe('E2E Testing for taqueria plugin file permissions,', () => {
+	beforeAll(async () => {
+		await generateTestProject(taqueriaProjectPath, ['ligo', 'archetype', 'smartpy', 'contract-types']);
+
+		await exec(`cp e2e/data/fa12.arl ${taqueriaProjectPath}/contracts`);
+		await exec(`cp e2e/data/increment.jsligo ${taqueriaProjectPath}/contracts`);
+		await exec(`cp e2e/data/hello-tacos.py ${taqueriaProjectPath}/contracts`);
+
+		userGroup = (await exec(`id -g -n ${username}`)).stdout.trim();
+	});
+
+	test('testing that ligo artifacts will have the correct permissions', async () => {
+		await exec(`taq compile --plugin ligo`, { cwd: `./${taqueriaProjectPath}` });
+		const fileUser = await exec(`stat -c %U ${taqueriaProjectPath}/artifacts/increment.tz`);
+		const fileGroup = await exec(`stat -c %G ${taqueriaProjectPath}/artifacts/increment.tz`);
+
+		expect(fileUser.stdout.trim()).toBe(username);
+		expect(fileGroup.stdout.trim()).toBe(userGroup);
+	});
+
+	test('testing that archetype artifacts will have the correct permissions', async () => {
+		await exec(`taq compile --plugin archetype`, { cwd: `./${taqueriaProjectPath}` });
+		const fileUser = await exec(`stat -c %U ${taqueriaProjectPath}/artifacts/fa12.tz`);
+		const fileGroup = await exec(`stat -c %G ${taqueriaProjectPath}/artifacts/fa12.tz`);
+
+		expect(fileUser.stdout.trim()).toBe(username);
+		expect(fileGroup.stdout.trim()).toBe(userGroup);
+	});
+
+	// Skipping test currently due to smartpy installation in pipeline causing build to hang for hours
+	// and in the future we will use a docker container. When that is a reality we can enable this test
+	test.skip('testing that smartpy artifacts will have the correct permissions', async () => {
+		await exec(`taq compile --plugin smartpy`, { cwd: `./${taqueriaProjectPath}` });
+		const fileFolderUser = await exec(`stat -c %U ${taqueriaProjectPath}/artifacts/HelloTacos_comp/`);
+		const fileFolderGroup = await exec(`stat -c %G ${taqueriaProjectPath}/artifacts/HelloTacos_comp/`);
+
+		expect(fileFolderUser.stdout.trim()).toBe(username);
+		expect(fileFolderGroup.stdout.trim()).toBe(userGroup);
+
+		const fileContractUser = await exec(`stat -c %U ${taqueriaProjectPath}/artifacts/HelloTacos_comp/*contract.tz`);
+		const fileContractGroup = await exec(`stat -c %G ${taqueriaProjectPath}/artifacts/HelloTacos_comp/*contract.tz`);
+
+		expect(fileContractUser.stdout.trim()).toBe(username);
+		expect(fileContractGroup.stdout.trim()).toBe(userGroup);
+
+		const fileStorageUser = await exec(`stat -c %U ${taqueriaProjectPath}/artifacts/HelloTacos_comp/*storage.tz`);
+		const fileStorageGroup = await exec(`stat -c %G ${taqueriaProjectPath}/artifacts/HelloTacos_comp/*storage.tz`);
+
+		expect(fileStorageUser.stdout.trim()).toBe(username);
+		expect(fileStorageGroup.stdout.trim()).toBe(userGroup);
+	});
+
+	test('testing that type generation artifacts will have the correct permissions', async () => {
+		await exec(`taq compile --plugin ligo`, { cwd: `./${taqueriaProjectPath}` });
+		await exec(`taq generate types`, { cwd: `./${taqueriaProjectPath}` });
+
+		const incrementCodeUser = await exec(`stat -c %U ${taqueriaProjectPath}/types/increment.code.ts`);
+		const incrementCodeGroup = await exec(`stat -c %G ${taqueriaProjectPath}/types/increment.code.ts`);
+
+		expect(incrementCodeUser.stdout.trim()).toBe(username);
+		expect(incrementCodeGroup.stdout.trim()).toBe(userGroup);
+
+		const incrementTypeUser = await exec(`stat -c %U ${taqueriaProjectPath}/types/increment.types.ts`);
+		const incrementTypeGroup = await exec(`stat -c %G ${taqueriaProjectPath}/types/increment.types.ts`);
+
+		expect(incrementTypeUser.stdout.trim()).toBe(username);
+		expect(incrementTypeGroup.stdout.trim()).toBe(userGroup);
+
+		const typeAliasUser = await exec(`stat -c %U ${taqueriaProjectPath}/types/type-aliases.ts`);
+		const typeAliasGroup = await exec(`stat -c %G ${taqueriaProjectPath}/types/type-aliases.ts`);
+
+		expect(typeAliasUser.stdout.trim()).toBe(username);
+		expect(typeAliasGroup.stdout.trim()).toBe(userGroup);
+
+		const typeUtilUser = await exec(`stat -c %U ${taqueriaProjectPath}/types/type-utils.ts`);
+		const typeUtilGroup = await exec(`stat -c %G ${taqueriaProjectPath}/types/type-utils.ts`);
+
+		expect(typeUtilUser.stdout.trim()).toBe(username);
+		expect(typeUtilGroup.stdout.trim()).toBe(userGroup);
+	});
+
+	// Clean up process to remove taquified project folder
+	// Comment if need to debug
+	afterAll(async () => {
+		try {
+			fsPromises.rm(taqueriaProjectPath, { recursive: true });
+		} catch (error) {
+			throw new Error(`error: ${error}`);
+		}
+	});
+});
