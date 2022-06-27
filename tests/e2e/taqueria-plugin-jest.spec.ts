b1a1d87ae6af2852e71784fb05192d4bbe10dece
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-06-27 15:20:08 +0000

    updated jest tests to use cwd

--- tests/e2e/taqueria-plugin-jest.spec.ts
+++ tests/e2e/taqueria-plugin-jest.spec.ts
@@ -17,7 +17,7 @@ describe('E2E Testing for the taqueria jest plugin', () => {
 	});
 
 	test('Jest plugin creates default "tests" partition and jest config when running command with no arguments', async () => {
-		await exec(`taq test -p ${taqueriaProjectPath}`);
+		await exec(`taq test`, { cwd: `${taqueriaProjectPath}` });
 		const directoryContents = await exec(`ls ${taqueriaProjectPath}`);
 		const taqContents = await exec(`ls ${taqueriaProjectPath}/.taq`);
 		const testsContents = await exec(`ls ${taqueriaProjectPath}/tests`);
@@ -29,7 +29,7 @@ describe('E2E Testing for the taqueria jest plugin', () => {
 
 	test('Initializing a different partition with the init argument', async () => {
 		const directory = 'automated-tests-initialization';
-		await exec(`taq test -i ${directory} -p ${taqueriaProjectPath}`);
+		await exec(`taq test -i ${directory}`, { cwd: `${taqueriaProjectPath}` });
 		const directoryContents = await exec(`ls ${taqueriaProjectPath}`);
 		expect(directoryContents.stdout).toContain(directory);
 	});
@@ -37,7 +37,7 @@ describe('E2E Testing for the taqueria jest plugin', () => {
 	test('local jest config references global config with local info added', async () => {
 		const directory = 'test-config';
 
-		await exec(`taq test -i ${directory} -p ${taqueriaProjectPath}`);
+		await exec(`taq test -i ${directory}`, { cwd: `${taqueriaProjectPath}` });
 		const pwd = await exec(`pwd`);
 		const pwdFormatted = pwd.stdout.replace(/(\r\n|\n|\r)/gm, '');
 		const localConfigContents = await exec(`cat ${pwdFormatted}/${taqueriaProjectPath}/${directory}/jest.config.js`);
@@ -52,11 +52,11 @@ describe('E2E Testing for the taqueria jest plugin', () => {
 
 	test('simple jest test file can be run successfully with plugin', async () => {
 		const directory = 'dummy-test';
-		await exec(`taq test -i ${directory} -p ${taqueriaProjectPath}`);
+		await exec(`taq test -i ${directory}`, { cwd: `${taqueriaProjectPath}` });
 		await exec(
 			`cp e2e/data/empty-jest-test-file-1.ts ${taqueriaProjectPath}/${directory}/empty-jest-test-file-1.spec.ts`,
 		);
-		const testOutput = await exec(`taq test ${directory} -p ${taqueriaProjectPath}`);
+		const testOutput = await exec(`taq test ${directory}`, { cwd: `${taqueriaProjectPath}` });
 
 		expect(testOutput.stderr).toContain(
 			`\x1B[0m\x1B[7m\x1B[1m\x1B[32m PASS \x1B[39m\x1B[22m\x1B[27m\x1B[0m \x1B[2m${taqueriaProjectPath}/${directory}/\x1B[22m\x1B[1mempty-jest-test-file-1.spec.ts\x1B[22m`,
@@ -68,11 +68,11 @@ describe('E2E Testing for the taqueria jest plugin', () => {
 		const file1 = 'empty-jest-test-file-1.spec.ts';
 		const file2 = 'empty-jest-test-file-2.spec.ts';
 
-		await exec(`taq test -i ${directory} -p ${taqueriaProjectPath}`);
+		await exec(`taq test -i ${directory}`, { cwd: `${taqueriaProjectPath}` });
 
 		await exec(`cp e2e/data/empty-jest-test-file-1.ts ${taqueriaProjectPath}/${directory}/${file1}`);
 		await exec(`cp e2e/data/empty-jest-test-file-2.ts ${taqueriaProjectPath}/${directory}/${file2}`);
-		const testOutput = await exec(`taq test ${directory} -p ${taqueriaProjectPath}`);
+		const testOutput = await exec(`taq test ${directory}`, { cwd: `${taqueriaProjectPath}` });
 
 		expect(testOutput.stderr).toContain(
 			`\x1B[0m\x1B[7m\x1B[1m\x1B[32m PASS \x1B[39m\x1B[22m\x1B[27m\x1B[0m \x1B[2m${taqueriaProjectPath}/${directory}/\x1B[22m\x1B[1m${file1}\x1B[22m`,
@@ -87,11 +87,11 @@ describe('E2E Testing for the taqueria jest plugin', () => {
 		const file1 = 'empty-jest-test-file-1.spec.ts';
 		const file2 = 'empty-jest-test-file-2.spec.ts';
 
-		await exec(`taq test -i ${directory} -p ${taqueriaProjectPath}`);
+		await exec(`taq test -i ${directory}`, { cwd: `${taqueriaProjectPath}` });
 
 		await exec(`cp e2e/data/empty-jest-test-file-1.ts ${taqueriaProjectPath}/${directory}/${file1}`);
 		await exec(`cp e2e/data/empty-jest-test-file-2.ts ${taqueriaProjectPath}/${directory}/${file2}`);
-		const testOutput = await exec(`taq test ${directory} --testPattern file-* -p ${taqueriaProjectPath}`);
+		const testOutput = await exec(`taq test ${directory} --testPattern file-*`, { cwd: `${taqueriaProjectPath}` });
 
 		expect(testOutput.stderr).toContain(
 			`\x1B[0m\x1B[7m\x1B[1m\x1B[32m PASS \x1B[39m\x1B[22m\x1B[27m\x1B[0m \x1B[2m${taqueriaProjectPath}/${directory}/\x1B[22m\x1B[1m${file1}\x1B[22m`,
@@ -106,11 +106,11 @@ describe('E2E Testing for the taqueria jest plugin', () => {
 		const file1 = 'empty-jest-test-file-1.spec.ts';
 		const file2 = 'empty-jest-test-file-2.spec.ts';
 
-		await exec(`taq test -i ${directory} -p ${taqueriaProjectPath}`);
+		await exec(`taq test -i ${directory}`, { cwd: `${taqueriaProjectPath}` });
 
 		await exec(`cp e2e/data/empty-jest-test-file-1.ts ${taqueriaProjectPath}/${directory}/${file1}`);
 		await exec(`cp e2e/data/empty-jest-test-file-2.ts ${taqueriaProjectPath}/${directory}/${file2}`);
-		const testOutput = await exec(`taq test ${directory} --testPattern 1 -p ${taqueriaProjectPath}`);
+		const testOutput = await exec(`taq test ${directory} --testPattern 1`, { cwd: `${taqueriaProjectPath}` });
 
 		expect(testOutput.stderr).toContain(
 			`\x1B[0m\x1B[7m\x1B[1m\x1B[32m PASS \x1B[39m\x1B[22m\x1B[27m\x1B[0m \x1B[2m${taqueriaProjectPath}/${directory}/\x1B[22m\x1B[1m${file1}\x1B[22m`,
@@ -118,14 +118,14 @@ describe('E2E Testing for the taqueria jest plugin', () => {
 	});
 
 	test('no tests present will result in an error', async () => {
-		const testOutput = await exec(`taq test -p ${taqueriaProjectPath}`);
+		const testOutput = await exec(`taq test`, { cwd: `${taqueriaProjectPath}` });
 		expect(testOutput.stdout).toContain('No tests found, exiting with code 1');
 	});
 
 	test('global jest config matches reference config', async () => {
 		const directory = 'config-matching';
 
-		await exec(`taq test -i ${directory} -p ${taqueriaProjectPath}`);
+		await exec(`taq test -i ${directory}`, { cwd: `${taqueriaProjectPath}` });
 		const configContents = require(`../${taqueriaProjectPath}/.taq/jest.config.js`);
 
 		if (process.env.CI === 'true') {
