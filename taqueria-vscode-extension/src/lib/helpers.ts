af28514be4b916bbf3a980299e0ff887760dd2ac
Michał Lytek <michal.lytek@ecadlabs.com>
2022-08-04 19:59:42 +0200

    Add mising return artifactsFolderWatcher

--- taqueria-vscode-extension/src/lib/helpers.ts
+++ taqueria-vscode-extension/src/lib/helpers.ts
@@ -1035,6 +1035,7 @@ export class VsCodeHelper {
 					stateWatcher,
 					contractsFolderWatcher,
 					contractsWatcher,
+					artifactsFolderWatcher,
 					artifactsWatcher,
 					testsWatcher,
 				];
