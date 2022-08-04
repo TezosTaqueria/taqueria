0ed17d8f86505f7e8e8889f324f6a29120afdfe1
houston <houston.krohman@ecadlabs.com>
2022-08-04 11:21:51 -0700

    Upgrade Docusaurus to 2.0.1

--- website/src/theme/Footer/index.js
+++ website/src/theme/Footer/index.js
@@ -12,7 +12,6 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 import isInternalUrl from "@docusaurus/isInternalUrl";
 import styles from "./styles.module.css";
 import ThemedImage from "@theme/ThemedImage";
-// import IconExternalLink from "@theme/IconExternalLink";
 import FooterForm from "../../components/FooterForm/FooterForm";
 
 function FooterLink({
