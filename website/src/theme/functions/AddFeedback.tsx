fb9a4724524f11441e0a90cda8c9391a25011190
houston <houston.krohman@ecadlabs.com>
2022-08-05 18:19:10 -0700

    update button text

--- website/src/theme/functions/AddFeedback.tsx
+++ website/src/theme/functions/AddFeedback.tsx
@@ -119,7 +119,7 @@ class AddFeedback extends React.Component<IProps, IState> {
             <div className="card-demo">
               <div className="card">
                 <div className="card__header">
-                  <h3>Feedback / Bug Report</h3>
+                  <h3>Detailed Feedback</h3>
                 </div>
                 <div className="card__body">
                   <div className="radio">
