ee31217e7963e94d5bd058b96f96def4e884d7f3
AlirezaHaghshenas <alirezahaghshenas@gmail.com>
2022-08-09 10:53:41 -0700

    fix artifacts treeView for smartpy (#1099)

--- taqueria-vscode-extension/src/lib/gui/ArtifactsDataProvider.ts
+++ taqueria-vscode-extension/src/lib/gui/ArtifactsDataProvider.ts
@@ -1,6 +1,8 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
-import { HasFileName, HasRefresh, VsCodeHelper } from '../helpers';
+import { HasFileName, HasRefresh, OutputLevels, VsCodeHelper } from '../helpers';
+import { TaqifiedDir } from '../pure';
+import * as Util from '../pure';
 import { TaqueriaDataProviderBase } from './TaqueriaDataProviderBase';
 
 export class ArtifactsDataProvider extends TaqueriaDataProviderBase
@@ -15,11 +17,16 @@ export class ArtifactsDataProvider extends TaqueriaDataProviderBase
 	}
 
 	async getChildren(element?: ArtifactTreeItem): Promise<ArtifactTreeItem[]> {
-		const artifacts = await vscode.workspace.findFiles('artifacts/*.*', '**/node_modules/**');
-
+		const mainFolder = this.helper.getMainWorkspaceFolder();
+		if (!mainFolder) {
+			return [];
+		}
+		const config = await Util.TaqifiedDir.create(mainFolder.path as Util.PathToDir, this.helper.i18n);
+		const artifactsFolder = config.config.artifactsDir ?? 'artifacts';
+		const artifacts = await vscode.workspace.findFiles(`${artifactsFolder}/**/*.tz`, '**/node_modules/**');
 		const treeItems = artifacts.map(uri =>
 			new ArtifactTreeItem(
-				path.basename(uri.path),
+				mainFolder ? path.relative(path.join(mainFolder.path, artifactsFolder), uri.path) : path.basename(uri.path),
 				vscode.TreeItemCollapsibleState.None,
 			)
 		);
