754cc4994ac3e8787213ce7663e323103a99d49f
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-08-18 10:18:15 -0700

    Show the number of accounts, contracts, current level from sandbox and level from indexer

--- taqueria-vscode-extension/src/lib/gui/SandboxesDataProvider.ts
+++ taqueria-vscode-extension/src/lib/gui/SandboxesDataProvider.ts
@@ -1,5 +1,6 @@
 import { toSHA256 } from '@taqueria/protocol/SHA256';
 import { SpawnSyncOptionsWithBufferEncoding } from 'child_process';
+import { randomInt } from 'crypto';
 import fetch from 'node-fetch';
 import * as vscode from 'vscode';
 import { HasRefresh, mapAsync, OutputLevels, VsCodeHelper } from '../helpers';
@@ -13,13 +14,16 @@ export class SandboxesDataProvider extends TaqueriaDataProviderBase
 		super(helper);
 	}
 
+	refreshLevelInterval: NodeJS.Timer | undefined;
+	private sandboxTreeItems: SandboxTreeItem[] | undefined;
+
 	getTreeItem(element: SandboxTreeItemBase): vscode.TreeItem {
 		return element;
 	}
 
 	async getChildren(element?: SandboxTreeItemBase): Promise<SandboxTreeItemBase[]> {
 		if (element instanceof SandboxTreeItem) {
-			return this.getSandboxChildren(element);
+			return await this.getSandboxChildren(element);
 		}
 		if (element instanceof SandboxSmartContractTreeItem) {
 			return this.getSmartContractChildren(element);
@@ -47,6 +51,10 @@ export class SandboxesDataProvider extends TaqueriaDataProviderBase
 		if (element) {
 			return [];
 		}
+		return this.getSandboxItems();
+	}
+
+	private async getSandboxItems() {
 		const [{ config, pathToDir }, runningContainers] = await Promise.all([
 			this.getConfig(),
 			getRunningContainerNames(),
@@ -69,7 +77,35 @@ export class SandboxesDataProvider extends TaqueriaDataProviderBase
 				return new SandboxTreeItem(sandboxName, environmentName, containerName, isRunning);
 			},
 		);
+		this.sandboxTreeItems = items;
+		this.refreshLevelInterval = setInterval(async () => {
+			await this.updateSandboxInfo();
+		}, 1000);
 		return items;
+	}
+
+	async updateSandboxInfo(): Promise<void> {
+		if (!this.sandboxTreeItems) {
+			return;
+		}
+		for (const sandbox of this.sandboxTreeItems) {
+			try {
+				const sandBoxHeadResponse = await fetch(`http://127.0.0.1:20000/chains/main/blocks/head`);
+				const sandboxHead = await sandBoxHeadResponse.json();
+				sandbox.sandboxLevel = (sandboxHead as any).header.level;
+			} catch (e: unknown) {
+				this.helper.showLog(OutputLevels.warn, `${e}`);
+			}
+
+			try {
+				const tzKtHeadResponse = await fetch(`http://localhost:5000/v1/head`);
+				const tzKtHead = await tzKtHeadResponse.json();
+				sandbox.indexerLevel = (tzKtHead as any).level;
+			} catch (e: unknown) {
+				this.helper.showLog(OutputLevels.warn, `${e}`);
+			}
+			this.refreshItem(sandbox);
+		}
 	}
 
 	async getSmartContractEntrypoints(
@@ -79,7 +115,7 @@ export class SandboxesDataProvider extends TaqueriaDataProviderBase
 		if (!containerName) {
 			return [];
 		}
-		const response = await fetch(`https://api.tzkt.io/v1/contracts/${element.parent.address}/entrypoints`);
+		const response = await fetch(`http://localhost:5000/v1/contracts/${element.parent.address}/entrypoints`);
 		const data = await response.json();
 		return (data as any[]).map(item =>
 			new SmartContractEntrypointTreeItem(item.name, item.jsonParameters, element.parent)
@@ -90,13 +126,29 @@ export class SandboxesDataProvider extends TaqueriaDataProviderBase
 		throw new Error('Method not implemented.');
 	}
 
-	private getSandboxChildren(element: SandboxTreeItem): SandboxChildrenTreeItem[] {
+	private async getSandboxChildren(element: SandboxTreeItem): Promise<SandboxChildrenTreeItem[]> {
+		const containerName = element.containerName;
+		const [accountCount, contractCount] = containerName
+			? await Promise.all([this.getAccountCount(containerName), this.getContractCount(containerName)])
+			: [undefined, undefined];
 		return [
-			new SandboxChildrenTreeItem('Implicit Accounts', element),
-			new SandboxChildrenTreeItem('Smart Contracts', element),
-			new SandboxChildrenTreeItem('Operations', element),
-			new SandboxChildrenTreeItem('Non-Empty Blocks', element),
+			new SandboxChildrenTreeItem('Implicit Accounts', accountCount, element),
+			new SandboxChildrenTreeItem('Smart Contracts', contractCount, element),
+			new SandboxChildrenTreeItem('Operations', undefined, element),
+			new SandboxChildrenTreeItem('Non-Empty Blocks', undefined, element),
 		];
+	}
+
+	private async getAccountCount(containerName: string): Promise<number> {
+		const response = await fetch('http://localhost:5000/v1/accounts/count?type.ne=contract');
+		const data = await response.json();
+		return data as number;
+	}
+
+	private async getContractCount(containerName: string): Promise<number> {
+		const response = await fetch('http://localhost:5000/v1/contracts/count');
+		const data = await response.json();
+		return data as number;
 	}
 
 	private getSmartContractChildren(element: SandboxSmartContractTreeItem): SmartContractChildrenTreeItem[] {
@@ -123,9 +175,10 @@ export class SandboxesDataProvider extends TaqueriaDataProviderBase
 		if (!containerName) {
 			return [];
 		}
-		const response = await fetch('http://localhost:5000/v1/contracts');
+		const response = await fetch(`http://localhost:5000/v1/contracts?limit=1000&rnd=${randomInt(1000000000)}`);
 		const data = await response.json();
-		return (data as any[]).map(item => new SandboxSmartContractTreeItem(item.address, containerName));
+
+		return (data as any[]).map(item => new SandboxSmartContractTreeItem(item.address, item.type, containerName));
 	}
 
 	private _onDidChangeTreeData: vscode.EventEmitter<SandboxTreeItemBase | undefined | null | void> = new vscode
@@ -173,6 +226,9 @@ export class SandboxesDataProvider extends TaqueriaDataProviderBase
 	}
 
 	refresh(): void {
+		if (this.refreshLevelInterval) {
+			clearInterval(this.refreshLevelInterval);
+		}
 		this._onDidChangeTreeData.fire();
 	}
 }
@@ -195,6 +251,8 @@ export class SandboxTreeItemBase extends vscode.TreeItem {
 }
 
 export class SandboxTreeItem extends SandboxTreeItemBase {
+	private _sandboxLevel: number | undefined;
+	private _indexerLevel: number | undefined;
 	constructor(
 		public readonly sandboxName: string,
 		public readonly environmentName: string | undefined,
@@ -218,14 +276,42 @@ export class SandboxTreeItem extends SandboxTreeItemBase {
 			this.contextValue = running ? 'sandbox:running' : 'sandbox:stopped';
 		}
 	}
+
+	set sandboxLevel(value: number | undefined) {
+		this._sandboxLevel = value;
+		this.setDescription();
+	}
+
+	get sandboxLevel(): number | undefined {
+		return this._sandboxLevel;
+	}
+
+	set indexerLevel(value: number | undefined) {
+		this._indexerLevel = value;
+		this.setDescription();
+	}
+
+	get indexerLevel(): number | undefined {
+		return this._indexerLevel;
+	}
+
+	setDescription() {
+		this.description = this.running === undefined
+			? ''
+			: `${this.running ? 'running' : 'stopped'} ${this._sandboxLevel ?? ''} ${
+				this._indexerLevel ? 'Indexer: ' + this._indexerLevel : ''
+			}`;
+	}
 }
 
 export class SandboxChildrenTreeItem extends SandboxTreeItemBase {
 	constructor(
 		public readonly kind: 'Implicit Accounts' | 'Smart Contracts' | 'Operations' | 'Non-Empty Blocks',
+		description: string | number | undefined,
 		public readonly parent: SandboxTreeItem,
 	) {
 		super(kind, 'sandboxChild', vscode.TreeItemCollapsibleState.Collapsed);
+		this.description = `${description ?? ''}`;
 	}
 }
 
@@ -240,9 +326,11 @@ export class SandboxImplicitAccountTreeItem extends SandboxTreeItemBase {
 export class SandboxSmartContractTreeItem extends SandboxTreeItemBase {
 	constructor(
 		public readonly address: string,
+		public type: string,
 		public readonly containerName: string,
 	) {
-		super(address, 'smartContract', vscode.TreeItemCollapsibleState.Collapsed);
+		super(type, 'smartContract', vscode.TreeItemCollapsibleState.Collapsed);
+		this.description = address;
 	}
 }
 
