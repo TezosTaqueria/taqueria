e3f83e9bf56ab3dd01ac2d81f45b0e3a214cfd1f
Rick Love <rick@toldpro.com>
2022-08-17 23:25:52 -0500

    Add pin task test

--- tests/e2e/taqueria-plugin-ipfs-pinata.spec.ts
+++ tests/e2e/taqueria-plugin-ipfs-pinata.spec.ts
@@ -22,17 +22,17 @@ describe('E2E Testing for the taqueria ipfs pinata plugin', () => {
 	test('ipfs pinata plugin should warn if no env jwt token exists', async () => {
 		await exec(`rm ${taqueriaProjectPath}/.env`);
 
-		const publishRun = await exec(`cd ${taqueriaProjectPath} && taq publish ./ipfs/0001.txt`);
+		const publishRun = await exec(`taq publish ./ipfs/0001.txt`, { cwd: taqueriaProjectPath });
 		expect(publishRun.stderr).toMatch(/jwt *token.*not *found/i);
 	});
 
 	test('publish a single file to ipfs and get back the expected cid', async () => {
-		const publishRun = await exec(`cd ${taqueriaProjectPath} && taq publish ./ipfs/0001.txt`);
+		const publishRun = await exec(`taq publish ./ipfs/0001.txt`, { cwd: taqueriaProjectPath });
 		expect(publishRun.stdout).toMatch(/0001\.txt.*QmZcEXgfE9K4zFVkTrqW5x4skvEqpNajZzvb7frrWnkQa5/i);
 	});
 
 	test('publish a directory to ipfs and get back the expected cid for each file', async () => {
-		const publishRun = await exec(`cd ${taqueriaProjectPath} && taq publish ./ipfs/`);
+		const publishRun = await exec(`taq publish ./ipfs/`, { cwd: taqueriaProjectPath });
 
 		for (let i = 1; i <= 12; i++) {
 			expect(publishRun.stdout).toContain(`${i}.txt`);
@@ -42,6 +42,13 @@ describe('E2E Testing for the taqueria ipfs pinata plugin', () => {
 		expect(publishRun.stdout).toMatch(/0005\.txt.*QmQZF8kEyU6iuyc4xbnkTYNTtMEL4gjYgXhPjcXK14gnJ1/i);
 		expect(publishRun.stdout).toMatch(/0010\.txt.*QmW6GqMj6bXPXygCVCeVvcZN6w1w8ou4aVNKvarQrz8N4w/i);
 		expect(publishRun.stdout).toMatch(/0012\.txt.*QmZLSfobCPA8rQrm6dss28gyDwqXaw8GhQ8CXXQMSMnuNm/i);
+	});
+
+	test('pin using an ipfs hash', async () => {
+		const publishRun = await exec(`taq pin QmZcEXgfE9K4zFVkTrqW5x4skvEqpNajZzvb7frrWnkQa5`, {
+			cwd: taqueriaProjectPath,
+		});
+		expect(publishRun.stdout).toMatch(/QmZcEXgfE9K4zFVkTrqW5x4skvEqpNajZzvb7frrWnkQa5/i);
 	});
 
 	afterAll(async () => {
