8715dde46beae32a993706bc4b225cabfcc2d99d
GImbrailo <ginoimbrailo@gmail.com>
2022-08-18 19:22:20 -0700

    chore: changed pre-release npm publish to not happen when a releaes is published

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo "Runng \"npm version -ws $1\""
-npm version -ws "$1"
 
-echo "Runng \"npm --no-git-tag-version version $1\""
-npm version --no-git-tag-version "$1"
 
 # list of dependencies to be check
 dependencies=(
@@ -11,13 +7,38 @@ dependencies=(
     "@taqueria/node-sdk"
 )
 
+packages=(
+    "taqueria-protocol"
+    "taqueria-sdk"
+    "taqueria-plugin-flextesa"
+    "taqueria-plugin-contract-types"
+    "taqueria-flextesa-manager"
+    "taqueria-plugin-ipfs-pinata"
+    "taqueria-plugin-jest"
+    "taqueria-plugin-smartpy"
+    "taqueria-vscode-extension"
+    "taqueria-plugin-ligo"
+    "taqueria-plugin-mock"
+    "taqueria-plugin-archetype"
+    "taqueria-state"
+    "taqueria-plugin-tezos-client"
+    "taqueria-plugin-taquito"
+    "taqueria-utils"
+)
+
+echo "Runng \"npm version -ws $1\""
+npm version -ws "$1"
+
+echo "Runng \"npm --no-git-tag-version version $1\""
+npm version --no-git-tag-version "$1"
+
 echo "Updating dependencies..."
-for folder in $(find -maxdepth 1 -name "taqueria-*" -type d); do
-    if [ -f "$folder/package.json" ]; then
+for package in "${packages[@]}"; do
+    if [ -f "$package/package.json" ]; then
         for dependency in "${dependencies[@]}"; do
-            output=$(jq --arg dependency "$dependency" --arg version "^$1" 'select(.dependencies[$dependency] != null) | .dependencies[$dependency] |= $version' "$folder/package.json")
+            output=$(jq --arg dependency "$dependency" --arg version "^$1" 'select(.dependencies[$dependency] != null) | .dependencies[$dependency] |= $version' "$package/package.json")
             if [ -n "$output" ]; then
-                echo "$output" > "$folder/package.json"
+                echo "$output" > "$package/package.json"
             fi
         done
     fi
