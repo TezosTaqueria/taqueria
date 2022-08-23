b72ae62be584ec9ba5176afd36c3b4169c47b814
Jimmy Chen Chen <jimmy.chenchen@ecadlabs.com>
2022-08-23 10:27:39 -0700

    Refactor LIGO compiler and extend it to work with storage and parameter compilation as well

--- taqueria-plugin-ligo/compile.ts
+++ taqueria-plugin-ligo/compile.ts
@@ -1,50 +1,87 @@
 import { execCmd, getArch, getContracts, sendAsyncErr, sendErr, sendJsonRes } from '@taqueria/node-sdk';
 import { RequestArgs } from '@taqueria/node-sdk/types';
+import { mapOperationsToPlugins } from '@taqueria/protocol/EphemeralState';
+import { readFile, writeFile } from 'fs/promises';
 import { basename, extname, join } from 'path';
 
 interface Opts extends RequestArgs.t {
-	entrypoint?: string;
-	syntax?: string;
-	sourceFile?: string;
+	sourceFile: string;
 }
 
-const getContractArtifactFilename = (opts: Opts) =>
-	(sourceFile: string) => {
-		const outFile = basename(sourceFile, extname(sourceFile));
-		return join(opts.config.artifactsDir, `${outFile}.tz`);
-	};
+type TableRow = { contract: string; artifact: string };
 
-const getInputFilename = (opts: Opts) =>
-	(sourceFile: string) => {
-		return join(opts.config.contractsDir, sourceFile);
-	};
+const getInputFilename = (parsedArgs: Opts, sourceFile: string): string =>
+	join(parsedArgs.config.contractsDir, sourceFile);
 
-const getCompileCommand = (opts: Opts) =>
-	(sourceFile: string) => {
-		const projectDir = process.env.PROJECT_DIR ?? opts.projectDir;
+const getOutputFilename = (parsedArgs: Opts, sourceFile: string): string => {
+	const outputFile = basename(sourceFile, extname(sourceFile));
+	return join(parsedArgs.config.artifactsDir, `${outputFile}.tz`);
+};
 
-		if (!projectDir) throw `No project directory provided`;
+const getCompileContractCmd = (parsedArgs: Opts, sourceFile: string): string => {
+	const projectDir = process.env.PROJECT_DIR ?? parsedArgs.projectDir;
+	if (!projectDir) throw `No project directory provided`;
+	const baseCmd =
+		`DOCKER_DEFAULT_PLATFORM=linux/amd64 docker run --rm -v \"${projectDir}\":/project -w /project ligolang/ligo:next compile contract`;
+	const inputFile = getInputFilename(parsedArgs, sourceFile);
+	const outputFile = `-o ${getOutputFilename(parsedArgs, sourceFile)}`;
+	const cmd = `${baseCmd} ${inputFile} ${outputFile}`;
+	return cmd;
+};
 
-		const inputFile = getInputFilename(opts)(sourceFile);
-		const baseCommand =
-			`DOCKER_DEFAULT_PLATFORM=linux/amd64 docker run --rm -v \"${projectDir}\":/project -w /project -u $(id -u):$(id -g) ligolang/ligo:next compile contract ${inputFile}`;
-		const entryPoint = opts.entrypoint ? `-e ${opts.entrypoint}` : '';
-		const syntax = opts['syntax'] ? `-s ${opts['syntax']} : ""` : '';
-		const outFile = `-o ${getContractArtifactFilename(opts)(sourceFile)}`;
-		const cmd = `${baseCommand} ${entryPoint} ${syntax} ${outFile}`;
-		return cmd;
-	};
+const compileContract = (parsedArgs: Opts, sourceFile: string): Promise<TableRow> =>
+	getArch()
+		.then(() => getCompileContractCmd(parsedArgs, sourceFile))
+		.then(execCmd)
+		.then(({ stderr }) => {
+			if (stderr.length > 0) sendErr(stderr);
+			return {
+				contract: sourceFile,
+				artifact: getOutputFilename(parsedArgs, sourceFile),
+			};
+		})
+		.catch(err => {
+			sendErr(' ');
+			sendErr(err.message.split('\n').slice(1).join('\n'));
+			return Promise.resolve({
+				contract: sourceFile,
+				artifact: 'Not compiled',
+			});
+		});
 
-const compileContract = (opts: Opts) =>
-	(sourceFile: string): Promise<{ contract: string; artifact: string }> =>
+// Get the contract name that the storage file is associated with
+// e.g. If sourceFile is token.storages.mligo, then it'll return token.mligo
+const getContractNameForStorage = (sourceFile: string): string =>
+	sourceFile.match(/.+(?=\.storages\.(ligo|religo|mligo|jsligo))/)!.join('.');
+
+// If sourceFile is token.storages.mligo, then it'll return token.storage.{storageName}.tz
+const getOutputStorageFileName = (parsedArgs: Opts, sourceFile: string, storageName: string): string => {
+	const contractName = basename(getContractNameForStorage(sourceFile), extname(sourceFile));
+	const outputFile = `${contractName}.storage.${storageName}.tz`;
+	return join(parsedArgs.config.artifactsDir, `${outputFile}`);
+};
+
+const getCompileStorageCmd = (parsedArgs: Opts, sourceFile: string, storageName: string): string => {
+	const projectDir = process.env.PROJECT_DIR ?? parsedArgs.projectDir;
+	if (!projectDir) throw `No project directory provided`;
+	const baseCmd =
+		`DOCKER_DEFAULT_PLATFORM=linux/amd64 docker run --rm -v \"${projectDir}\":/project -w /project ligolang/ligo:next compile storage`;
+	const inputFile = getInputFilename(parsedArgs, sourceFile);
+	const outputFile = `-o ${getOutputStorageFileName(parsedArgs, sourceFile, storageName)}`;
+	const cmd = `${baseCmd} ${inputFile} ${storageName} ${outputFile}`;
+	return cmd;
+};
+
+const compileStorage = (parsedArgs: Opts, sourceFile: string) =>
+	(storageName: string): Promise<TableRow> =>
 		getArch()
-			.then(() => getCompileCommand(opts)(sourceFile))
+			.then(() => getCompileStorageCmd(parsedArgs, sourceFile, storageName))
 			.then(execCmd)
-			.then(({ stderr }) => { // How should we output warnings?
+			.then(({ stderr }) => {
 				if (stderr.length > 0) sendErr(stderr);
 				return {
 					contract: sourceFile,
-					artifact: getContractArtifactFilename(opts)(sourceFile),
+					artifact: getOutputStorageFileName(parsedArgs, sourceFile, storageName),
 				};
 			})
 			.catch(err => {
@@ -56,30 +93,116 @@ const compileContract = (opts: Opts) =>
 				});
 			});
 
-const compileAll = (parsedArgs: Opts) =>
-	Promise.all(getContracts(/\.(ligo|religo|mligo|jsligo)$/, parsedArgs.config))
-		.then(entries => entries.map(compileContract(parsedArgs)))
-		.then(processes => {
-			if (processes.length > 0) return processes;
-			return [];
+const compileStorages = (parsedArgs: Opts, sourceFile: string): Promise<TableRow[]> =>
+	readFile(getInputFilename(parsedArgs, sourceFile), 'utf8')
+		.then(data => {
+			if (!data.includes('#include')) {
+				writeFile(
+					getInputFilename(parsedArgs, sourceFile),
+					`#include "${getContractNameForStorage(sourceFile)}"\n` + data,
+					'utf8',
+				);
+			}
+			return data;
 		})
-		.then(promises => Promise.all(promises));
+		.then(data => data.match(/(?<=\s*(let|const)\s+)[a-zA-Z0-9_]+/g))
+		.then(storageNames =>
+			storageNames
+				? Promise.all(storageNames.map(compileStorage(parsedArgs, sourceFile)))
+				: Promise.resolve([])
+		);
 
-export const compile = (parsedArgs: Opts) => {
-	const p = parsedArgs.sourceFile
-		? compileContract(parsedArgs)(parsedArgs.sourceFile as string)
-			.then(result => [result])
-		: compileAll(parsedArgs)
-			.then(results => {
-				if (results.length === 0) {
-					sendErr('No contracts found to compile. Have you run "taq add-contract [sourceFile]" ?');
-				}
-				return results;
+// Get the contract name that the parameter file is associated with
+// e.g. If sourceFile is token.parameters.mligo, then it'll return token.mligo
+const getContractNameForParameter = (sourceFile: string): string =>
+	sourceFile.match(/.+(?=\.parameters\.(ligo|religo|mligo|jsligo))/)!.join('.');
+
+// If sourceFile is token.parameters.mligo, then it'll return token.parameter.{parameterName}.tz
+const getOutputParameterFileName = (parsedArgs: Opts, sourceFile: string, parameterName: string): string => {
+	const contractName = basename(getContractNameForParameter(sourceFile), extname(sourceFile));
+	const outputFile = `${contractName}.parameter.${parameterName}.tz`;
+	return join(parsedArgs.config.artifactsDir, `${outputFile}`);
+};
+
+const getCompileParameterCmd = (parsedArgs: Opts, sourceFile: string, parameterName: string): string => {
+	const projectDir = process.env.PROJECT_DIR ?? parsedArgs.projectDir;
+	if (!projectDir) throw `No project directory provided`;
+	const baseCmd =
+		`DOCKER_DEFAULT_PLATFORM=linux/amd64 docker run --rm -v \"${projectDir}\":/project -w /project ligolang/ligo:next compile parameter`;
+	const inputFile = getInputFilename(parsedArgs, sourceFile);
+	const outputFile = `-o ${getOutputParameterFileName(parsedArgs, sourceFile, parameterName)}`;
+	const cmd = `${baseCmd} ${inputFile} ${parameterName} ${outputFile}`;
+	return cmd;
+};
+
+const compileParameter = (parsedArgs: Opts, sourceFile: string) =>
+	(parameterName: string): Promise<TableRow> =>
+		getArch()
+			.then(() => getCompileParameterCmd(parsedArgs, sourceFile, parameterName))
+			.then(execCmd)
+			.then(({ stderr }) => {
+				if (stderr.length > 0) sendErr(stderr);
+				return {
+					contract: sourceFile,
+					artifact: getOutputParameterFileName(parsedArgs, sourceFile, parameterName),
+				};
+			})
+			.catch(err => {
+				sendErr(' ');
+				sendErr(err.message.split('\n').slice(1).join('\n'));
+				return Promise.resolve({
+					contract: sourceFile,
+					artifact: 'Not compiled',
+				});
 			});
 
-	return p
-		.then(sendJsonRes)
-		.catch(err => sendAsyncErr(err, false));
+const compileParameters = (parsedArgs: Opts, sourceFile: string): Promise<TableRow[]> =>
+	readFile(getInputFilename(parsedArgs, sourceFile), 'utf8')
+		.then(data => {
+			if (!data.includes('#include')) {
+				writeFile(
+					getInputFilename(parsedArgs, sourceFile),
+					`#include "${getContractNameForParameter(sourceFile)}"\n` + data,
+					'utf8',
+				);
+			}
+			return data;
+		})
+		.then(data => data.match(/(?<=\s*(let|const)\s+)[a-zA-Z0-9_]+/g))
+		.then(parameterNames =>
+			parameterNames
+				? Promise.all(parameterNames.map(compileParameter(parsedArgs, sourceFile)))
+				: Promise.resolve([])
+		);
+
+const mergeArtifactsOutput = (sourceFile: string) =>
+	(tableRows: TableRow[]): TableRow[] => {
+		const artifactsOutput = tableRows.reduce(
+			(acc: string, row: TableRow) => row.artifact === 'Not compiled' ? acc : `${acc}${row.artifact}\n`,
+			'',
+		);
+		return [{
+			contract: sourceFile,
+			artifact: artifactsOutput,
+		}];
+	};
+
+const isStoragesFile = (sourceFile: string): boolean => /.+\.storages\.(ligo|religo|mligo|jsligo)$/.test(sourceFile);
+
+const isParametersFile = (sourceFile: string): boolean =>
+	/.+\.parameters\.(ligo|religo|mligo|jsligo)$/.test(sourceFile);
+
+export const compile = (parsedArgs: Opts) => {
+	const sourceFile: string = parsedArgs.sourceFile;
+	let p: Promise<TableRow[]>;
+	if (isStoragesFile(sourceFile)) {
+		p = compileStorages(parsedArgs, sourceFile).then(mergeArtifactsOutput(sourceFile));
+	} else if (isParametersFile(sourceFile)) {
+		p = compileParameters(parsedArgs, sourceFile).then(mergeArtifactsOutput(sourceFile));
+	} else {
+		p = compileContract(parsedArgs, sourceFile).then(result => [result]);
+	}
+	return p.then(sendJsonRes).catch(err => sendAsyncErr(err, false));
 };
 
 export default compile;
--- taqueria-plugin-ligo/index.ts
+++ taqueria-plugin-ligo/index.ts
@@ -12,23 +12,6 @@ Plugin.create(i18n => ({
 			command: 'compile [sourceFile]',
 			aliases: ['c', 'compile-ligo'],
 			description: 'Compile a smart contract written in a Ligo syntax to Michelson code',
-			options: [
-				Option.create({
-					shortFlag: 'e',
-					flag: 'entrypoint',
-					description: 'The entry point that will be compiled',
-				}),
-				Option.create({
-					shortFlag: 's',
-					flag: 'syntax',
-					description: 'The syntax used in the contract',
-				}),
-				Option.create({
-					shortFlag: 'i',
-					flag: 'infer',
-					description: 'Enable type inference',
-				}),
-			],
 			handler: 'proxy',
 			encoding: 'json',
 		}),
