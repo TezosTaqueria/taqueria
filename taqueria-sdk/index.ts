3b615ffe62b84bdbe08b3365c7f221ac27636ae0
Jimmy Chen Chen <30018387+jchenche@users.noreply.github.com>
2022-09-02 08:36:20 -0700

    Add aliasing in deployment (#1196)
    * Add aliasing in deployment
    
    * Improve deploy UI
    
    * Make deploy fail message clearer
    
    * Fix some test by adding file name to deploy task
    
    * Fix taquito test except for 2
    
    Co-authored-by: houston <houston.krohman@ecadlabs.com>
    Co-authored-by: Houston <32914364+hu3man@users.noreply.github.com>
    Co-authored-by: Ivan Romanov <sinapsist@gmail.com>

--- taqueria-sdk/index.ts
+++ taqueria-sdk/index.ts
@@ -32,6 +32,7 @@ import { LikeAPromise, pluginDefiner, StdIO } from './types';
 // @ts-ignore interop issue. Maybe find a different library later
 import { templateRawSchema } from '@taqueria/protocol/SanitizedArgs';
 import generateName from 'project-name-generator';
+import { parsed } from 'yargs';
 
 // To use esbuild with yargs, we can't use ESM: https://github.com/yargs/yargs/issues/1929
 const yargs = require('yargs');
@@ -399,7 +400,7 @@ export const getSandboxAccountConfig = (parsedArgs: RequestArgs.t) =>
 		};
 
 /**
- * Gets the initial storage for the contract
+ * Gets the initial storage for the contract. TODO: replace all calls to this function with newGetInitialStorage
  */
 export const getInitialStorage = async (parsedArgs: RequestArgs.t, contractFilename: string) => {
 	const env = getCurrentEnvironmentConfig(parsedArgs);
@@ -414,6 +415,40 @@ export const getInitialStorage = async (parsedArgs: RequestArgs.t, contractFilen
 		}
 	}
 	return undefined;
+};
+
+/**
+ * Gets the initial storage for the contract. TODO: replace all calls to this function with newGetInitialStorage
+ */
+export const newGetInitialStorage = async (parsedArgs: RequestArgs.t, storageFilename: string) => {
+	const storagePath = join(parsedArgs.config.projectDir, parsedArgs.config.artifactsDir, storageFilename);
+	try {
+		const content = await readFile(storagePath, { encoding: 'utf-8' });
+		return content;
+	} catch (err) {
+		sendErr(`Could not read ${storagePath}. Maybe it doesn't exist.\n`);
+		return undefined;
+	}
+};
+
+/**
+ * Update the alias of an address for the current environment
+ */
+export const updateAddressAlias = async (parsedArgs: RequestArgs.t, alias: string, address: string): Promise<void> => {
+	const env = getCurrentEnvironmentConfig(parsedArgs);
+	if (!env) return;
+	if (!env.aliases) {
+		env.aliases = { [alias]: { address } };
+	} else if (!env.aliases[alias]) {
+		env.aliases[alias] = { address };
+	} else {
+		env.aliases[alias].address = address;
+	}
+	try {
+		await writeJsonFile('./.taq/config.json')(parsedArgs.config);
+	} catch (err) {
+		sendErr(`Could not write to ./.taq/config.json\n`);
+	}
 };
 
 /**
