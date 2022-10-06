1e085d2dd980cc38bfd6ac6d737cd83dbc09a502
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-10-06 14:56:17 -0700

    Implement all Micheline data types

--- taqueria-vscode-extension-web-ui/src/data-editors/DataEditorNode.tsx
+++ taqueria-vscode-extension-web-ui/src/data-editors/DataEditorNode.tsx
@@ -7,7 +7,7 @@ import { PairEditor } from './PairEditor';
 import { PrimitiveEditor } from './PrimitiveEditor';
 
 export const DataEditorNode = (
-	{ dataType, onChange, value }: { dataType: any; value: any; onChange: (value: unknown) => void },
+	{ dataType, onChange, value }: { dataType: any; value: any; onChange: (value: any) => void },
 ) => (
 	<div>
 		{(dataType.annots as string[])?.map(x => x.substring(1)).join(' ')}
@@ -16,7 +16,7 @@ export const DataEditorNode = (
 );
 
 const getEditor = (
-	{ dataType, onChange, value }: { dataType: any; value: any; onChange: (value: unknown) => void },
+	{ dataType, onChange, value }: { dataType: any; value: any; onChange: (value: any) => void },
 ) => {
 	const prim = dataType.prim;
 	switch (prim) {
@@ -26,17 +26,44 @@ const getEditor = (
 		case 'nat':
 		case 'int':
 		case 'bytes':
+		case 'timestamp':
+		// TODO: investigate each Domain-Specific type and make sure it's working fine
+		case 'mutez':
+		case 'address':
+		case 'key':
+		case 'key_hash':
+		case 'signature':
+		case 'chain_id':
+		case 'bls12_381_g1':
+		case 'bls12_381_g2':
+		case 'bls12_381_fr':
+		case 'sapling_transaction ms':
+		case 'sapling_state ms':
+		case 'ticket':
+		case 'chest':
+		case 'chest_key':
+		case 'tx_rollup_l2_address':
 			return <PrimitiveEditor dataType={prim} value={value} onChange={onChange} />;
 		case 'bool':
 			return <BoolEditor value={value} onChange={onChange} />;
 		case 'list':
-			return <ListEditor dataType={dataType} value={value as unknown[]} onChange={onChange} />;
+		case 'set':
+			return <ListEditor dataType={dataType} value={value as any[]} onChange={onChange} />;
 		case 'option':
 			return <OptionEditor dataType={dataType} value={value} onChange={onChange} />;
 		case 'pair':
 			return <PairEditor value={value} dataType={dataType} onChange={onChange} />;
 		case 'map':
+		case 'big_map':
 			return <MapEditor value={value} dataType={dataType} onChange={onChange} />;
+		default:
+			return (
+				<span>
+					The editor for {dataType.prim}{' '}
+					is not implemented yet, please contract the taqueria team at github.com/ecadlabs/taqueria to report this
+					issue.
+				</span>
+			);
 	}
 };
 
