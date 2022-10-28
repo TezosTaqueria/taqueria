0b63ffff47f02c25f2514ccdaa267914d3ac3fb1
Rick Love <rick@toldpro.com>
2022-10-28 17:46:13 -0500

    Rewriting types

--- taqueria-protocol-types/types.ts
+++ taqueria-protocol-types/types.ts
@@ -1,4 +1,9 @@
+type TODO = { notDoneYet: true };
+
 // ---- Simple Types & Type Fragments ----
+
+/** @minLength 1 */
+export type NonEmptyString = string;
 
 /** @pattern ^[A-Za-z]$ */
 export type SingleChar = string;
@@ -20,17 +25,31 @@ export type Settings = {
 	consent: 'opt_in' | 'opt_out';
 };
 
-// ---- CLI Types ----
-
-// export type SanitizedArgs = string
-
-// TODO: Option
-// TODO: PositionalArg
-
 // ---- Plugin Definition Types ----
 
 /** interpreted using yargs @pattern ^([A-Za-z-_ ]+ ?)((\[.+\] ?)|(\<.+\>) ?)*$ */
 export type Command = string;
+
+// export type SanitizedArgs = string
+
+export type Option = {
+	shortFlag?: SingleChar;
+	flag: Verb;
+	description: NonEmptyString;
+	defaultValue?: string | number | boolean;
+	type?: string | number | boolean;
+	required?: boolean;
+	boolean?: boolean;
+	choices?: string[];
+};
+
+export type PositionalArg = {
+	placeholder: HumanReadableIdentifier;
+	description: NonEmptyString;
+	defaultValue?: string | number | boolean;
+	type?: string | number | boolean;
+	required?: boolean;
+};
 
 export type InstalledPlugin = {
 	type: 'npm' | 'binary' | 'deno';
@@ -44,10 +63,43 @@ export type PluginActionName =
 	| 'installRuntimeDependencies'
 	| 'proxyTemplate';
 
-// TODO: Operation
-// TODO: ParsedOperation
-// TODO: ParsedTemplate
-// TODO: Template
+export type Operation = {
+	operation: Verb;
+	command: Command;
+	description?: string;
+	positionals?: PositionalArg[];
+	options?: Option[];
+	handler?: (args: PersistentState) => (args: RequestArgs) => void;
+};
+
+export type ParsedOperation = Omit<Operation, 'handler'>;
+
+export type Template = {
+	template: Verb;
+	command: Command;
+	/** @minLength 4 */
+	description: string;
+	hidden?: boolean;
+	options?: Option;
+	positionals?: PositionalArg;
+	handler: TemplateHandler;
+	encoding: PluginResponseEncoding;
+};
+
+type TemplateHandler =
+	| NonEmptyString // TODO: should this be Verb?
+	| ((args: RequestArgs) =>
+		| void
+		| PluginJsonResponse
+		| Promise<void>
+		| Promise<Promise<void>>
+		| Promise<PluginJsonResponse>);
+
+export type ParsedTemplate = Template & {
+	handler: string;
+	encoding: PluginResponseEncoding;
+};
+
 // TODO: PluginInfo
 // TODO: PluginSchema
 
@@ -55,9 +107,14 @@ export type PluginActionName =
 
 // TODO: PluginDependenciesResponse
 // TODO: PluginJsonResponse
+export type PluginJsonResponse = TODO;
+
 // TODO: PluginProxyResponse
 // TODO: PluginResponseEncoding
+export type PluginResponseEncoding = TODO;
+
 // TODO: RequestArgs
+export type RequestArgs = TODO;
 
 // ---- Hash Types ----
 
@@ -102,6 +159,7 @@ export type Faucet = {
 
 // TODO: EphemeralState
 // TODO: PersistentState
+export type PersistentState = TODO;
 
 // TODO: Provisioner
 // TODO: ProvisionerID
