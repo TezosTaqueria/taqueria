340c882c89596227377bf9733ea80cfe77306c8b
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-11-09 12:13:48 -0800

    Fix Test

--- taqueria-plugin-contract-types/example/types-file/example-lambda.types.ts
+++ taqueria-plugin-contract-types/example/types-file/example-lambda.types.ts
@@ -1,20 +1,20 @@
 
 import { ContractAbstractionFromContractType, WalletContractAbstractionFromContractType } from './type-utils';
-import { address } from './type-aliases';
+import { address, Instruction } from './type-aliases';
 
 type Storage = {
-    feedbackFunction: (arg: address) => string;
+    feedbackFunction: Instruction[];
     someAddress: address;
 };
 
 type Methods = {
     makeACall: (param: address) => Promise<void>;
-    updateFeedbackFunction: (param: (arg: address) => string) => Promise<void>;
+    updateFeedbackFunction: (param: Instruction[]) => Promise<void>;
 };
 
 type MethodsObject = {
     makeACall: (param: address) => Promise<void>;
-    updateFeedbackFunction: (param: (arg: address) => string) => Promise<void>;
+    updateFeedbackFunction: (param: Instruction[]) => Promise<void>;
 };
 
 type contractTypes = { methods: Methods, methodsObject: MethodsObject, storage: Storage, code: { __type: 'ExampleLambdaCode', protocol: string, code: object[] } };
