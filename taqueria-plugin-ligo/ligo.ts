f3115ac05bf9e4b07cf65a02b017be6d8a48ea91
Jimmy Chen Chen <jimmy.chenchen@ecadlabs.com>
2022-11-08 22:57:01 -0800

    Impl arbitrary command passing

--- taqueria-plugin-ligo/ligo.ts
+++ taqueria-plugin-ligo/ligo.ts
@@ -1,17 +1,41 @@
-import { sendAsyncErr } from '@taqueria/node-sdk';
-import { IntersectionOpts as Opts } from './common';
-import compile from './compile';
-import test from './test';
+import { execCmd, getArch, sendAsyncErr, sendErr, sendRes, sendWarn } from '@taqueria/node-sdk';
+import { writeFile } from 'fs/promises';
+import { LIGO_DOCKER_IMAGE, LigoOpts as Opts } from './common';
 
-const ligo = (parsedArgs: Opts): Promise<void> => {
-	switch (parsedArgs.task) {
-		case 'compile':
-			return compile(parsedArgs);
-		case 'test':
-			return test(parsedArgs);
-		default:
-			return sendAsyncErr(`${parsedArgs.task} is not an understood task by the LIGO plugin`);
-	}
+const MAX_OUTPUT_LIMIT = 50000;
+
+const getArbitraryLigoCmd = (parsedArgs: Opts, cmd: string): string => {
+	const projectDir = process.env.PROJECT_DIR ?? parsedArgs.projectDir;
+	if (!projectDir) throw `No project directory provided`;
+	const baseCmd =
+		`DOCKER_DEFAULT_PLATFORM=linux/amd64 docker run --rm -v \"${projectDir}\":/project -w /project -u $(id -u):$(id -g) ${LIGO_DOCKER_IMAGE}`;
+	return `${baseCmd} ${cmd}`;
+};
+
+const runArbitraryLigoCmd = (parsedArgs: Opts, cmd: string): Promise<string> =>
+	getArch()
+		.then(() => getArbitraryLigoCmd(parsedArgs, cmd))
+		.then(execCmd)
+		.then(async ({ stdout, stderr }) => {
+			if (stderr.length > 0) sendWarn(stderr);
+			if (stdout.length >= MAX_OUTPUT_LIMIT) {
+				const outputFile = './ligo.output';
+				sendWarn(
+					`The output of the underlying LIGO binary is too big so we are dumping it to "${outputFile}" instead of displaying it here in the console`,
+				);
+				await writeFile(outputFile, stdout, { encoding: 'utf8' });
+				return '';
+			}
+			return stdout;
+		})
+		.catch(err => {
+			sendErr(err.message.replace(/Command failed.+?\n/, ''));
+			return '';
+		});
+
+const ligo = async (parsedArgs: Opts): Promise<void> => {
+	const cmd = parsedArgs.command;
+	return runArbitraryLigoCmd(parsedArgs, cmd).then(sendRes).catch(err => sendAsyncErr(err, false));
 };
 
 export default ligo;
--- taqueria-plugin-tezos-client/client.ts
+++ taqueria-plugin-tezos-client/client.ts
@@ -1,17 +1,42 @@
-import { sendAsyncErr } from '@taqueria/node-sdk';
-import { IntersectionOpts as Opts } from './common';
-import simulate from './simulate';
-import typecheck from './typecheck';
+import { execCmd, getArch, getFlextesaImage, sendAsyncErr, sendErr, sendRes, sendWarn } from '@taqueria/node-sdk';
+import { writeFile } from 'fs/promises';
+import { ClientOpts as Opts } from './common';
 
-const client = (parsedArgs: Opts): Promise<void> => {
-	switch (parsedArgs.task) {
-		case 'typecheck':
-			return typecheck(parsedArgs);
-		case 'simulate':
-			return simulate(parsedArgs);
-		default:
-			return sendAsyncErr(`${parsedArgs.task} is not an understood task by the Tezos-client plugin`);
-	}
+const MAX_OUTPUT_LIMIT = 50000;
+
+const getArbitraryClientCmd = async (parsedArgs: Opts, cmd: string): Promise<string> => {
+	const projectDir = process.env.PROJECT_DIR ?? parsedArgs.projectDir;
+	if (!projectDir) throw `No project directory provided`;
+	const arch = await getArch();
+	const flextesaImage = await getFlextesaImage(arch);
+	const baseCmd =
+		`docker run --rm -v \"${projectDir}\":/project -w /project --platform ${arch} ${flextesaImage} octez-client`;
+	return `${baseCmd} ${cmd}`;
+};
+
+const runArbitraryClientCmd = (parsedArgs: Opts, cmd: string): Promise<string> =>
+	getArbitraryClientCmd(parsedArgs, cmd)
+		.then(execCmd)
+		.then(async ({ stdout, stderr }) => {
+			if (stderr.length > 0) sendWarn(stderr);
+			if (stdout.length >= MAX_OUTPUT_LIMIT) {
+				const outputFile = './octez-client.output';
+				sendWarn(
+					`The output of the underlying Octez Client binary is too big so we are dumping it to "${outputFile}" instead of displaying it here in the console`,
+				);
+				await writeFile(outputFile, stdout, { encoding: 'utf8' });
+				return '';
+			}
+			return stdout;
+		})
+		.catch(err => {
+			sendErr(err.message.replace(/Command failed.+?\n/, ''));
+			return '';
+		});
+
+const client = async (parsedArgs: Opts): Promise<void> => {
+	const cmd = parsedArgs.command;
+	return runArbitraryClientCmd(parsedArgs, cmd).then(sendRes).catch(err => sendAsyncErr(err, false));
 };
 
 export default client;
