011fbd646dbde5d8345e0849273e7079f33de797
houston <houston.krohman@ecadlabs.com>
2022-11-10 17:49:02 -0800

    Bump Ligo version to 0.55.0

--- taqueria-plugin-ligo/common.ts
+++ taqueria-plugin-ligo/common.ts
@@ -14,7 +14,7 @@ export type IntersectionOpts = CompileOpts & TestOpts;
 type UnionOpts = CompileOpts | TestOpts;
 
 // Points to the latest stable version. Needs to update this as part of our release process
-export const LIGO_DOCKER_IMAGE = 'ligolang/ligo:0.51.0';
+export const LIGO_DOCKER_IMAGE = 'ligolang/ligo:0.55.0';
 
 export const getInputFilename = (parsedArgs: UnionOpts, sourceFile: string): string =>
 	join(parsedArgs.config.contractsDir, sourceFile);
