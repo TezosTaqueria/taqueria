239638f7aa89b9d70a98de36aaa207c6845d63f6
Michael Weichert <mweichert@gmail.com>
2022-11-16 21:30:46 -0500

    Fixed CI/CD error

--- taqueria-vscode-extension/src/test/suite/utils/utils.ts
+++ taqueria-vscode-extension/src/test/suite/utils/utils.ts
@@ -1,25 +1,22 @@
-import retry from 'async-retry';
 import fsPromises from 'fs/promises';
 
 export function sleep(ms: number): Promise<void> {
 	return new Promise<void>(resolve => setTimeout(resolve, ms));
 }
 
-export async function checkFolderExistsWithTimeout(filePath: string) {
-	try {
-		const dir = filePath;
-
-		await retry(
-			async () => {
-				const watcher = await fsPromises.stat(dir);
-				return (watcher.birthtime !== undefined);
-			},
-			{
-				retries: 10,
-				maxTimeout: 1000,
-			},
-		);
-	} catch (error) {
-		throw new Error(`error: ${error}`);
+// The solution was taken from this source:
+// https://stackoverflow.com/questions/26165725/nodejs-check-file-exists-if-not-wait-till-it-exist
+// It is pull&wait mechanism and it is async by nature, because
+// there is no fs.watch sync solution
+export async function checkFolderExistsWithTimeout(filePath: string, attempts = 0) {
+	while (true) {
+		try {
+			const dir = filePath;
+			const watcher = await fsPromises.stat(dir);
+			break;
+		} catch (e) {
+			if (attempts < 5) await sleep(1000);
+			else throw e;
+		}
 	}
 }
