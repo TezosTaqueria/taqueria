e11c7d3f788cfeca884f85195c6ad8daade0bf93
GImbrailo <ginoimbrailo@gmail.com>
2022-11-16 17:51:14 -0800

    chore: remove test chunking

--- tests/jest.config.e2e.ts
+++ tests/jest.config.e2e.ts
@@ -158,7 +158,6 @@ export default {
 	// An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
 	testPathIgnorePatterns: [
 		'/node_modules/',
-		'./e2e/taqueria-plugin-ipfs-pinata.spec.ts',
 	],
 
 	// The regexp pattern or array of patterns that Jest uses to detect test files
