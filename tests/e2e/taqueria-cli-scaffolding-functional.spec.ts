bd55b88501612af04ad2cd66a2a2ff2318de024a
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-11-18 15:20:27 +0000

    updated scaffold sandbox to have scaffold in name to help with identification

--- tests/e2e/taqueria-cli-scaffolding-functional.spec.ts
+++ tests/e2e/taqueria-cli-scaffolding-functional.spec.ts
@@ -25,12 +25,11 @@ describe('E2E Testing for taqueria scaffolding initialization,', () => {
 	});
 
 	test('Verify that scaffold project can start and stop taqueria locally', async () => {
-		const startResults = await exec(`taq start sandbox local`, { cwd: `${scaffoldDirName}` });
-		expect(startResults.stdout).toContain('Started local.');
+		const startResults = await exec(`taq start sandbox local-scaffold`, { cwd: `${scaffoldDirName}` });
+		expect(startResults.stdout).toContain('Started local-scaffold.');
 
-		console.log(await exec(`docker ps -a --no-trunc`))
-		const stopResults = await exec(`taq stop sandbox local`, { cwd: `${scaffoldDirName}` });
-		expect(stopResults.stdout).toContain('Stopped local.');
+		const stopResults = await exec(`taq stop sandbox local-scaffold`, { cwd: `${scaffoldDirName}` });
+		expect(stopResults.stdout).toContain('Stopped local-scaffold.');
 	});
 
 	afterAll(async () => {
