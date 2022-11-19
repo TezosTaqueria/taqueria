d5a777684e4fccc913e514da0fb021912adc84a9
Jimmy Chen Chen <jimmy.chenchen@ecadlabs.com>
2022-11-19 03:00:19 -0800

    Update e2e test to reflect scaffold repo change

--- tests/e2e/taqueria-cli-scaffolding-functional.spec.ts
+++ tests/e2e/taqueria-cli-scaffolding-functional.spec.ts
@@ -25,11 +25,11 @@ describe('E2E Testing for taqueria scaffolding initialization,', () => {
 	});
 
 	test('Verify that scaffold project can start and stop taqueria locally', async () => {
-		const startResults = await exec(`taq start sandbox local`, { cwd: `${scaffoldDirName}` });
-		expect(startResults.stdout).toContain('Started local.');
+		const startResults = await exec(`taq start sandbox local-scaffold`, { cwd: `${scaffoldDirName}` });
+		expect(startResults.stdout).toContain('Started local-scaffold.');
 
-		const stopResults = await exec(`taq stop sandbox local`, { cwd: `${scaffoldDirName}` });
-		expect(stopResults.stdout).toContain('Stopped local.');
+		const stopResults = await exec(`taq stop sandbox local-scaffold`, { cwd: `${scaffoldDirName}` });
+		expect(stopResults.stdout).toContain('Stopped local-scaffold.');
 	});
 
 	afterAll(async () => {
