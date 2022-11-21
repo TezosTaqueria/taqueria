041a9a087bc2dfe2854b758f76aac96c74ff5016
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-11-21 12:48:56 -0800

    Fixes to build process

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+set -e # exiting on error
 
 if [ "$0" == "./bin/build-all.sh" ] && [ -f index.ts ]; then
     echo '**********************************************'
     echo '******* BUILDING ALL TAQUERIA PACKAGES *******'
     echo '**********************************************'
+
+    echo ""
+    echo '**********************************************'
+    echo "** Checking Dependencies"
+    docker ps
+
     echo ""
     echo '**********************************************'
     echo "** Installing NPM dependencies"
@@ -12,13 +19,13 @@ if [ "$0" == "./bin/build-all.sh" ] && [ -f index.ts ]; then
 
     echo ""
     echo '**********************************************'
-    echo "** Building packages"
-    npm run build:packages
+    echo "** Building taqueria"
+    npm run build:binary
 
     echo ""
     echo '**********************************************'
-    echo "** Building taqueria"
-    npm run build:binary
+    echo "** Building packages"
+    npm run build:packages
 
     echo ""
     echo '**********************************************'
