11a325351eb2a659898df1f4cf2ee19145072ad6
Alexander Zbusko <alexander.zbusko@gmail.com>
2022-11-22 21:32:32 +0000

    upped timeout and making tests not verbose

--- tests/jest.config.e2e.ts
+++ tests/jest.config.e2e.ts
@@ -185,12 +185,12 @@ export default {
 	// ],
 
 	// Changing default timeout from 5000 to 30000 for tests
-	testTimeout: 120000,
+	testTimeout: 300000,
 	// An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
 	// unmockedModulePathPatterns: undefined,
 
 	// Indicates whether each individual test should be reported during the run
-	verbose: true,
+	verbose: false,
 	// An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
 	// watchPathIgnorePatterns: [],
 
