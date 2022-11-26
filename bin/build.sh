2a552cb215438555fc7d5867872ca62125bc3179
Jimmy Chen Chen <jimmy.chenchen@ecadlabs.com>
2022-11-26 08:28:53 -0800

    Revert "Use bash bin/build.sh instead of dot slash bin/build.sh so that it works on Windows in CICD"
    This reverts commit ebf6bd31eb9223b83a5073923901cef0277c9fcb.

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source ./bin/set-vars.sh
 
-if [ "$0" == "bin/build.sh" -a -f index.ts ]; then
+if [ "$0" == "./bin/build.sh" -a -f index.ts ]; then
     HAS_DENO=`which deno`
     if [  -z "$HAS_DENO" ]; then
         echo "Please install deno before attempting to build."
@@ -10,6 +10,6 @@ if [ "$0" == "bin/build.sh" -a -f index.ts ]; then
         DENO_DIR=./deno deno compile -o taq --allow-run --allow-write --allow-read --allow-env --allow-net --import-map ./import_map.json --no-prompt index.ts --setBuild "$BUILD" --setVersion "$TAQ_VERSION" --lock ./deno-lock.json
     fi
 else
-    echo "Usage: bash bin/build.sh"
+    echo "Usage: ./bin/build.sh"
     echo "(please run from within project root)"
 fi
--- bin/run.sh
+++ bin/run.sh
@@ -6,9 +6,9 @@ BUILD="${BRANCH}-${TIMESTAMP}"
 BIN_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
 PROJ_DIR="${BIN_DIR}/.."
 
-if [ "$0" == "bin/run.sh" -a -f index.ts ]; then
+if [ "$0" == "./bin/run.sh" -a -f index.ts ]; then
     DENO_DIR=./deno deno run --allow-run --allow-write --allow-read --allow-env --allow-net --import-map "${PROJ_DIR}/import_map.json" index.ts --setBuild "$BUILD" --setVersion "$TAQ_VERSION"  --lock ./deno-lock.json $@
 else
-    echo "Usage: bash bin/build.sh"
+    echo "Usage: ./bin/build.sh"
     echo "(please run from within project root)"
 fi
