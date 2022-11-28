5c805c0e8aef8cbc32d16b95e30406a3fe4378a8
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-11-28 14:35:18 -0800

    another attempt to read from Github

--- bin/set-vars.sh
+++ bin/set-vars.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 BRANCH=`git branch --show-current`
 COMMIT=`git rev-parse --short HEAD`
-if [ -z "$GITHUB_SHA"]; then
-    TAQ_VERSION="dev-$BRANCH"
-else
-    TAQ_VERSION=${foo:0:8}
-fi
-TIMESTAMP=`date +%s`
-
 if [  -z "$GITHUB_REF" ]; then
     TAQ_VERSION="dev-$BRANCH"
 else
     TAQ_VERSION="${GITHUB_REF##*/}"
+fi
+TIMESTAMP=`date +%s`
+if [ -z "$GITHUB_SHA"]; then
+    BUILD="$COMMIT"
+else
+    BUILD="${GITHUB_SHA:0:8}"
+fi
