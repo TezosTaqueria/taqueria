32e702af46c0a5ff7d347ec253051f113a974f32
ben <ben@ecadlabs.com>
2022-11-28 14:16:42 -0800

    Change branch command

--- bin/set-vars.sh
+++ bin/set-vars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-BRANCH=`git branch --show-current`
+BRANCH=`git rev-parse --abbrev-ref HEAD`
 COMMIT=`git rev-parse --short HEAD`
 TAQ_VERSION="dev-$BRANCH"
 TIMESTAMP=`date +%s`
