4e4c77ec0d3538817336aa4ab35badd308dabf06
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-11-28 21:51:26 -0800

    Go back to using GITHUB_REF_NAME for TAQ_VERSION

--- bin/set-vars.sh
+++ bin/set-vars.sh
@@ -4,7 +4,7 @@ COMMIT=`git rev-parse --short HEAD`
 if [  -z "$GITHUB_REF_NAME" ]; then
     TAQ_VERSION="dev-${BRANCH//\//-}"
 else
-    TAQ_VERSION="${BRANCH//\//-}"
+    TAQ_VERSION="${GITHUB_REF_NAME//\//-}"
 fi
 TIMESTAMP=`date +%s`
 BUILD="$COMMIT"
