fca979246d17b940cfbab0e62aa675d0caf86cf7
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2022-11-28 21:43:30 -0800

    read branch name from git

--- bin/set-vars.sh
+++ bin/set-vars.sh
@@ -4,7 +4,7 @@ COMMIT=`git rev-parse --short HEAD`
 if [  -z "$GITHUB_REF_NAME" ]; then
     TAQ_VERSION="dev-${BRANCH//\//-}"
 else
-    TAQ_VERSION="${GITHUB_REF_NAME//\//-}"
+    TAQ_VERSION="${BRANCH//\//-}"
 fi
 TIMESTAMP=`date +%s`
 BUILD="$COMMIT"
