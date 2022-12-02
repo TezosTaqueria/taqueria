25d4b7331882bd1a36c4ffec56c16f1a03b84893
Jimmy Chen Chen <jimmy.chenchen@ecadlabs.com>
2022-12-01 21:16:19 -0800

    clean up

--- bin/before-all-tests.sh
+++ bin/before-all-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e # exiting on error
 
-docker ps > /dev/null
+docker ps > /dev/null # making sure docker is installed, running, and the user has permission
 
 # Clean all docker containers before running any tests
 docker rm -vf $(docker ps -aq) > /dev/null 2>&1 || echo "There are no docker containers to remove"
