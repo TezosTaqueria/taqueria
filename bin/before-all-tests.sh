8089353fcaaab203a056dc296aa48532eaddd5ad
Jimmy Chen Chen <jimmy.chenchen@ecadlabs.com>
2022-12-01 20:57:11 -0800

    add back check of docker cmd

--- bin/before-all-tests.sh
+++ bin/before-all-tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e # exiting on error
 
+docker ps > /dev/null
+
 # Clean all docker containers before running any tests
 docker rm -vf $(docker ps -aq) > /dev/null 2>&1 || echo "There are no docker containers to remove"
 
