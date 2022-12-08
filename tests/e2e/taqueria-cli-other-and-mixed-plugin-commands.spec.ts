2bf82a32e3e0a8e574eda80ea4b252570f93d3a0
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-12-08 07:36:31 -0800

    updated the scaffold tests

--- tests/e2e/taqueria-cli-other-and-mixed-plugin-commands.spec.ts
+++ tests/e2e/taqueria-cli-other-and-mixed-plugin-commands.spec.ts
@@ -3,121 +3,63 @@ import { prepareEnvironment } from '@gmrchk/cli-testing-library';
 describe('E2E Testing for taqueria CLI,', () => {
 	test('Verify that taq --help gives the help menu for a non-initialized project', async () => {
 		const { execute, cleanup } = await prepareEnvironment();
-
-		const { code, stdout } = await execute(
-			'taq',
-			'--help',
-		);
-
+		const { code } = await execute('taq', '--help');
 		expect(code).toBe(0);
 		expect.arrayContaining(['taq scaffold ']);
-
 		await cleanup();
 	});
 
 	test('Verify that taq --help gives the help menu for an initialized project', async () => {
 		const { execute, cleanup } = await prepareEnvironment();
-
-		const { code } = await execute(
-			'taq',
-			'--help -p auto-test-cli',
-		);
-
+		const { code } = await execute('taq', '--help -p auto-test-cli');
 		expect(code).toBe(0);
 		expect.arrayContaining(['taq install <pluginName> ']);
-
 		await cleanup();
 	});
 
 	test('Verify that taq reports a version', async () => {
 		const { execute, cleanup } = await prepareEnvironment();
-
-		const { code, stdout } = await execute(
-			'taq',
-			'--version',
-		);
-
+		const { code, stdout } = await execute('taq', '--version');
 		expect(code).toBe(0);
-
 		expect(stdout[0].toString().trim()).toMatch(
 			/^((v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)|(dev-[\w-]+)|main?|(\d+)-[\w-]+)$/,
 		);
-
 		await cleanup();
 	});
 
 	test('Verify that build reports build information about the version', async () => {
 		const { execute, cleanup } = await prepareEnvironment();
-
-		const { code, stdout } = await execute(
-			'taq',
-			'--build',
-		);
-
+		const { code, stdout } = await execute('taq', '--build');
 		expect(code).toBe(0);
 		expect(stdout[0].trim()).toMatch(/^\w+$/);
-
 		await cleanup();
 	});
 
 	test('Verify that trying a command that is not available returns an error', async () => {
 		const { execute, cleanup } = await prepareEnvironment();
-
-		const { code, stderr } = await execute(
-			'taq',
-			'compile -p foobar',
-		);
-
+		const { code } = await execute('taq', 'compile -p foobar');
 		expect(code).toBe(1);
 		expect.arrayContaining(["Taqueria isn't aware of this task. Perhaps you need to install a plugin first?"]);
-
 		await cleanup();
 	});
 
 	test('Verify that trying to install a package that does not exist returns an error', async () => {
 		const { execute, cleanup } = await prepareEnvironment();
-
-		const { code } = await execute(
-			'taq',
-			'install acoupleofecadhamburgers -p foobar',
-		);
-
+		const { code } = await execute('taq', 'install acoupleofecadhamburgers -p foobar');
 		expect(code).toBe(1);
 		expect.arrayContaining(['Could not read.*acoupleofecadhamburgers']);
-
 		await cleanup();
 	});
 
 	test('Verify that ligo and smartpy expose the plugin choice option for compile in the help menu', async () => {
 		const { execute, cleanup } = await prepareEnvironment();
-
-		const { code } = await execute(
-			'taq',
-			'install ../../../taqueria-plugin-ligo -p foobar',
-		);
-
-		await execute(
-			'taq',
-			'install ../../../taqueria-plugin-smartpy -p foobar',
-		);
-
-		await execute(
-			'taq',
-			'--help --projectDir=foobar',
-		);
-
+		const { code } = await execute('taq', 'install ../../../taqueria-plugin-ligo -p foobar');
+		await execute('taq', 'install ../../../taqueria-plugin-smartpy -p foobar');
+		await execute('taq', '--help --projectDir=foobar');
 		expect(code).toBe(1);
 		expect.arrayContaining(['taq ligo ']);
-
-		const {} = await execute(
-			'taq',
-			'uninstall @taqueria/plugin-ligo -p foobar',
-		);
-
-		const {} = await execute(
-			'taq',
-			'uninstall @taqueria/plugin-smartpy -p foobar`',
-		);
+		const {} = await execute('taq', 'uninstall @taqueria/plugin-ligo -p foobar');
+		const {} = await execute('taq', 'uninstall @taqueria/plugin-smartpy -p foobar');
 
 		await cleanup();
 	});
--- tests/e2e/taqueria-cli-scaffolding-functional.spec.ts
+++ tests/e2e/taqueria-cli-scaffolding-functional.spec.ts
@@ -1,79 +1,29 @@
+import { exec as exec1 } from 'child_process';
+import util from 'util';
+const exec = util.promisify(exec1);
 import { prepareEnvironment } from '@gmrchk/cli-testing-library';
 
 describe('E2E Testing for taqueria scaffolding initialization,', () => {
-	test('Verify that scaffold project sets up compiles contracts as part of setup', async () => {
-		const { execute, exists } = await prepareEnvironment();
+	test('Verify that scaffold project gets sets up.', async () => {
+		const { execute, exists, cleanup } = await prepareEnvironment();
 		const { code } = await execute(
 			'taq',
-			'scaffold https://github.com/ecadlabs/taqueria-scaffold-taco-shop.git auto-test-taco-shop-functional',
+			'scaffold https://github.com/ecadlabs/taqueria-scaffold-taco-shop.git scaffold-test',
 		);
 		expect(code).toBe(0);
-		await exists('./auto-test-taco-shop-functional}/app');
-		await exists('./auto-test-taco-shop-functional');
-		await exists('./auto-test-taco-shop-functional/node_modules');
-		await exists('./auto-test-taco-shop-functional/contracts');
-		await exists('./auto-test-taco-shop-functional/artifacts');
-		await exists('./auto-test-taco-shop-functional/artifacts/hello-tacos.tz');
-	});
-
-	test('Verify that scaffold project can start and stop taqueria locally', async () => {
-		jest.setTimeout(500000);
-		const { execute, spawn, cleanup } = await prepareEnvironment();
-
-		const {} = await spawn(
-			'taq',
-			'install @taqueria/plugin-flextesa',
-		);
-
-		expect.arrayContaining(['Plugin installed successfully']);
-
-		const { code } = await execute(
-			'taq',
-			'scaffold https://github.com/ecadlabs/taqueria-scaffold-taco-shop.git auto-test-taco-shop-functional',
-		);
-
-		expect(code).toBe(0);
-
-		const {} = await spawn(
-			'taq',
-			'install @taqueria/plugin-flextesa',
-		);
-
-		expect.arrayContaining(['Plugin installed successfully']);
-
-		const {} = await execute(
-			'taq',
-			'scaffold https://github.com/ecadlabs/taqueria-scaffold-taco-shop.git auto-test-taco-shop-functional',
-		);
-
-		expect(code).toBe(0);
-		expect.arrayContaining(['Scaffolding 🛠']);
-		expect.arrayContaining(["Project Taq'ified"]);
-
-		const {} = await execute(
-			'taq',
-			'start sandbox local-scaffold',
-		);
-
-		expect.arrayContaining(['start']);
-
-		const { stdout } = await execute(
-			'taq',
-			'start sandbox local-scaffold',
-		);
-
-		console.log(stdout);
-		expect(code).toBe(0);
-		expect.arrayContaining(['start']);
-
-		const {} = await execute(
-			'taq',
-			'stop sandbox local-scaffold',
-		);
-
-		expect(code).toBe(0);
-		expect.arrayContaining(['stop']);
-
+		await exists('./scaffold-test');
+		await exists('./scaffold-test/.taq/config.json');
+		await exists('./scaffold-test/app');
+		await exists('./scaffold-test/node_modules');
+		await exists('./scaffold-test/contracts');
+		await exists('./scaffold-test/artifacts');
+		await exists('./scaffold-test/artifacts/hello-tacos.tz');
+		await exists('./scaffold-test/artifacts/hello-tacos.tz.meta.json');
+		// see # https://github.com/ecadlabs/taqueria/issues/1592
+		// const startResults = await exec(`taq start sandbox local-scaffold`, { cwd: `scaffold-test` });
+		// expect(startResults.stdout).toContain('Started local-scaffold.');
+		// const stopResults = await exec(`taq stop sandbox local-scaffold`, { cwd: `scaffold-test` });
+		// expect(stopResults.stdout).toContain('Stopped local-scaffold.');
 		await cleanup();
 	});
 });
