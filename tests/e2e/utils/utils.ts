42196c558a3d81c167128399b02697f4f7c145fb
Michael Weichert <mweichert@gmail.com>
2022-12-08 11:37:55 -0500

    Fixed memory leak in test/e2e/utils

--- tests/e2e/utils/utils.ts
+++ tests/e2e/utils/utils.ts
@@ -1,32 +1,24 @@
 import { TezosToolkit } from '@taquito/taquito';
-import { exec as exec1, execSync } from 'child_process';
+import { exec as exec1 } from 'child_process';
 import fsPromises from 'fs/promises';
 import path from 'path';
 import util from 'util';
 const exec = util.promisify(exec1);
 
-export const sleep = (ms: number) => new Promise((resolve, reject) => setTimeout(resolve, ms));
+export const sleep = (ms: number) => new Promise((resolve, _reject) => setTimeout(resolve, ms));
 
 export const generateTestProject = async (
 	projectPath: string,
 	packageNames: string[] = [],
 	localPackages: boolean = true,
 ) => {
+	debugger;
 	const targetDir = path.join('/tmp', projectPath);
 
-	let projectInit;
-	try {
-		projectInit = await exec(`taq init ${targetDir}`);
-	} catch (error) {
-		throw new Error(`error: ${error}`);
-	}
+	const projectInit = await exec(`taq init ${targetDir}`);
 
 	await checkFolderExistsWithTimeout(targetDir);
-	try {
-		await fsPromises.chmod(targetDir, 0o777);
-	} catch (error) {
-		throw new Error(`error: ${error}`);
-	}
+	await fsPromises.chmod(targetDir, 0o777);
 
 	await exec(`touch package.json`, { cwd: `${targetDir}`, encoding: `utf-8` });
 	await exec('npm init -y', { cwd: `${targetDir}`, encoding: 'utf-8' });
@@ -77,17 +69,8 @@ export function itemArrayInTable(regex: RegExp, inputTable: { stdout: string; st
 // https://stackoverflow.com/questions/26165725/nodejs-check-file-exists-if-not-wait-till-it-exist
 // It is pull&wait mechanism and it is async by nature, because
 // there is no fs.watch sync solution
-export async function checkFolderExistsWithTimeout(filePath: string, attempts = 0) {
-	while (true) {
-		try {
-			const dir = filePath;
-			const watcher = await fsPromises.stat(dir);
-			break;
-		} catch (e) {
-			if (attempts < 5) await sleep(1000);
-			else throw e;
-		}
-	}
+export function checkFolderExistsWithTimeout(filePath: string, timeout = 10000, interval = 100) {
+	return waitFor(() => fsPromises.stat(filePath), { timeout, interval });
 }
 
 export async function waitFor<T>(fn: () => Promise<T>, opts = { timeout: 10000, interval: 100 }) {
@@ -102,6 +85,7 @@ export async function waitFor<T>(fn: () => Promise<T>, opts = { timeout: 10000, 
 		} catch (err) {
 			lastErr = err;
 			await sleep(interval);
+			elapsed += interval;
 		}
 	}
 	throw lastErr;
@@ -132,19 +116,16 @@ export async function installDependencies(
 	localPackages: boolean = true,
 ) {
 	for (const packageName of packageNames) {
-		try {
-			if (localPackages) {
-				execSync(`taq install ../../../taqueria-plugin-${packageName}`, {
-					cwd: `./${projectPath}`,
-					encoding: 'utf8',
-				});
-			} else {
-				execSync(`taq install @taqueria/plugin-${packageName}`, { cwd: `./${projectPath}` });
-			}
-		} catch (error) {
-			throw new Error(`error: ${error}`);
+		if (localPackages) {
+			const output = await exec(`taq install ../../../taqueria-plugin-${packageName}`, {
+				cwd: `./${projectPath}`,
+				encoding: 'utf8',
+			});
+		} else {
+			const output = await exec(`taq install @taqueria/plugin-${packageName}`, { cwd: `./${projectPath}` });
 		}
 
-		await checkFolderExistsWithTimeout(`./${projectPath}/node_modules/@taqueria/plugin-${packageName}/index.js`);
+		await checkFolderExistsWithTimeout(`${projectPath}/node_modules/@taqueria/plugin-${packageName}/index.js`);
 	}
+	return;
 }
