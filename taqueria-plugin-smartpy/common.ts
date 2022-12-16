e2ffbc3e50df578454da2bd6c9c9e145cdf546ee
Jimmy Chen Chen <jimmy.chenchen@ecadlabs.com>
2022-12-16 15:03:21 -0800

    Version management

--- taqueria-plugin-smartpy/common.ts
+++ taqueria-plugin-smartpy/common.ts
@@ -1,6 +1,5 @@
 import { execCmd, getArtifactsDir, getContractsDir, sendErr, sendWarn } from '@taqueria/node-sdk';
 import { ProxyTaskArgs } from '@taqueria/node-sdk/types';
-import { RequestArgs } from '@taqueria/node-sdk/types';
 import { access } from 'fs/promises';
 import { join } from 'path';
 
@@ -17,15 +16,46 @@ export type IntersectionOpts = CompileOpts & TestOpts;
 
 type UnionOpts = CompileOpts | TestOpts;
 
+// Should point to the latest stable version, so it needs to be updated as part of our release process.
+const SMARTPY_DEFAULT_VERSION = 'v0.16.0';
+
+const SMARTPY_VERSION_ENV_VAR = 'TAQ_SMARTPY_VERSION';
+
 const SMARTPY_ARTIFACTS_DIR = '.smartpy';
 
-// Should point to the latest version, so it needs to be updated as part of our release process.
-// Currently this points to v0.15.0
-const SMARTPY_INSTALLER =
-	'https://smartpy.io/releases/20221026-28e8c18e46035c353804eb5fd725573c5d434e8a/cli/install.sh';
+const smartpyVersionToInstallerMap: { [k: string]: string } = {
+	'v0.16.0': 'https://smartpy.io/releases/20221215-8f134ebb649f5a7b37c44fca8f336f970f523565/cli/install.sh',
+	'v0.15.0': 'https://smartpy.io/releases/20221026-28e8c18e46035c353804eb5fd725573c5d434e8a/cli/install.sh',
+	'v0.14.0': 'https://smartpy.io/releases/20220926-1c748c4572188f65a525792468e37da2182f18a2/cli/install.sh',
+};
 
-const SMARTPY_INSTALL_CMD =
-	`curl -s ${SMARTPY_INSTALLER} > ~/SmartPyCliInstaller.sh; bash ~/SmartPyCliInstaller.sh --yes; rm ~/SmartPyCliInstaller.sh`;
+const getSmartpyVersion = (): string => {
+	const userDefinedSmartpyVersion = process.env[SMARTPY_VERSION_ENV_VAR];
+	if (userDefinedSmartpyVersion) {
+		if (smartpyVersionToInstallerMap[userDefinedSmartpyVersion]) {
+			return userDefinedSmartpyVersion;
+		} else {
+			sendWarn(
+				`Version ${userDefinedSmartpyVersion} is not supported by Taqueria yet. Will default to ${SMARTPY_DEFAULT_VERSION}`,
+			);
+			return SMARTPY_DEFAULT_VERSION;
+		}
+	} else {
+		return SMARTPY_DEFAULT_VERSION;
+	}
+};
+
+const getPathToSmartPyCliDir = (): string => `${process.env.HOME}/smartpy-cli-${getSmartpyVersion()}`;
+
+const getSmartPyInstallerCmd = (): string => {
+	const installer = '~/SmartPyCliInstaller.sh';
+	const download = `curl -s ${smartpyVersionToInstallerMap[getSmartpyVersion()]} > ${installer};`;
+	const install = `bash ${installer} --yes --prefix ${getPathToSmartPyCliDir()};`;
+	const clean = `rm ${installer};`;
+	return download + install + clean;
+};
+
+export const getSmartPyCli = (): string => `${getPathToSmartPyCliDir()}/SmartPy.sh`;
 
 export const addPyExtensionIfMissing = (sourceFile: string): string =>
 	/\.py$/.test(sourceFile) ? sourceFile : `${sourceFile}.py`;
@@ -40,8 +70,6 @@ const removeExt = (path: string): string => {
 	return path.replace(extRegex, '');
 };
 
-export const getSmartPyCli = (): string => `${process.env.HOME}/smartpy-cli/SmartPy.sh`;
-
 export const getInputFilename = (parsedArgs: UnionOpts, sourceFile: string): string =>
 	join(getContractsDir(parsedArgs), sourceFile);
 
@@ -52,7 +80,7 @@ export const installSmartPyCliIfNotExist = () =>
 	access(getSmartPyCli())
 		.catch(() => {
 			sendWarn('SmartPy CLI not found. Installing it now...');
-			return execCmd(SMARTPY_INSTALL_CMD)
+			return execCmd(getSmartPyInstallerCmd())
 				.then(({ stderr }) => {
 					if (stderr.length > 0) sendWarn(stderr);
 				});
