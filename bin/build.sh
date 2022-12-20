286e605eeb653d84ae57c3409c6ebd851dab75e0
Daniele Lisi <22307776+danielelisi@users.noreply.github.com>
2022-12-20 01:14:49 -0800

    Remove deno-lock file

--- bin/build.sh
+++ bin/build.sh
@@ -19,7 +19,7 @@ if [ "$0" == "./bin/build.sh" -a -f index.ts ]; then
         else
             TARGET_ARG="--target $DENO_TARGET"
         fi
-        deno compile -o taq --allow-run --allow-write --allow-read --allow-env --allow-net --import-map ./import_map.json --no-prompt index.ts --setBuild "$BUILD" --setVersion "$TAQ_VERSION" --lock ./deno-lock.json $TARGET_ARG
+        deno compile -o taq --allow-run --allow-write --allow-read --allow-env --allow-net --import-map ./import_map.json --no-prompt index.ts --setBuild "$BUILD" --setVersion "$TAQ_VERSION" $TARGET_ARG
     fi
 else
     echo "Usage: ./bin/build.sh"
