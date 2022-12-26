0bf8be1563a5a9b7c953f18de95842449c35e9f4
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-12-26 09:29:10 -0800

    polishing

--- tests/e2e/taqueria-plugin-flextesa.spec.ts
+++ tests/e2e/taqueria-plugin-flextesa.spec.ts
@@ -260,101 +260,9 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		await cleanup();
 	});
 
-	/// Not got to yet zone ///
-
-	// TODO: Need to come back to this one. Sometimes it succeeds, and other times it fails.
-	test.skip('Verify that taqueria can return JSON when request for list of accounts from a sandbox is made by TVsCE', async () => {
-		await exec(`taq start sandbox ${'local'}`, { cwd: `././test-project` });
-		// await sleep(17000); // not too sure why this is needed here
-		const accounts = await exec(`taq list accounts ${'local'} --fromVsCode`, { cwd: `././test-project` });
-		expect(accounts.stdout).toEqual(
-			JSON.stringify([
-				{
-					account: 'bob',
-					balance: '3000 ꜩ',
-					address: 'tz1aSkwEot3L2kmUvcoxzjMomb9mvBNuzFK6',
-				},
-				{
-					account: 'alice',
-					balance: '3000 ꜩ',
-					address: 'tz1VSUr8wwNhLAzempoch5d6hLRiTh8Cjcjb',
-				},
-				{
-					account: 'john',
-					balance: '3000 ꜩ',
-					address: 'tz1Zwoh1QCVAvJ4sVTojMp9pLYp6Ji4NoZy6',
-				},
-				{
-					account: 'jane',
-					balance: '3000 ꜩ',
-					address: 'tz1aHUAC4oviwJuZF1EvVSvFz7cu9KMNYBph',
-				},
-				{
-					account: 'joe',
-					balance: '3000 ꜩ',
-					address: 'tz1MVGjgD1YtAPwohsSfk8i3ZiT1yEGM2YXB',
-				},
-			]) + '\n',
-		);
-	});
-
-	// TODO: may add it add later
-	test.skip('Verify that an environment variable can override the flextesa docker image', async () => {
-		// const imageName = 'ghcr.io/ecadlabs/taqueria-flextesa:1429-merge-1ccbcc8';
-
-		// const sandboxStart = await exec(`TAQ_ECAD_FLEXTESA_IMAGE=${imageName} taq start sandbox`, {
-		// 	cwd: `././test-project`,
-		// 	// Cannot use the env property as it replaces the environment, which
-		// 	// contains the PATH for how to find the `taq` binary
-		// 	// env: {'TAQ_FLEXTESA_IMAGE': imageName}
-		// });
-
-		// // 2. Verify that sandbox has been started and taqueria returns proper message into console
-		// expect(sandboxStart.stdout).toContain(`Started ${'local'}.`);
-		// expect(sandboxStart.stdout).toContain(`Done.`);
-
-		// // 3. Verify that docker container has been started
-		// const dockerContainerTest = await getContainerName('local');
-		// expect(dockerContainerTest).toContain(`taq-flextesa-${'local'}`);
-		// expect(await getContainerImage('local')).toBe(imageName);
-
-		// // 4.  Run stop command and verify the output
-		// const sandboxStop = await exec(`TAQ_ECAD_FLEXTESA_IMAGE=${imageName} taq stop sandbox ${'local'}`, {
-		// 	cwd: `././test-project`,
-		// });
-		// await sleep(2500);
-	});
-
-	test.skip('Verify that an environment variable can override the flextesa docker image', async () => {
-		// const imageName = 'oxheadalpha/flextesa:20221026';
-
-		// await exec(`cp e2e/data/config-flextesa-test-sandbox.json ./test-project/.taq/config.json`);
-
-		// const sandboxStart = await exec(`TAQ_ECAD_FLEXTESA_IMAGE=${imageName} taq start sandbox override`, {
-		// 	cwd: `././test-project`,
-		// 	// Cannot use the env property as it replaces the environment, which
-		// 	// contains the PATH for how to find the `taq` binary
-		// 	// env: {'TAQ_ECAD_FLEXTESA_IMAGE': imageName}
-		// });
-
-		// // 2. Verify that sandbox has been started and taqueria returns proper message into console
-		// expect(sandboxStart.stdout).toContain(`The sandbox "override" is ready.`);
-
-		// // 3. Verify that docker container has been started
-		// const dockerContainerTest = await getContainerName('local');
-		// expect(dockerContainerTest).toContain(`taq-flextesa-override`);
-
-		// // Verify that the specific image is being used
-		// expect(await getContainerImage('override')).toContain(imageName);
-
-		// // 4.  Run stop command and verify the output
-		// await exec(`taq stop sandbox override`, { cwd: `././test-project` });
-		// await sleep(5000);
-	});
-
-	// TODO skipping for now - I don't know why, but this test only works if run-in-band (-i) is specified
+	// config does not include baker by default - is this a bug?
 	test.skip('start sandbox will initiate a baker daemon by default', async () => {
-		const { execute, cleanup, spawn } = await prepareEnvironment();
+		const { execute, cleanup, spawn, readFile } = await prepareEnvironment();
 		const { waitForText } = await spawn('taq', 'init test-project');
 		await waitForText("Project taq'ified!");
 		const { stdout } = await execute('taq', 'install ../taqueria-plugin-core', './test-project');
@@ -364,62 +272,18 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 
 		const { stdout: stdout2 } = await execute('taq', 'start sandbox local', './test-project');
 		expect(stdout2).toContain('The sandbox "local" is ready.');
+
+		console.log(await readFile('./test-project/.taq/config.json'));
+
 		const { stdout: stdout3 } = await execute('docker', 'ps', './test-project');
-		expect(stdout3).toContain('baker');
 
-		await cleanup();
-	});
+		console.log(stdout3);
 
-	test.skip('Verify that Taqueria accepts any origin and does not emit any CORS related errors', async () => {
-		// // Start web server for preflight request
-		// const p = await spawn('npx', ['ws'], {
-		// 	// detached: true,
-		// 	shell: true,
-		// });
-		// processes.push(p); // store process for cleanup
-
-		// // Stop the sandbox
-		// await exec(`taq start sandbox local`, { cwd: `././test-project` });
-
-		// // Give the sandbox some time to bake the genesis block
-		// await sleep(2000);
-
-		// // Get the port that the sandbox is running on
-		// const configContents = JSON.parse(
-		// 	await fsPromises.readFile(`./test-project/.taq/config.json`, { encoding: 'utf-8' }),
-		// );
-		// const port = configContents.sandbox.local.rpcUrl;
-
-		// // Connect to the sandbox using a different origin (CORS test)
-		// const { stdout } = await exec(`curl -i -H "Origin: http://localhost:8080" ${port}/version`);
-
-		// // Stop the sandbox when done
-		// await exec(`taq stop sandbox local`, { cwd: `././test-project` });
-
-		// // Assert that the connection to the sandbox via different origin was successful
-		// expect(stdout).toContain('HTTP/1.1 200 OK');
-	});
-
-	// hangs forever on the start sandbox with environent variable
-	test.skip('environment variable will override the flextesa docker image', async () => {
-		const { execute, cleanup, spawn } = await prepareEnvironment();
-		const { waitForText } = await spawn('taq', 'init test-project');
-		await waitForText("Project taq'ified!");
-		const { stdout } = await execute('taq', 'install ../taqueria-plugin-core', './test-project');
-		expect(stdout).toContain('Plugin installed successfully');
-		const { stdout: stdout1 } = await execute('taq', 'install ../taqueria-plugin-flextesa', './test-project');
-		expect(stdout1).toContain('Plugin installed successfully');
-
-		const { stdout: stdout2 } = await execute(
-			'TAQ_ECAD_FLEXTESA_IMAGE="oxheadalpha/flextesa:20221026" taq',
-			'start sandbox',
-			'./test-project',
-		);
-		expect(stdout2).toEqual(expect.arrayContaining(['The sandbox "local" is ready.']));
-
-		const { stdout: stdout3 } = await execute('docker', 'ps --filter name=taq-flextesa-local', './test-project');
 		expect(stdout3).toEqual(expect.arrayContaining([expect.stringContaining('taq-flextesa-local')]));
-		expect(stdout3).toEqual(expect.arrayContaining([expect.stringContaining('oxheadalpha/flextesa:20221026')]));
+		expect(stdout3).toEqual(expect.arrayContaining([expect.stringContaining('oxhead')]));
+
+		// const { stdout: stdout3 } = await execute('docker', 'ps', './test-project')
+		// expect(stdout3).toContain('baker');
 
 		await cleanup();
 	});
