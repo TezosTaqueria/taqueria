9b64683d241829f7ca9b59626c63808e55ff620e
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2022-12-29 11:18:49 -0800

    revised the smartpy plugin e2e tests

--- tests/e2e/taqueria-plugin-smartpy.spec.ts
+++ tests/e2e/taqueria-plugin-smartpy.spec.ts
@@ -1,116 +1,101 @@
+import { prepareEnvironment } from '@gmrchk/cli-testing-library';
 import { exec as exec1 } from 'child_process';
-import fsPromises from 'fs/promises';
-import path from 'path';
 import util from 'util';
-import { checkFolderExistsWithTimeout, generateTestProject } from './utils/utils';
 const exec = util.promisify(exec1);
 
-const taqueriaProjectPath = 'scrap/auto-test-smartpy-plugin';
+describe('SmartPy Plugin E2E Testing for Taqueria CLI', () => {
+	// blocked by https://github.com/ecadlabs/taqueria/issues/1635
+	test.skip('project help will offer contextual help', async () => {
+		const { execute, cleanup, spawn } = await prepareEnvironment();
+		const { waitForText } = await spawn('taq', 'init test-project');
+		await waitForText("Project taq'ified!");
+		const { stdout } = await execute('taq', 'install ../taqueria-plugin-smartpy', './test-project');
+		expect(stdout).toContain('Plugin installed successfully');
 
-describe('E2E Testing for taqueria SmartPy plugin', () => {
-	beforeAll(async () => {
-		await generateTestProject(taqueriaProjectPath, ['smartpy']);
-		// TODO: This can removed after this is resolved:
-		// https://github.com/ecadlabs/taqueria/issues/528
-		try {
-			await exec(`taq -p ${taqueriaProjectPath}`);
-		} catch (_) {}
+		const { stdout: stdout1 } = await execute('taq', '--help --projectDir=./test-project/', './test-project');
+		expect(stdout1).toEqual(expect.arrayContaining(['taq compile <sourceFile>']));
+		expect(stdout1).toEqual(expect.arrayContaining(['taq compile <sourceFile>']));
+
+		await cleanup();
 	});
 
-	// Remove all files from artifacts folder without removing folder itself
-	afterEach(async () => {
-		try {
-			const files = await fsPromises.readdir(`${taqueriaProjectPath}/artifacts/`);
-			for (const file of files) {
-				await fsPromises.rm(path.join(`${taqueriaProjectPath}/artifacts/`, file), { recursive: true });
-			}
-		} catch (error) {
-			throw new Error(`error: ${error}`);
-		}
+	// blocked by https://github.com/ecadlabs/taqueria/issues/1635
+	test.skip('compile will offer contextual help', async () => {
+		const { execute, cleanup, spawn } = await prepareEnvironment();
+		const { waitForText } = await spawn('taq', 'init test-project');
+		await waitForText("Project taq'ified!");
+		const { stdout } = await execute('taq', 'install ../taqueria-plugin-smartpy', './test-project');
+		expect(stdout).toContain('Plugin installed successfully');
+
+		const { stdout: stdout1 } = await execute('taq', 'compile --help --projectDir=./test-project/', './test-project');
+		expect(stdout1).toEqual(expect.arrayContaining(['taq compile <sourceFile>']));
+		expect(stdout1).toEqual(expect.arrayContaining(['taq compile <sourceFile>']));
+
+		await cleanup();
 	});
 
-	// Clean up process to remove taquified project folder
-	// Comment if need to debug
-	afterAll(async () => {
-		try {
-			await fsPromises.rm(taqueriaProjectPath, { recursive: true });
-		} catch (error) {
-			throw new Error(`error: ${error}`);
-		}
+	test('compile will compile one contract with compile <sourceFile> command', async () => {
+		const { execute, cleanup, spawn, writeFile, exists } = await prepareEnvironment();
+		const { waitForText } = await spawn('taq', 'init test-project');
+		await waitForText("Project taq'ified!");
+		const { stdout } = await execute('taq', 'install ../taqueria-plugin-smartpy', './test-project');
+		expect(stdout).toContain('Plugin installed successfully');
+
+		const py_file = await (await exec(`cat e2e/data/hello-tacos.py`)).stdout;
+		await writeFile('./test-project/contracts/hello-tacos.py', py_file);
+
+		const { stdout: stdout2 } = await execute('taq', 'compile hello-tacos.py', './test-project');
+		expect(stdout2).toEqual(expect.arrayContaining(['│ hello-tacos.py │ artifacts/hello-tacos.tz                 │']));
+		expect(stdout2).toEqual(expect.arrayContaining(['│                │ artifacts/hello-tacos.default_storage.tz │']));
+
+		await exists(`./test-project/artifacts/hello-tacos.tz`);
+		await exists(`./test-project/artifacts/hello-tacos.default_storage.tz`);
+
+		await cleanup();
 	});
 
-	test('Verify that the SmartPy plugin exposes the associated commands in the help menu', async () => {
-		try {
-			const { stdout } = await exec(`taq --help --projectDir=${taqueriaProjectPath}`);
-			expect(stdout).toContain('taq compile <sourceFile>');
-			expect(stdout).toContain('taq test <sourceFile>');
-		} catch (error) {
-			throw new Error(`error: ${error}`);
-		}
+	test('compile will error if no contract', async () => {
+		const { execute, cleanup, spawn } = await prepareEnvironment();
+		const { waitForText } = await spawn('taq', 'init test-project');
+		await waitForText("Project taq'ified!");
+		const { stdout } = await execute('taq', 'install ../taqueria-plugin-smartpy', './test-project');
+		expect(stdout).toContain('Plugin installed successfully');
+
+		const { stdout: stdout2 } = await execute('taq', 'compile no_such_file.py', './test-project');
+		expect(stdout2).toEqual(expect.arrayContaining(['│ no_such_file.py │ Not compiled │']));
+
+		await cleanup();
 	});
 
-	test('Verify that the compile task of the SmartPy plugin exposes the associated options in the help menu', async () => {
-		try {
-			const { stdout } = await exec(`taq compile --help --projectDir=${taqueriaProjectPath}`);
-			expect(stdout).toContain('--json');
-		} catch (error) {
-			throw new Error(`error: ${error}`);
-		}
+	test('test will run test on <sourceFile>', async () => {
+		const { execute, cleanup, spawn, writeFile, exists } = await prepareEnvironment();
+		const { waitForText } = await spawn('taq', 'init test-project');
+		await waitForText("Project taq'ified!");
+		const { stdout } = await execute('taq', 'install ../taqueria-plugin-smartpy', './test-project');
+		expect(stdout).toContain('Plugin installed successfully');
+
+		const py_file = await (await exec(`cat e2e/data/hello-tacos.py`)).stdout;
+		await writeFile('./test-project/contracts/hello-tacos.py', py_file);
+
+		const { stdout: stdout2 } = await execute('taq', 'test hello-tacos.py', './test-project');
+		expect(stdout2).toEqual(expect.arrayContaining(['│ hello-tacos.py │ 🎉 All tests passed 🎉 │']));
+
+		await cleanup();
 	});
 
-	test('Verify that taqueria SmartPy plugin can compile one contract using taq compile <sourceFile> command', async () => {
-		try {
-			// 1. Copy contract from data folder to taqueria project folder
-			await exec(`cp e2e/data/hello-tacos.py ${taqueriaProjectPath}/contracts`);
+	test('test will show fails on test file with fails', async () => {
+		const { execute, cleanup, spawn, writeFile } = await prepareEnvironment();
+		const { waitForText } = await spawn('taq', 'init test-project');
+		await waitForText("Project taq'ified!");
+		const { stdout } = await execute('taq', 'install ../taqueria-plugin-smartpy', './test-project');
+		expect(stdout).toContain('Plugin installed successfully');
 
-			// 2. Run taq compile ${contractName}
-			await exec(`taq compile hello-tacos.py`, { cwd: `./${taqueriaProjectPath}` });
+		const py_file = await (await exec(`cat e2e/data/hello-tacos-failed-tests.py`)).stdout;
+		await writeFile('./test-project/contracts/hello-tacos-failed-tests.py', py_file);
 
-			// 3. Verify that compiled michelson version has been generated
-			await checkFolderExistsWithTimeout(`./${taqueriaProjectPath}/artifacts/hello-tacos.tz`);
-			await checkFolderExistsWithTimeout(`./${taqueriaProjectPath}/artifacts/hello-tacos.default_storage.tz`);
-		} catch (error) {
-			throw new Error(`error: ${error}`);
-		}
-	});
+		const { stdout: stdout2 } = await execute('taq', 'test hello-tacos-failed-tests.py', './test-project');
+		expect(stdout2).toEqual(expect.arrayContaining(['│ hello-tacos-failed-tests.py │ Some tests failed :( │']));
 
-	test('Verify that taqueria SmartPy plugin will display proper message if user tries to compile contract that does not exist', async () => {
-		try {
-			// 1. Run taq compile ${contractName} for contract that does not exist
-			const { stdout } = await exec(`taq compile test.py`, { cwd: `./${taqueriaProjectPath}` });
-
-			// 2. Verify that output includes next messages:
-			expect(stdout).toContain('Not compiled');
-		} catch (error) {
-			throw new Error(`error: ${error}`);
-		}
-	});
-
-	test('Verify that taqueria SmartPy plugin can run SmartPy test using taq test <sourceFile> command', async () => {
-		try {
-			// 1. Copy contract from data folder to taqueria project folder
-			await exec(`cp e2e/data/hello-tacos.py ${taqueriaProjectPath}/contracts`);
-
-			// 2. Run taq test ${testFileName}
-			const { stdout } = await exec(`taq test hello-tacos.py`, { cwd: `./${taqueriaProjectPath}` });
-			expect(stdout).toContain('All tests passed');
-		} catch (error) {
-			throw new Error(`error: ${error}`);
-		}
-	});
-
-	test('Verify that taqueria SmartPy plugin will output proper error message running taq test <sourceFile> command against invalid test file', async () => {
-		try {
-			// 1. Copy contract from data folder to taqueria project folder
-			await exec(`cp e2e/data/hello-tacos-failed-tests.py ${taqueriaProjectPath}/contracts`);
-
-			// 2. Run taq test ${testFileName}
-			const { stdout } = await exec(`taq test hello-tacos-failed-tests.py`, {
-				cwd: `./${taqueriaProjectPath}`,
-			});
-			expect(stdout).toContain('Some tests failed');
-		} catch (error) {
-			throw new Error(`error: ${error}`);
-		}
+		await cleanup();
 	});
 });
