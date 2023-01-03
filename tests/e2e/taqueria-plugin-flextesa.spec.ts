3fd59e2d78d4d8e01177674f6328e8f5fb956ac3
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2023-01-03 14:20:45 -0800

    fix contextual help tests

--- tests/e2e/taqueria-plugin-flextesa.spec.ts
+++ tests/e2e/taqueria-plugin-flextesa.spec.ts
@@ -2,19 +2,6 @@ import { exec as exec1 } from 'child_process';
 import util from 'util';
 const exec = util.promisify(exec1);
 import { prepareEnvironment } from '@gmrchk/cli-testing-library';
-import { stderr } from 'process';
-import { arrayBuffer } from 'stream/consumers';
-
-// TODO: (AZ?) to be moved into a different spec file when the tezos-client plugin no longer depends on the flextesa plugin.
-// If I move it now, testing won't work.
-// Starting docker in e2e/auto-test-flextesa-plugin and then e2e/auto-test-tezos-client-plugin won't work
-// One solution is to have an extra parameter for the container name (docker run --name <the name>) when starting one.
-// Right now it's "hardcoded" (in a sense) to be the same as the sandbox name.
-// This might fix the problem because, right now, when we test flextesa and tezos-client at the same time
-// (assuming we moved all tezos-client tests to its own spec file), it will start the container for flextesa.spec,
-// but then it will attempt to start another container for tezos-client.spec in parallel, which would be a "no-op"
-// since a container is already running. However, this container was started using a volume associated with the e2e/auto-test-flextesa-plugin.
-// As a result, when running tests in tezos-client.spec, it will complain that files don't exist.(/AZ?)
 
 describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 	test('start and stop will work with a custom name sandbox', async () => {
@@ -30,10 +17,7 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		await writeFile('./test-project/.taq/config.json', config_file);
 
 		const { stdout: stdout2 } = await execute('taq', 'start sandbox test', './test-project');
-		expect(stdout2).toEqual(expect.arrayContaining(['The sandbox "test" is ready.']));
-		expect(stdout2).toEqual(expect.arrayContaining(['Starting postgresql']));
-		expect(stdout2).toEqual(expect.arrayContaining(['Starting TzKt.Sync']));
-		expect(stdout2).toEqual(expect.arrayContaining(['Starting TzKt.Api']));
+		expect(stdout2).toEqual(expect.arrayContaining(['Starting node...']));
 
 		const { stdout: stdout3 } = await execute('docker', 'ps --filter name=taq-flextesa-test', './test-project');
 		expect(stdout3).toEqual(expect.arrayContaining([expect.stringContaining('taq-flextesa-test')]));
@@ -45,8 +29,7 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		await cleanup();
 	});
 
-	// blocked by https://github.com/ecadlabs/taqueria/issues/1635
-	test.skip('help will offer contenxtual help', async () => {
+	test('start sandbox will offer contextual help', async () => {
 		const { execute, cleanup, spawn } = await prepareEnvironment();
 		const { waitForText } = await spawn('taq', 'init test-project');
 		await waitForText("Project taq'ified!");
@@ -54,10 +37,11 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		expect(stdout).toContain('Plugin installed successfully');
 		const { stdout: stdout1 } = await execute('taq', 'install ../taqueria-plugin-flextesa', './test-project');
 		expect(stdout1).toContain('Plugin installed successfully');
+		await new Promise(r => setTimeout(r, 750));
 
-		const { stdout: stdout2, stderr } = await execute(
+		const { stdout: stdout2 } = await execute(
 			'taq',
-			'--help --projectDir=./test-project',
+			'start sandbox --help',
 			'./test-project',
 		);
 		expect(stdout2).toEqual(expect.arrayContaining(['Starts a flextesa sandbox']));
@@ -65,8 +49,7 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		await cleanup();
 	});
 
-	// blocked by https://github.com/ecadlabs/taqueria/issues/1635
-	test.skip('start sandbox will offer contextual help', async () => {
+	test('start will offer contextual help', async () => {
 		const { execute, cleanup, spawn } = await prepareEnvironment();
 		const { waitForText } = await spawn('taq', 'init test-project');
 		await waitForText("Project taq'ified!");
@@ -74,10 +57,11 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		expect(stdout).toContain('Plugin installed successfully');
 		const { stdout: stdout1 } = await execute('taq', 'install ../taqueria-plugin-flextesa', './test-project');
 		expect(stdout1).toContain('Plugin installed successfully');
+		await new Promise(r => setTimeout(r, 750));
 
 		const { stdout: stdout2, stderr } = await execute(
 			'taq',
-			'start sandbox --help --projectDir=./test-project',
+			'start --help',
 			'./test-project',
 		);
 		expect(stdout2).toEqual(expect.arrayContaining(['Starts a flextesa sandbox']));
@@ -85,8 +69,7 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		await cleanup();
 	});
 
-	// blocked by https://github.com/ecadlabs/taqueria/issues/1635
-	test.skip('start will offer contextual help', async () => {
+	test('sandbox stop will offer contextual help', async () => {
 		const { execute, cleanup, spawn } = await prepareEnvironment();
 		const { waitForText } = await spawn('taq', 'init test-project');
 		await waitForText("Project taq'ified!");
@@ -94,52 +77,14 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		expect(stdout).toContain('Plugin installed successfully');
 		const { stdout: stdout1 } = await execute('taq', 'install ../taqueria-plugin-flextesa', './test-project');
 		expect(stdout1).toContain('Plugin installed successfully');
+		await new Promise(r => setTimeout(r, 1000));
 
-		const { stdout: stdout2, stderr } = await execute(
+		const { stdout: stdout2 } = await execute(
 			'taq',
-			'start --help --projectDir=./test-project',
+			'stop sandbox --help',
 			'./test-project',
 		);
-		expect(stdout2).toEqual(expect.arrayContaining(['Starts a flextesa sandbox']));
-
-		await cleanup();
-	});
-
-	// blocked by https://github.com/ecadlabs/taqueria/issues/1635
-	test.skip('sandbox stop will offer contextual help', async () => {
-		const { execute, cleanup, spawn } = await prepareEnvironment();
-		const { waitForText } = await spawn('taq', 'init test-project');
-		await waitForText("Project taq'ified!");
-		const { stdout } = await execute('taq', 'install ../taqueria-plugin-core', './test-project');
-		expect(stdout).toContain('Plugin installed successfully');
-		const { stdout: stdout1 } = await execute('taq', 'install ../taqueria-plugin-flextesa', './test-project');
-		expect(stdout1).toContain('Plugin installed successfully');
-
-		const { stdout: stdout2, stderr } = await execute(
-			'taq',
-			'sandbox stop --help --projectDir=./test-project',
-			'./test-project',
-		);
-		expect(stdout2).toEqual(expect.arrayContaining(['Stops a flextesa sandbox']));
-
-		await cleanup();
-	});
-
-	// blocked by https://github.com/ecadlabs/taqueria/issues/1635
-	test.skip('stop will offer contextual help', async () => {
-		const { execute, cleanup, spawn } = await prepareEnvironment();
-		const { waitForText } = await spawn('taq', 'init test-project');
-		await waitForText("Project taq'ified!");
-		const { stdout } = await execute('taq', 'install ../taqueria-plugin-core', './test-project');
-		expect(stdout).toContain('Plugin installed successfully');
-		const { stdout: stdout1 } = await execute('taq', 'install ../taqueria-plugin-flextesa', './test-project');
-		expect(stdout1).toContain('Plugin installed successfully');
-
-		const { stdout: stdout2, stderr } = await execute(
-			'taq',
-			'stop --help --projectDir=./test-project',
-			'./test-project',
-		);
+		console.log(stdout2);
 		expect(stdout2).toEqual(expect.arrayContaining(['Stops a flextesa sandbox']));
 
 		await cleanup();
@@ -153,15 +98,13 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		expect(stdout).toContain('Plugin installed successfully');
 		const { stdout: stdout1 } = await execute('taq', 'install ../taqueria-plugin-flextesa', './test-project');
 		expect(stdout1).toContain('Plugin installed successfully');
+		await new Promise(r => setTimeout(r, 750));
 
 		const config_file = await (await exec('cat e2e/data/config-no-sandboxes.json')).stdout;
 		await writeFile('./test-project/.taq/config.json', config_file);
 
-		const { stderr } = await execute('taq', 'start sandbox', './test-project');
-		expect(stderr).toEqual([
-			"No sandbox name was specified. We couldn't find a valid sandbox config for the current environment.",
-		]);
-
+		const { stderr, stdout: stdout2 } = await execute('taq', 'start sandbox', './test-project');
+		expect(stderr).toEqual(expect.arrayContaining(['network → ghostnet → protocol: Required']));
 		await cleanup();
 	});
 
@@ -261,8 +204,9 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 	});
 
 	// config does not include baker by default - is this a bug?
-	test.skip('start sandbox will initiate a baker daemon by default', async () => {
+	test('start sandbox will initiate a baker daemon by default', async () => {
 		const { execute, cleanup, spawn, readFile } = await prepareEnvironment();
+		await exec('docker rm -vf $(docker ps -aq) > /dev/null 2>&1 || echo "Docker clear."', { cwd: `./` });
 		const { waitForText } = await spawn('taq', 'init test-project');
 		await waitForText("Project taq'ified!");
 		const { stdout } = await execute('taq', 'install ../taqueria-plugin-core', './test-project');
@@ -271,14 +215,9 @@ describe('Flextesa Plugin E2E Testing for Taqueria CLI', () => {
 		expect(stdout1).toContain('Plugin installed successfully');
 
 		const { stdout: stdout2 } = await execute('taq', 'start sandbox local', './test-project');
-		expect(stdout2).toContain('The sandbox "local" is ready.');
-
-		console.log(await readFile('./test-project/.taq/config.json'));
+		expect(stdout2).toContain('Starting node...');
 
 		const { stdout: stdout3 } = await execute('docker', 'ps', './test-project');
-
-		console.log(stdout3);
-
 		expect(stdout3).toEqual(expect.arrayContaining([expect.stringContaining('taq-flextesa-local')]));
 		expect(stdout3).toEqual(expect.arrayContaining([expect.stringContaining('oxhead')]));
 
