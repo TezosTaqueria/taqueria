908fff29b86d951346ae70f037e70c7029bc8010
Alireza Haghshenas <alirezahaghshenas@gmail.com>
2023-01-04 18:02:49 -0800

    Update to latest TzKt images maintained by ECAD

--- taqueria-plugin-flextesa/tzkt-manager.ts
+++ taqueria-plugin-flextesa/tzkt-manager.ts
@@ -5,8 +5,8 @@ import { ValidOpts } from './types';
 
 const getTzKtDockerImages = (opts: ValidOpts) => ({
 	postgres: `postgres:14.5-alpine`,
-	sync: `tzktsynclocal`, // TODO: This will be replaced by our image or BakingBad, if they marge our PR, should fix before merging
-	api: `tzktapilocal`, // Same as above
+	sync: `ghcr.io/ecadlabs/tzkt-sync:v1.11.0-taqueria`,
+	api: `ghcr.io/ecadlabs/tzkt-api:v1.11.0-taqueria`,
 });
 
 export const getTzKtContainerNames = async (sandboxName: string, parsedArgs: ValidOpts) => {
