33e66d43e946baa7f59262190e25dec93a28b859
Michael Kernaghan <michaelkernaghan@ecadlabs.com>
2023-01-17 07:46:11 -0800

    added test to show marigold training tutorial works

--- tests/e2e/marigold-training-e2e-smoke-test.spec.ts
+++ tests/e2e/marigold-training-e2e-smoke-test.spec.ts
@@ -2,12 +2,23 @@ import { exec as exec1 } from 'child_process';
 import util from 'util';
 const exec = util.promisify(exec1);
 import { prepareEnvironment } from '@gmrchk/cli-testing-library';
+import { readFile } from 'fs';
 
 describe('Marigold Training E2E Smoke Test for Taqueria CLI', () => {
 	test('Step 1 : Create folder & file', async () => {
-		const { execute, cleanup, writeFile, ls, exists } = await prepareEnvironment();
+		// set up the project
+		const { execute, cleanup, writeFile, exists } = await prepareEnvironment();
 		await execute('taq', 'init test-project');
 		await exists('./test-project/.taq/config.json');
+
+		// use prepard config with funded PKH
+		const test_config_file =
+			await (await exec('cat e2e/data/config-data/config-taquito-test-environment-funded-pkh.json')).stdout;
+		await writeFile('./test-project/.taq/config.json', test_config_file);
+
+		// install plugins
+		await execute('taq', 'install ../taqueria-plugin-core', './test-project');
+		await exists('./test-project/node_modules/@taqueria/plugin-core/index.js');
 		await execute('taq', 'install ../taqueria-plugin-ligo', './test-project');
 		await exists('./test-project/node_modules/@taqueria/plugin-ligo/index.js');
 		await execute('taq', 'install ../taqueria-plugin-tezos-client', './test-project');
@@ -15,6 +26,7 @@ describe('Marigold Training E2E Smoke Test for Taqueria CLI', () => {
 		await execute('taq', 'install ../taqueria-plugin-taquito', './test-project');
 		await exists('./test-project/node_modules/@taqueria/plugin-taquito/index.js');
 
+		// copy jsligo files to contracts folder
 		const jligo_file = await (await exec(`cat e2e/data/ligo-data/pokeGame.jsligo`)).stdout;
 		await writeFile('./test-project/contracts/pokeGame.jsligo', jligo_file);
 		const storage_file = await (await exec(`cat e2e/data/ligo-data/pokeGame.storageList.jsligo`)).stdout;
@@ -22,6 +34,7 @@ describe('Marigold Training E2E Smoke Test for Taqueria CLI', () => {
 		const parameters_file = await (await exec(`cat e2e/data/ligo-data/pokeGame.parameterList.jsligo`)).stdout;
 		await writeFile('./test-project/contracts/pokeGame.parameterList.jsligo', parameters_file);
 
+		// compile the contract
 		const { stdout } = await execute('taq', 'compile pokeGame.jsligo', './test-project');
 		expect(stdout).toMatchInlineSnapshot(`
        [
@@ -37,24 +50,29 @@ describe('Marigold Training E2E Smoke Test for Taqueria CLI', () => {
        ]
    `);
 
+		// simulate the contract
 		const { stdout: stdout2 } = await execute(
 			'taq',
 			'simulate pokeGame.tz --param pokeGame.parameter.default_parameter.tz',
 			'./test-project',
 		);
-		expect(stdout2).toEqual(expect.arrayContaining(['│ pokeGame.tz │ storage                                      │']));
 
-		const { stdout: stdout3, stderr } = await execute(
+		// deploy the contract
+		// expect(stdout2).toEqual(expect.arrayContaining(['│ pokeGame.tz │ storage                                      │']));
+		// await execute(
+		// 	'taq',
+		// 	'deploy pokeGame.tz -e "testing"',
+		// 	'./test-project',
+		// );
+		// await execute('taq', 'fund -e testing', './test-project');
+		const { stdout: stdout4 } = await execute(
 			'taq',
 			'deploy pokeGame.tz -e "testing"',
 			'./test-project',
 		);
-		console.log(stderr);
-		expect(stderr).toEqual(
-			expect.arrayContaining(['1. Go to https://teztnets.xyz and click "Faucet" of the target testnet']),
-		);
-		0;
+		expect(stdout4).toEqual(expect.arrayContaining([expect.stringContaining('https://ghostnet.ecadinfra.com')]));
 
+		// tear down the project
 		await cleanup();
 	});
 });
