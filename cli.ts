b64483c8b1890272b2b30eb81473c871e3da9f53
Michael Weichert <mweichert@gmail.com>
2023-02-09 17:19:22 -0500

    🛠️ Fix ➾ Dynamically set the column wrapping of the yargs help menu (#1842)
    * Dynamically set the column wrapping of the yargs help menu
    
    * Add failback value of 80 columns for CLI

--- cli.ts
+++ cli.ts
@@ -115,9 +115,19 @@ const getVersion = (inputArgs: DenoArgs) => {
 		: 'not-provided';
 };
 
-const commonCLI = (env: EnvVars, args: DenoArgs, i18n: i18n.t) =>
-	yargs(args)
+const getColumnWidth = () => {
+	try {
+		return Deno.consoleSize(Deno.stdout.rid).columns;
+	} catch {
+		return 80;
+	}
+};
+
+const commonCLI = (env: EnvVars, args: DenoArgs, i18n: i18n.t) => {
+	const instance = yargs(args);
+	return instance
 		.scriptName('taq')
+		.wrap(getColumnWidth())
 		.option('setVersion', {
 			describe: i18n.__('setVersionDesc'),
 			demandOption: true,
@@ -173,6 +183,7 @@ const commonCLI = (env: EnvVars, args: DenoArgs, i18n: i18n.t) =>
 		})
 		.epilogue(i18n.__('betaWarning'))
 		.help(false);
+};
 
 const initCLI = (env: EnvVars, args: DenoArgs, i18n: i18n.t) => {
 	// Add "init" task used to initialize a new project
