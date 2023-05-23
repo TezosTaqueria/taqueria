d3eb9be5429855f20631dc72fc24d813efa65331
Michael Weichert <mweichert@gmail.com>
2023-05-23 11:59:58 -0400

    Fixed use of rambda's omit

--- plugins.ts
+++ plugins.ts
@@ -225,10 +225,10 @@ export const inject = (deps: PluginDeps) => {
 		// --
 		// We'll use the projectDir from the config rather than the one from the CLI, as
 		// as the actual projectDir could be found via traversal
-		const passableParsedArgs = omit(['p', 'project-dir', 'projectDir'], parsedArgs);
+		const passableParsedArgs = omit(['p', 'project-dir', 'projectDir'], parsedArgs) ?? {};
 		const passableRequestArgs = omit(Object.keys(passableParsedArgs), requestArgs);
 		return Object.entries({ ...passableParsedArgs, ...passableRequestArgs, projectDir: config.projectDir }).reduce(
-			(retval: (string | number | boolean)[], [key, val]) => {
+			(retval: (string | number | boolean)[], [key, val]: [string, unknown]) => {
 				const omit = [
 					'$0',
 					'quickstart',
