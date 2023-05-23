e03598db0b3b678b02334223f96fb73f35e3d32d
Michael Weichert <mweichert@gmail.com>
2023-05-23 15:54:45 -0400

    Debug

--- tests/e2e/smartpy-plugin-e2e-tests.spec.ts
+++ tests/e2e/smartpy-plugin-e2e-tests.spec.ts
@@ -3,7 +3,8 @@ import { exec as exec1 } from 'child_process';
 import util from 'util';
 const exec = util.promisify(exec1);
 
-jest.setTimeout(100000);
+// Set timeout to 5 minutes
+jest.setTimeout(5 * 60 * 1000);
 
 describe('SmartPy Plugin E2E Testing for Taqueria CLI', () => {
 	test('compile will offer contextual help', async () => {
