5ad79d65c7433991c55274737c89aeb544b24de7
Michael Weichert <mweichert@gmail.com>
2023-05-23 23:47:30 -0400

    Add more logging

--- taqueria-plugin-smartpy/install.sh
+++ taqueria-plugin-smartpy/install.sh
@@ -105,6 +105,9 @@ fi
 
 # # Copy prebuilt files stored in the plugin
 >&2 echo "Copying prebuilt files..." 
+>&2 ls $projectDir/node_modules/
+>&2 ls $projectDir/node_modules/@taqueria
+>&2 ls $projectDir/node_modules/@taqueria/plugin-smartpy
 PREBUILD_DIR="$projectDir/node_modules/@taqueria/plugin-smartpy/smartpy-v0.16.0"
 for file in `ls $PREBUILD_DIR/`; do
     dst="$prefix/`basename $file`"
