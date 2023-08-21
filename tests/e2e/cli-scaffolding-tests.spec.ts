bcd55970315d431951e04bc18ecc757a080cfd85
Michael Weichert <mweichert@gmail.com>
2023-08-21 19:22:18 -0400

    Formatting

---
+++ tests/e2e/cli-scaffolding-tests.spec.ts
@@ -0,0 +1,38 @@
+import { prepareEnvironment } from '@gmrchk/cli-testing-library';
+
+// This test suite does two time-consuming things:
+// - Scaffolds a project
+// - Starts and stops a sandbox
+describe('E2E Testing for taqueria scaffolding initialization,', () => {
+	jest.setTimeout(40000);
+
+	test('Verify that scaffold project gets sets up.', async () => {
+		const { execute, cleanup, path: projectDir } = await prepareEnvironment();
+		// console.log(projectDir)
+
+		// Scaffold the project
+		const { code } = await execute(
+			'taq',
+			'scaffold https://github.com/pinnacle-labs/taqueria-scaffold-taco-shop.git scaffold-test',
+		);
+		expect(code).toBe(0);
+		const expectedContents: string[] = [
+			'scaffold-test/.taq/config.json',
+			'scaffold-test/app',
+			'scaffold-test/node_modules',
+			'scaffold-test/contracts',
+			'scaffold-test/artifacts/hello-tacos.tz',
+		];
+
+		// Get the path to the `ls` command
+		const whichResult = await execute('which', 'ls');
+		const ls = whichResult.stdout.join('').trim();
+
+		// Get the contents of the scaffold directory
+		const scaffoldDirContents = await execute(ls, `-d ${expectedContents.join(' ')}`);
+		expectedContents.map(file => expect(scaffoldDirContents.stdout).toContain(file));
+
+		// Cleanup
+		return await cleanup();
+	});
+});
