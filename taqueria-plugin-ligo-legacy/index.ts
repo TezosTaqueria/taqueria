830b6274ec0133878448471a2ab8f35ee9acc311
Michael Weichert <mweichert@gmail.com>
2023-10-05 11:01:15 -0400

    Added @taqueria/plugin-ligo-legacy package which targets LIGO v0.73.0. Related to #1909

---
+++ taqueria-plugin-ligo-legacy/index.ts
@@ -0,0 +1,9 @@
+import { configurePlugin } from '@taqueria/lib-ligo';
+
+configurePlugin({
+	name: '@taqueria/plugin-ligo-legacy',
+	alias: 'ligo-legacy',
+	dockerImage: 'ligolang/ligo:0.73.0',
+	dockerImageEnvVar: 'TAQ_LIGO_LEGACY_IMAGE',
+	unparsedArgs: process.argv,
+});
