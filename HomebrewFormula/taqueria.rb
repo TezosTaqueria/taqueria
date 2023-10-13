6eec0e195f11db5d8b22ccc1f37afe4eb41ab8ac
pinnacle-deren <deren@pinnaclelabs.limited>
2023-10-13 14:27:01 -0400

    Fixes #1917, working homebrew formula

--- HomebrewFormula/taqueria.rb
+++ HomebrewFormula/taqueria.rb
@@ -1,18 +1,20 @@
 class Taqueria < Formula
     desc "Extensible, open-source plugin-based framework and task runner for Tezos development with a CLI and a library of plugins."
     homepage "https://taqueria.io"
-    version "0.41.0"
-    url "https://github.com/pinnacle-labs/taqueria/archive/refs/tags/v0.41.0.tar.gz"
+    version "0.41.12"
+    head "https://github.com/pinnacle-labs/taqueria.git", :branch => "main"
+    url "https://github.com/pinnacle-labs/taqueria/archive/refs/tags/v0.41.12.tar.gz"
     sha256 "9e95ce34ecf7cd984b2e373151136f9baccfa04fd1b7c710b7d858561164dcfc"
   
     depends_on "node"
     depends_on "deno"
   
-    def install      
-      # Change to the repo directory
-      cd "taqueria-#{version}" do
+    def install
+        # Append the path to npm to the PATH environment variable
+        ENV["PATH"] = "/usr/local/bin:#{ENV["PATH"]}"
+
         # Install npm dependencies
-        system "npm", "install"
+        system "npm install >/dev/null 2>&1"
         
         # Build types and binary
         system "npm", "run", "build-types"
@@ -21,7 +23,6 @@ class Taqueria < Formula
         # You might need to specify where to place the binary, and potentially 
         # other steps to finalize the installation.
         bin.install "taq"
-      end
     end
   end
   
