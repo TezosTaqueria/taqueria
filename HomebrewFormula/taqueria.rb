6d140535c36ccf5229dec84ff91e7b0895340471
pinnacle-deren <deren@pinnaclelabs.limited>
2023-10-13 14:31:49 -0400

    Fix husky pre-commit hook

--- HomebrewFormula/taqueria.rb
+++ HomebrewFormula/taqueria.rb
@@ -1,9 +1,9 @@
 class Taqueria < Formula
     desc "Extensible, open-source plugin-based framework and task runner for Tezos development with a CLI and a library of plugins."
     homepage "https://taqueria.io"
-    version "0.41.12"
+    version "0.41.0"
     head "https://github.com/pinnacle-labs/taqueria.git", :branch => "main"
-    url "https://github.com/pinnacle-labs/taqueria/archive/refs/tags/v0.41.12.tar.gz"
+    url "https://github.com/pinnacle-labs/taqueria/archive/refs/tags/v0.41.0.tar.gz"
     sha256 "9e95ce34ecf7cd984b2e373151136f9baccfa04fd1b7c710b7d858561164dcfc"
   
     depends_on "node"
