4d7c632e6dbf1f2ca2e1b3e4285f8992eb589293
pinnacle-deren <deren@pinnaclelabs.limited>
2023-10-13 16:12:07 -0400

    Ensure that 0.42.0 is distributed via Homebrew

--- HomebrewFormula/taqueria.rb
+++ HomebrewFormula/taqueria.rb
@@ -1,10 +1,10 @@
 class Taqueria < Formula
     desc "Extensible, open-source plugin-based framework and task runner for Tezos development with a CLI and a library of plugins."
     homepage "https://taqueria.io"
-    version "0.41.0"
+    version "0.42.0"
     head "https://github.com/pinnacle-labs/taqueria.git", :branch => "main"
-    #url "https://github.com/pinnacle-labs/taqueria/archive/refs/tags/v0.41.00.tar.gz"
-    url "https://github.com/pinnacle-labs/taqueria/archive/03c85ca453f5010df75e0357e6d8e940be1aec85.tar.gz" # v0.41.23
+    url "https://github.com/pinnacle-labs/taqueria/archive/refs/tags/v0.42.00.tar.gz"
+    #url "https://github.com/pinnacle-labs/taqueria/archive/03c85ca453f5010df75e0357e6d8e940be1aec85.tar.gz" # v0.41.23
   
     depends_on "node"
     depends_on "deno"
