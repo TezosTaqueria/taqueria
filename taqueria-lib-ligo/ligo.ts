3090d9146063a369dc4bfcef3c1bc7dbdf601fed
callistonianembrace <87455417+callistonianembrace@users.noreply.github.com>
2023-10-27 16:13:35 +0530

    Use baseDriverCmd() in ligo.ts

--- taqueria-lib-ligo/ligo.ts
+++ taqueria-lib-ligo/ligo.ts
@@ -1,61 +1,53 @@
 import { execCmd, getArch, sendAsyncErr, sendRes, spawnCmd } from '@taqueria/node-sdk';
 import { readJsonFile, writeJsonFile } from '@taqueria/node-sdk';
 import { join } from 'path';
-import { Common, configure, LigoOpts as Opts } from './common';
+import { baseDriverCmd, Common, configure, LigoOpts as Opts } from './common';
 
 const getArbitraryLigoCmd = (
 	commonObj: Common,
 	parsedArgs: Opts,
-	uid: string,
-	gid: string,
 	userArgs: string,
 ): [string, Record<string, string>] => {
 	const projectDir = process.env.PROJECT_DIR ?? parsedArgs.projectDir;
+	const { getLigoDockerImage } = commonObj;
 	if (!projectDir) throw `No project directory provided`;
 
-	const userMap = uid && gid ? `${uid}:${gid}` : uid;
-	const userMapArgs = uid ? ['-u', userMap] : [];
+	const processedUserArgs = userArgs
+		.split(' ')
+		.map(arg => (arg.startsWith('\\-') ? arg.substring(1) : arg))
+		.filter(arg => arg);
 
-	const binary = 'docker';
-	const baseArgs = [
-		'run',
-		'--rm',
-		'-v',
-		`${projectDir}:/project`,
-		'-w',
-		'/project',
-		...userMapArgs,
-		commonObj.getLigoDockerImage(),
-	];
-	const processedUserArgs = userArgs.split(' ').map(arg => arg.startsWith('\\-') ? arg.substring(1) : arg).filter(arg =>
-		arg
-	);
-	const args = [...baseArgs, ...processedUserArgs, '--skip-analytics'];
-	const envVars = { 'DOCKER_DEFAULT_PLATFORM': 'linux/amd64' };
-	return [
-		[binary, ...args].join(' '),
-		envVars,
-	];
+	const cmd = `${
+		baseDriverCmd(
+			projectDir,
+			getLigoDockerImage(),
+		)
+	} ${processedUserArgs.join(' ')}`;
+
+	const envVars = { DOCKER_DEFAULT_PLATFORM: 'linux/amd64' };
+	return [cmd, envVars];
 };
 
-const runArbitraryLigoCmd = (commonObj: Common, parsedArgs: Opts, cmd: string): Promise<string> =>
-	(async () => {
-		const uid = await execCmd('id -u');
-		const gid = await execCmd('id -g');
-		return [uid.stdout.trim(), gid.stdout.trim()];
-	})()
-		.then(([uid, gid]) => getArbitraryLigoCmd(commonObj, parsedArgs, uid, gid, cmd))
-		.then(([cmd, envVars]) => spawnCmd(cmd, envVars))
+const runArbitraryLigoCmd = (
+	commonObj: Common,
+	parsedArgs: Opts,
+	userCmd: string,
+): Promise<string> => {
+	let [cmd, envVars] = getArbitraryLigoCmd(commonObj, parsedArgs, userCmd);
+	return spawnCmd(cmd, envVars)
 		.then(code =>
 			code !== null && code === 0
 				? `Command "${cmd}" ran successfully by LIGO`
 				: `Command "${cmd}" failed. Please check your command`
 		)
 		.catch(err => sendAsyncErr(`An internal error has occurred: ${err.message}`));
+};
 
 const ligo = (commonObj: Common, parsedArgs: Opts): Promise<void> => {
 	const args = parsedArgs.command;
-	return runArbitraryLigoCmd(commonObj, parsedArgs, args).then(sendRes).catch(err => sendAsyncErr(err, false));
+	return runArbitraryLigoCmd(commonObj, parsedArgs, args)
+		.then(sendRes)
+		.catch(err => sendAsyncErr(err, false));
 };
 
 export default ligo;
