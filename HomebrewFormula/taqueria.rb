37d2cb4ce4db5c3c32ef4ecada9ff1e8992fb275
Michael Weichert <github@weichert.dev>
2024-11-04 12:43:41 -0500

    In homebrew formula, check if Node v23 is installed and output an error

--- HomebrewFormula/taqueria.rb
+++ HomebrewFormula/taqueria.rb
@@ -1,19 +1,25 @@
 class Taqueria < Formula
     desc "Extensible, open-source plugin-based framework and task runner for Tezos development with a CLI and a library of plugins."
     homepage "https://taqueria.io"
-    version "0.62.7"
+    version "0.63"
     head "https://github.com/tezostaqueria/taqueria.git", :branch => "main"
-    url "https://github.com/tezostaqueria/taqueria/archive/refs/tags/v0.62.7.tar.gz"
+    url "https://github.com/tezostaqueria/taqueria/archive/refs/tags/v0.63.tar.gz"
   
     depends_on "node@20"
     depends_on "deno"
     depends_on "pnpm"
   
     def install
+        # Check Node.js version and fail if v23 or later
+        node_version = Utils.popen_read("node", "-v").strip.delete_prefix("v").to_i
+        if node_version >= 23
+          odie "This formula is not compatible with Node.js v23 or later. Please use an earlier version of Node.js."
+        end
+
         # Append the path to npm to the PATH environment variable
         ENV["PATH"] = "/usr/local/bin:#{ENV["PATH"]}"
-        ENV['TAQ_VERSION'] = '0.62.7'
-        ENV['TAQ_BUILD'] = '0.62.7'
+        ENV['TAQ_VERSION'] = '0.63'
+        ENV['TAQ_BUILD'] = '0.63'
 
         # Install npm dependencies
         system "pnpm install >install.log 2>&1"
