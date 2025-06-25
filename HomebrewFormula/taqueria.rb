8512ef23cf797f8b37bd8928d987f77a76d79b94
Michael Weichert <github@weichert.dev>
2025-06-25 14:35:48 -0400

    Updated to the latest version

--- HomebrewFormula/taqueria.rb
+++ HomebrewFormula/taqueria.rb
@@ -1,9 +1,9 @@
 class Taqueria < Formula
     desc "Extensible, open-source plugin-based framework and task runner for Tezos development with a CLI and a library of plugins."
     homepage "https://taqueria.io"
-    version "0.68.0"
+    version "0.72.0"
     head "https://github.com/tezostaqueria/taqueria.git", :branch => "main"
-    url "https://github.com/tezostaqueria/taqueria/archive/refs/tags/v0.68.0.tar.gz"
+    url "https://github.com/tezostaqueria/taqueria/archive/refs/tags/v0.72.0.tar.gz"
   
     depends_on "node@20"
     depends_on "deno"
@@ -18,8 +18,8 @@ class Taqueria < Formula
 
         # Append the path to npm to the PATH environment variable
         ENV["PATH"] = "/usr/local/bin:#{ENV["PATH"]}"
-        ENV['TAQ_VERSION'] = '0.68.0'
-        ENV['TAQ_BUILD'] = '0.68.0'
+        ENV['TAQ_VERSION'] = '0.72.0'
+        ENV['TAQ_BUILD'] = '0.72.0'
 
         # Install npm dependencies
         system "pnpm install >install.log 2>&1"
